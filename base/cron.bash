99cbc7a4c3d805762327f79ee4401546bfd068fe
Simon C Blyth <simoncblyth@gmail.com>
2007-05-25 15:41:28 +0800

    crontab-setup

--- base/cron.bash
+++ base/cron.bash
@@ -1,5 +1,24 @@
 
+
+crontab-delete(){
+   sudo crontab -u root -r -i 
+}
+
 crontab-setup(){
+
+## defaults to three minutes from now
+## note limitation : assumes not about to go into another hr, day, month etc..
+
+local       def_minute=$(( $(date +"%M") + 3 ))   
+local         def_hour=$(date +"%H")
+local def_day_of_month=$(date +"%d")
+local        def_month=$(date +"%m")
+
+local       minute=${1:-$def_minute}
+local         hour=${2:-$def_hour}
+local day_of_month=${3:-$def_day_of_month}
+local        month=${4:-$def_month}
+local  day_of_week="*"
 
 
 if [ "$NODE_TAG" == "G" ]; then
@@ -26,7 +45,7 @@ HOME=/tmp
 # |  |  |  |  |
 # *  *  *  *  *  command to be executed
 #
-15 25 24 5 * $cmd
+$minute $hour $day_of_month $month $day_of_week $cmd
 #
 EOF
 
@@ -34,6 +53,7 @@ EOF
 reply=$(sudo crontab -u root -l 2>&1)      ## redirection sending stderr onto stdout
 if ([ "$reply" == "no crontab for root" ] || [ "$reply" == "crontab: no crontab for root" ])  then
    echo =========== initializing crontab for root to $tmp 
+   cat $tmp 
    sudo crontab -u root $tmp
 else
    echo cannot proceed as a crontab for root exists already
