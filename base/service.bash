6d0b1d418a10d063d7d6c9d36ccce74599bbe689
Simon C Blyth <simoncblyth@gmail.com>
2007-05-26 11:08:42 +0800

    ntp additions

--- base/service.bash
+++ base/service.bash
@@ -9,7 +9,7 @@ service-setup(){
 
    # 
    #  loop over links in /etc/init.d selecting those that point to 
-   #  files owned by the current user or that have have basename apachectl,
+   #  files owned by the current user or that have have service_name apache,
    #  for the selected files edit the script annotating with the chkconfig 2 lines
    #  that assign run levels and start/stop priority , and add the service into
    #  chkconfig management 
@@ -94,6 +94,53 @@ service-act(){
 
 
 }
+
+
+ntp-setup(){
+
+   cat << EOH
+enter server lines like this , of a stratum 2 ntp server :
+# http://ntp.isc.org/bin/view/Servers/StratumTwoTimeServers
+server ntp.ucsd.edu
+
+logfile /var/log/ntpd.log
+
+then restart the ntpd service
+
+EOH
+
+  echo sudo vi /etc/ntp.conf
+  echo sudo /sbin/service ntpd restart 
+
+  sudo /usr/sbin/ntpd -q -g 
+
+}
+
+
+ntp-log(){
+   cat /var/log/ntpd.log
+}
+
+
+# locate ntpd reveals :
+#
+#  /usr/sbin/ntpdate
+#  /usr/sbin/
+#
+#
+
+#
+#[blyth@hfag env]$  sudo /sbin/service ntpd restart
+#ntpd: Removing firewall opening for 127.127.1.0 port 123   [  OK  ]
+#ntpd: Removing firewall opening for ntp.ucsd.edu port 123iptables: Bad rule (does a matching rule exist in that chain?)
+#                                                           [FAILED]
+#Shutting down ntpd:                                        [  OK  ]
+#ntpd: Opening firewall for input from 127.127.1.0 port 123 [  OK  ]
+#ntpd: Opening firewall for input from ntp.ucsd.edu port 123[  OK  ]
+#Starting ntpd:                                             [  OK  ]
+#
+
+
 
 
 
