09631370cb11bec844234d0d5a38f39fd12d15f7
Simon C Blyth <simoncblyth@gmail.com>
2007-05-31 05:04:23 +0800

    tracnav plugin setup

--- scm/trac.bash
+++ scm/trac.bash
@@ -437,6 +437,34 @@ trac-webadmin-plugin-enable(){
 }
 
 
+trac-plugin-tracnav-get(){
+
+   #  documented at 
+   # http://svn.ipd.uka.de/trac/javaparty/wiki/TracNav
+   #
+
+   cd $LOCAL_BASE/trac
+   [ -d "plugins" ] || mkdir -p plugins
+   cd plugins
+    
+   svn co http://svn.ipd.uka.de/repos/javaparty/JP/trac/plugins/tracnav/
+   cd tracnav
+
+}
+
+trac-plugin-tracnav-install(){
+
+    cd $LOCAL_BASE/trac/plugins || ( echo error no plugins folder && return 1 ) 
+    cd tracnav
+    python setup.py install 
+  
+}
+
+
+
+
+
+
 
 trac-pygments-plugin-get(){
 
