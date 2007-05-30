f3855eb2e69eef11a0dbd57e04ac8bb63324b36c
Simon C Blyth <simoncblyth@gmail.com>
2007-05-30 11:03:55 +0800

    traj handling

---
+++ macros/trajectories.C
@@ -0,0 +1,23 @@
+
+{
+
+  TFile* f = new TFile("dummy.root","read");
+  dywTrajectories* trajs = new dywTrajectories();
+  TTree* traj_t = (TTree*)f->Get("trajectory_tree");
+
+  //tree->SetBranchAddress("dayabay_MC_track_output",&trk);  
+  TBranch* branch = traj_t->GetBranch( "mc_trajectories");
+  branch->SetAddress( &trajs );
+
+  Int_t nevent = traj_t->GetEntries();
+  cout << " nent " << nent << endl ;
+
+  for( Int_t ievent=0 ; ievent < nevent ; ++ievent ){  
+     branch->GetEntry(ievent);
+	 vector<dywTrajectory> vt = trajs->traj ;
+	 for (Int_t it=0;it<vt.size() ;it++) {
+	    cout << it << " " << vt[it].particleName << endl ;
+     }
+  }
+
+}
