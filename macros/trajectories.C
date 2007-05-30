949dd748f1bdb98bc9148918fa5dc8da80d4975e
Simon C Blyth <simoncblyth@gmail.com>
2007-05-30 11:46:48 +0800

    event + traj friends... starting point

--- macros/trajectories.C
+++ macros/trajectories.C
@@ -4,29 +4,40 @@
  gROOT->ProcessLine( Form(".x %s/DataStructure/MCEvent/cmt/load.C", gSystem->Getenv("DYW") ));
 
 
-  TFile* f = new TFile("dummy.root","read");
-  TTree* traj_t = (TTree*)f->Get("trajectory_tree");
-  TBranch* branch = traj_t->GetBranch( "mc_trajectories");
-  
+  TFile* f = new TFile("10.root","read");
+  TTree* event_tree = (TTree*)f->Get("event_tree");
+  TTree* trajectory_tree = (TTree*)f->Get("trajectory_tree");
+  event_tree->AddFriend("trajectory_tree");
+
+  // hmm how to do this with friends ???
+
+  //TBranch* branch = trajectory_tree->GetBranch( "mc_trajectories");
+  TBranch* branch = event_tree->GetBranch( "mc_trajectories");
+
   dywTrajectories* trajs = new dywTrajectories();
   branch->SetAddress( &trajs );
+  
 
-  Int_t nevent = traj_t->GetEntries();
+  Int_t nevent = event_tree->GetEntries();
   cout << " nevent " << nevent << endl ;
 
   TString ax1="x" ;
   TString ax2="y" ;
-  trajectory_tree->Draw(Form("traj.point.%s:traj.point.%s",ax1.Data(),ax2.Data()),"") ;
+
+  TCanvas* c ;
 
   for( Int_t ievent=0 ; ievent < nevent ; ++ievent ){  
-  
+ 
+     c = new TCanvas ;
+ 
+     trajectory_tree->Draw(Form("traj.point.%s:traj.point.%s",ax1.Data(),ax2.Data()),"") ;
      branch->GetEntry(ievent);
 	 
 	 vector<dywTrajectory> vt = trajs->traj ;
 	 for (Int_t it=0;it<vt.size() ;it++) {
 		 
         TString name = vt[it].particleName ;
-	    cout << it << " " << name << endl ;
+	//    cout << it << " " << name << endl ;
 
         Color_t col ;
         if( name == "e+" ){ 
