6d01016db7d5f3e505a806015b18aae0f4bb19e3
Simon C Blyth <simoncblyth@gmail.com>
2007-06-02 08:11:54 +0800

    removed modpython-tracs-conf from here placing equivalent into svn.bash

--- scm/modpython.bash
+++ scm/modpython.bash
@@ -44,41 +44,6 @@ modpython-apache2-configure(){
 #PythonPath 'sys.path'
 #PythonPath "sys.path + ['/path/to/trac']"
 
-modpython-tracs-conf(){
-userfile=$1
-cat << EOC
-
-<Location /mpinfo>
-   SetHandler mod_python
-   PythonHandler mod_python.testhandler
-</Location>
-
-<Directory $APACHE2_HTDOCS/test>
-   AddHandler mod_python .py
-   PythonHandler myscript
-   PythonDebug On
-</Directory>
-
-
-<Location /tracs>
-   SetHandler mod_python
-   PythonPath "sys.path + ['/usr/local/python/Python-2.5.1/lib/python2.5/site-packages']"
-   PythonHandler trac.web.modpython_frontend 
-   PythonOption TracEnvParentDir $SCM_FOLD/tracs
-   PythonOption TracUriRoot /tracs
-   PythonDebug On
-</Location>
-
-<LocationMatch "/tracs/[^/]+/login">
-    AuthType Basic
-    AuthName "svn-tracs"
-    AuthUserFile $userfile
-    Require valid-user
-</LocationMatch>
-
-EOC
-}
-
 
 
 
