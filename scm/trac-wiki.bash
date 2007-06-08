bc4549807faf4909e0ca45b2c06b5e85dd82b9ff
Simon C Blyth <simoncblyth@gmail.com>
2007-06-08 11:30:48 +0800

    path tweaking

--- scm/trac-wiki.bash
+++ scm/trac-wiki.bash
@@ -14,7 +14,7 @@ trac-wiki-backup(){
   cd $SCM_FOLD
   [ -d backup ] || ( sudo mkdir backup && sudo chown $USER backup )
   
-  dir="backup/tracs/$SCM_HOST/$name/wiki"
+  dir="backup/tracwikis/$SCM_HOST/$name/wiki"
   [ -d $dir ] || ( mkdir -p $dir || ( echo abort && return 1 ))
   cd $dir  
   python $HOME/$SCM_BASE/xmlrpc-wiki-backup.py $name $*
@@ -28,7 +28,7 @@ trac-wiki-restore(){
   shift
   
   cd $SCM_FOLD
-  dir="backup/tracs/$SCM_HOST/$name/wiki"
+  dir="backup/tracwikis/$SCM_HOST/$name/wiki"
   [ -d $dir ] || ( echo abort ... must backup before can restore  && return 1 )
   cd $dir
   python $HOME/$SCM_BASE/xmlrpc-wiki-restore.py $name $*
