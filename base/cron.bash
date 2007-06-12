c30bf24550f39d16da567630cf4f9750d93dbe4e
Simon C Blyth <simoncblyth@gmail.com>
2007-06-12 10:05:22 +0800

    cleaning

--- base/cron.bash
+++ base/cron.bash
@@ -82,6 +82,80 @@ EOT
 
 
 
+
+
+
+
+
+cron-test(){
+
+    #
+    # defaults to three minutes from now
+    # note limitation : assumes not about to go into another hr, day, month etc..
+    #
+
+    local user=${1:-root}
+
+    local       def_minute=$(( $(date +"%M") + 3 ))   
+    local         def_hour=$(date +"%H")
+    local def_day_of_month=$(date +"%d")
+    local        def_month=$(date +"%m")
+
+    local       minute=${1:-$def_minute}
+    local         hour=${2:-$def_hour}
+    local day_of_month=${3:-$def_day_of_month}
+    local        month=${4:-$def_month}
+    local  day_of_week="*"
+
+
+    local cmd="(. $ENV_BASE/$ENV_BASE.bash ; env ; type scm-backup-all ; scm-backup-all ) > /tmp/crontest 2>&1"  
+   
+    #
+    # seems must export variables for them to be visible on the above cron cmdline 
+    #
+    # the sudo environment is a little funny ... hence this test
+    #
+
+    tmp=/tmp/$$crontab
+    cat << EOF > $tmp
+#
+SHELL=/bin/bash
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+MAILTO=blyth@hep1.phys.ntu.edu.tw
+HOME=$HOME
+#
+# +---------------- minute (0 - 59)
+# |  +------------- hour (0 - 23)
+# |  |  +---------- day of month (1 - 31)
+# |  |  |  +------- month (1 - 12)
+# |  |  |  |  +---- day of week (0 - 7) (Sunday=0 or 7)
+# |  |  |  |  |
+# *  *  *  *  *  command to be executed
+#
+$minute $hour $day_of_month $month $day_of_week $cmd 
+#
+EOF
+
+
+     reply=$(sudo crontab -u $user -l 2>&1)      ## redirection sending stderr onto stdout
+     if ([ "$reply" == "no crontab for $user" ] || [ "$reply" == "crontab: no crontab for $user" ])  then
+          echo =========== initializing crontab for $user to $tmp 
+          cat $tmp 
+          sudo crontab -u $user $tmp
+     else
+          echo cannot proceed as a crontab for user $user exists already, do  cron-delete $user / cron-list $user  first 
+     fi
+
+}
+
+
+
+
+
+
+
+
+
 cron-setup-shutdown(){
 
       crondir=/usr/local/cron
@@ -130,71 +204,6 @@ fi
 
 
 
-
-
-
-cron-test(){
-
-## defaults to three minutes from now
-## note limitation : assumes not about to go into another hr, day, month etc..
-
-
-    local user=${1:-root}
-
-    local       def_minute=$(( $(date +"%M") + 3 ))   
-    local         def_hour=$(date +"%H")
-    local def_day_of_month=$(date +"%d")
-    local        def_month=$(date +"%m")
-
-    local       minute=${1:-$def_minute}
-    local         hour=${2:-$def_hour}
-    local day_of_month=${3:-$def_day_of_month}
-    local        month=${4:-$def_month}
-    local  day_of_week="*"
-
-
-    if [ "$NODE_TAG" == "G" ]; then
-        cmd="$(which apachectl) configtest > /tmp/crontest 2>&1"
-    else
-        cmd="(. $ENV_BASE/$ENV_BASE.bash ; env ; type scm-backup-all ; scm-backup-all ) > /tmp/crontest 2>&1"  
-    fi
-
-    ## seems must export variables for them to be visible on the above cron cmdline 
-    ##
-    ## the sudo environment is a little funny ... hence this test
-
-
-    tmp=/tmp/$$crontab
-    cat << EOF > $tmp
-#
-SHELL=/bin/bash
-PATH=/sbin:/bin:/usr/sbin:/usr/bin
-MAILTO=blyth@hep1.phys.ntu.edu.tw
-HOME=$HOME
-#
-# +---------------- minute (0 - 59)
-# |  +------------- hour (0 - 23)
-# |  |  +---------- day of month (1 - 31)
-# |  |  |  +------- month (1 - 12)
-# |  |  |  |  +---- day of week (0 - 7) (Sunday=0 or 7)
-# |  |  |  |  |
-# *  *  *  *  *  command to be executed
-#
-$minute $hour $day_of_month $month $day_of_week $cmd 
-#
-EOF
-
-
-     reply=$(sudo crontab -u $user -l 2>&1)      ## redirection sending stderr onto stdout
-     if ([ "$reply" == "no crontab for $user" ] || [ "$reply" == "crontab: no crontab for $user" ])  then
-          echo =========== initializing crontab for $user to $tmp 
-          cat $tmp 
-          sudo crontab -u $user $tmp
-     else
-          echo cannot proceed as a crontab for user $user exists already, do  cron-delete $user / cron-list $user  first 
-     fi
-
-}
 
 
 
