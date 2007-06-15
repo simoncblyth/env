c92d1e0c623b4a53208f3332fdc98fd09b469996
Simon C Blyth <simoncblyth@gmail.com>
2007-06-15 10:30:16 +0800

    generic batch submission

--- base/batch.bash
+++ base/batch.bash
@@ -19,6 +19,48 @@ hello(){
 }
 
 
+batch-timestamp(){
+	perl -MPOSIX -e  "print strftime( '%Y%m%d-%H%M%S' , localtime(time()) );" 
+}
+
+batch-fmtime(){
+	perl -MPOSIX -e  "print strftime( '%Y%m%d-%H%M%S' , localtime($1) );" 
+}
+
+
+batch-logged-task(){
+
+   ## runs the arguments passed sandwiched between xml logging information
+
+   local name=$1
+
+   printf "<task name=\"%s\"  >\n" $name 
+   printf "<pwd>%s</pwd>\n" $(pwd)
+   printf "<dirname>%s</dirname>\n" $(dirname $(pwd))
+   printf "<basename>%s</basename>\n" $(basename $(pwd))
+   
+   printf "<args>\n" 
+   for arg in "$@"
+   do
+	  printf "<arg>%s</arg>\n" $arg   
+   done
+   printf "</args>\n" 
+   
+   local start=$(date +'%s')
+   printf "<start stamp=\"%d\" time=\"%s\" />\n" $start $(batch-fmtime $start)  
+   printf "<body>\n" 
+   
+   eval "$@"
+   
+   printf "</body>\n" 
+   local finish=$(date +'%s')
+   printf "<finish stamp=\"%d\" time=\"%s\" />\n" $finish $(batch-fmtime $finish)  
+   printf "</task>\n" 
+}
+
+
+
+
 
 batch-script(){
 
