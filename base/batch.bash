8babcf84f2e78bc6de26cf73bbbb3b38a3efb3ef
Simon C Blyth <simoncblyth@gmail.com>
2007-06-15 10:19:41 +0800

    generic batch submission

---
+++ base/batch.bash
@@ -0,0 +1,32 @@
+
+
+batch-script-write(){
+
+   local path=$1
+   local func=$2
+   
+   batch-script > $func.sh    
+}
+
+
+
+batch-script(){
+
+   local path=$1
+   local func=$2
+   
+   [ "X$path" == "X" ] && echo need relative path that describes the task   && return 1
+   [ "X$func" == "X" ] && echo need func to perform                         && return 1 
+   
+   shift
+   shift
+
+#   -l means, behave like a login script
+cat << EOC
+#!/bin/bash -l
+$func $*
+
+EOC
+
+}
+
