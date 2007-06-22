2c0cbacefe55b9223c5bcd5d2120798d51ef6905
Simon C Blyth <simoncblyth@gmail.com>
2007-06-22 10:09:32 +0800

    seed checking

--- seed/seed.bash
+++ seed/seed.bash
@@ -5,8 +5,9 @@ seed(){
    local iwd=$(pwd)
    cd $HOME/$ENV_BASE/seed
    
-   g++ -o seed seed.cc
-   ./seed
+   local exe=${CMTBIN}-seed
+   [ -f "$exe" ] || g++ -o $exe seed.cc
+   ./$exe
 
    cd $iwd
 }
