07f1c3368d37b0d67fa0b7076cb7a5d8b2076bbb
Simon C Blyth <simoncblyth@gmail.com>
2007-06-22 15:41:31 +0800

    pymysql aborted install

--- scm/pymysql.bash
+++ scm/pymysql.bash
@@ -1,4 +1,12 @@
 
+#
+# http://www.sqlalchemy.org/
+#
+#
+#
+#
+
+
 PYMYSQL_NAME=MySQL-python-1.2.2
 
 pymysql-get(){
@@ -18,33 +26,45 @@ pymysql-get(){
 }
 
 pymysql-install(){
+  
+   local nam=$PYMYSQL_NAME
+   local nik=pymysql
+   cd $LOCAL_BASE/$nik/build/$nam
 
-   # http://initd.org/pub/software/pysqlite/doc/install-source.html
-   # NB the default python in path is too old: 2.2
-
-   nam=$PYSQLITE_NAME
-   cd $LOCAL_BASE/pysqlite/build/$nam
-
-   perl -pi -e "s|^(include_dirs=)(.*)$|\$1$SQLITE_HOME/include|g" setup.cfg
-   perl -pi -e "s|^(library_dirs=)(.*)$|\$1$SQLITE_HOME/lib|g"     setup.cfg
-
-  # doesnt work with this version of python 
-  #   /usr/bin/python2.4 setup.py build
-
-   $PYTHON_HOME/bin/python setup.py build
-   $PYTHON_HOME/bin/python setup.py install 
-
-   # installs as a site-package into the installed python at
-   #      $PYTHON_HOME/lib/python2.5/site-packages/pysqlite2
+   local ipath=$PATH
+   
    #
+   # need MAMPs mysql_config to be in the path 
+   # /Applications/MAMP/Library/bin
+   # NB will need to rebuild on updating MAMP ... imminent 
+   #
+   
+   export PATH=/Applications/MAMP/Library/bin:$PATH
+   $PYTHON_HOME/bin/python setup.py build
+   #sudo $PYTHON_HOME/bin/python setup.py install 
+   export PATH=$ipath  
+#
+# running build_ext
+# building '_mysql' extension
+# creating build/temp.macosx-10.4-ppc-2.5
+# gcc -fno-strict-aliasing -Wno-long-double -no-cpp-precomp -mno-fused-madd -DNDEBUG -g -O3 -Wall -Wstrict-prototypes -Dversion_info=(1,2,2,'final',0) -D__version__=1.2.2 -I/Applications/MAMP/Library/include/mysql -I/usr/local/python/Python-2.5.1/include/python2.5 -c _mysql.c -o build/temp.macosx-10.4-ppc-2.5/_mysql.o -fno-omit-frame-pointer
+# _mysql.c:35:23: error: my_config.h: No such file or directory
+# _mysql.c:40:19: error: mysql.h: No such file or directory
+# _mysql.c:41:26: error: mysqld_error.h: No such file or directory
+# _mysql.c:42:20: error: errmsg.h: No such file or directory
+# _mysql.c:78: error: parse error before 'MYSQL'
+#
+#
+#  MAMP does not come with headers !!!
+# [g4pb:/usr/local/pymysql/build/MySQL-python-1.2.2] blyth$ find /Applications/newMAMP/ -name '*.h'
+# [g4pb:/usr/local/pymysql/build/MySQL-python-1.2.2] blyth$ find /Applications/MAMP/ -name '*.h'
+#
+
 }
 
 pymysql-test(){
 
-    # need to run the test from a directory other than the build directory
-    #  otherwise it fails with
-    #   ImportError: No module named _sqlite
-    cd 
+       cd 
     $PYTHON_HOME/bin/python << EOP
 from pysqlite2 import test
 test.test()
