10550293a62a4b2b373923d2c051898009dacbf8
Simon C Blyth <simoncblyth@gmail.com>
2007-06-22 15:43:58 +0800

    anno

--- base/file.bash
+++ base/file.bash
@@ -17,7 +17,9 @@ file-dirlist(){
 
 
 file-tgz-topdir(){
-   
+   #
+   # return the top directories present in a tarball, if only one such directory return with success status, otherwise return with error status
+   #
    local tgz=${1:-dummy}
    [ -f "$tgz"  ] ||  return 1 
    tar -ztf  $tgz | perl -n -e 'BEGIN{ @n=(); }; m|(.*?)/.*| && do { push(@n,$1) if(grep($1 eq $_,@n)==0); } ; END{ print "@n " ; exit(1) if($#n + 1 != 1) ;} '
