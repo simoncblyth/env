42ac868a35011f0a708bd11de80af0dcfc4160d9
Simon C Blyth <simoncblyth@gmail.com>
2007-06-29 07:43:46 +0800

    annotation

--- scm/trac-macro-latexformulamacro.sh
+++ scm/trac-macro-latexformulamacro.sh
@@ -1,27 +1,47 @@
 #!/bin/bash 
+#
+#     http://trac-hacks.org/wiki/LatexFormulaMacro 
+#  allows writing latex formulas in wiki text, that get converted to images 
+#
+#  install with the below three steps 
+#
+#    1) . ~/$ENV_BASE/scm/trac-macro-latexformulamacro.sh 
+#    2) trac-macro-latexformulamacro-get 
+#    3) sudo -u $APACHE2_USER $HOME/$ENV_BASE/scm/trac-macro-latexformulamacro.sh env
+#
+#
+
+trac-macro-latexformulamacro-get(){
+ 
+   cd $LOCAL_BASE/trac
+   [ -d "wiki-macros" ] || mkdir -p wiki-macros
+   cd wiki-macros
+
+   local macro=latexformulamacro
+   mkdir -p $macro
+   svn co http://trac-hacks.org/svn/$macro/0.9/ $macro
+
+}
+
+
 
 trac-macro-latexformulamacro-install(){ 
 
-   ## http://trac-hacks.org/wiki/LatexFormulaMacro
-   ## invoke with sudo -u $APACHE2_USER $HOME/$ENV_BASE/scm/trac-macro-latexformulamacro.sh
- 
+
    local name=${1:-dummy}
+   local macro=latexformulamacro
+   
    [ "$name" == "dummy" ] && echo must provide the name of the environment && return 1
    
    local fold=$SCM_FOLD/tracs/$name
    [ -d "$fold" ] || ( echo trac-macro-latexformulamacro-install error no folder $fold && exit 1 )
      
    cd $fold/wiki-macros  
-   if [ -f "formula.py" ]; then
-     svn up formula.py
-   else  
-     svn co http://trac-hacks.org/svn/latexformulamacro/0.9/formula.py
-   fi
+   cp -f $LOCAL_BASE/trac/wiki-macros/$macro/formula.py .  
    
    local tmp=$SCM_FOLD/tmp
    mkdir -p $tmp 
-
-   ini-edit $fold/conf/trac.ini latex:temp_dir:$tmp latex:fleqn:0 latex:fleqn_width:5% 
+   $HOME/$ENV_BASE/base/ini-edit.pl $fold/conf/trac.ini latex:temp_dir:$tmp latex:fleqn:0 latex:fleqn_width:5% 
 
 }
 
