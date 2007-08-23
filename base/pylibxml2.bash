23330380589c49cf51afd6da477e39d0e5318af7
Simon C Blyth <simoncblyth@gmail.com>
2007-08-23 10:33:30 +0800

    reposition

--- base/pylibxml2.bash
+++ base/pylibxml2.bash
@@ -1,9 +1,10 @@
 
 pylibxml2-get(){
 
-    cd $LOCAL_BASE
-    mkdir libxml2
-    cd libxml2
+
+    local dir=$LOCAL_BASE/python/pylibxml2
+    mkdir -p $dir
+    cd $dir
     
     wget ftp://xmlsoft.org/libxml2/python/libxml2-python-2.6.21.tar.gz
     #wget ftp://xmlsoft.org/libxml2/python/libxml2-python-2.6.15.tar.gz
