cf5fae3df8f51ca50123da33f561627deb62c5b9
Simon C Blyth <simoncblyth@gmail.com>
2007-08-23 10:37:44 +0800

    standardize

--- base/pylibxml2.bash
+++ base/pylibxml2.bash
@@ -3,13 +3,24 @@ pylibxml2-get(){
 
 
     local dir=$LOCAL_BASE/python/pylibxml2
+
+   # 
+   # http://xmlsoft.org/XSLT/python.html 
+   #
+   # on hfag xsltproc --version indicates  2.6.16 is present
+   #  ... but no matching distro
+   #         nearest are  2.6.15 and 2.6.21  
+   #
+   
+    local name=libxml2-python-2.6.21
     mkdir -p $dir
     cd $dir
     
-    wget ftp://xmlsoft.org/libxml2/python/libxml2-python-2.6.21.tar.gz
-    #wget ftp://xmlsoft.org/libxml2/python/libxml2-python-2.6.15.tar.gz
-
-
+    local tgz=$name.tar.gz
+    local url= ftp://xmlsoft.org/libxml2/python/$tgz
+   
+    test -f $tgz || curl -o $tgz $url
+    test -d $name || tar zxvf $tgz 
 
 
 }
