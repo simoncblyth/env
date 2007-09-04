0b8b8a9cb2e0cf5e4d3f62705f83dfb97b7ce2d2
Simon C Blyth <simoncblyth@gmail.com>
2007-09-04 09:08:47 +0800

    dayabay sw via the gaudi framework environment setup

---
+++ dyb/dyb.bash
@@ -0,0 +1,13 @@
+
+DYB_BASE=$ENV_BASE/dyb
+export DYB_HOME=$HOME/$DYB_BASE
+
+[ -r $DYB_HOME/gaudi/gaudi.bash ]   && .  $DYB_HOME/gaudi/gaudi.bash 
+[ -r $DYB_HOME/lcgcmt/lcgcmt.bash ] && .  $DYB_HOME/lcgcmt/lcgcmt.bash
+[ -r $DYB_HOME/external/external.bash ] && .  $DYB_HOME/external/external.bash
+
+
+
+
+
+
