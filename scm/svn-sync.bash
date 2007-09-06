d208a52008f0706f3d762fc343749959d38c1eb5
Simon C Blyth <simoncblyth@gmail.com>
2007-09-06 12:11:03 +0800

    more visible

--- scm/svn-sync.bash
+++ scm/svn-sync.bash
@@ -1,13 +1,17 @@
 
 svn-sync-init(){
 
-    svnsync initialize $SCM_URL/repos/dybsvn $DYBSVN --username $SVN_SYNC_USER --password $SVN_SYNC_PASS
+    local cmd="svnsync initialize $SCM_URL/repos/dybsvn $DYBSVN --username $SVN_SYNC_USER --password $SVN_SYNC_PASS"
+    echo $cmd
+    eval $cmd
 
 }
 
 
 svn-sync-synchronize(){
 
-    svnsync synchronize $SCM_URL/repos/dybsvn --username $SVN_SYNC_USER --password $SVN_SYNC_PASS
+    local cmd="svnsync synchronize $SCM_URL/repos/dybsvn --username $SVN_SYNC_USER --password $SVN_SYNC_PASS"
+    echo $cmd
+    eval $cmd
                      
 }
