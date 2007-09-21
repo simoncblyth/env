b9f9289b11a15aa824ecac574c3eff236b2b190e
Simon C Blyth <simoncblyth@gmail.com>
2007-09-22 07:25:17 +0800

    looking at millions of fAke hits

--- root/fake_hit_data_treeplot.C
+++ root/fake_hit_data_treeplot.C
@@ -11,11 +11,37 @@
     event_tree->Draw("fAke.x_pmt:fAke.y_pmt:fAke.z_pmt","fAke.id==0");
     event_tree->Draw("fAke.x_pmt:fAke.y_pmt:fAke.z_pmt","fAke.id==1");
    
+    // huh everything is at id zero ???
     event_tree->Draw("fAke.t","fAke.id==0");
     event_tree->Draw("fAke.t","fAke.id==1");    
    
-    event_tree->Draw("fAke.wl:fAke.t");
-   
+      
+    
+    event_tree->Draw("fAke.wl:fAke.t");        // structure of 17 peaks in 370ns  ... offset of about 12 ns , spacing about 21 ns
+    event_tree->Draw("int(fAke.t - 12)%21");   //   c  299.792458 mm/ns
+                                               
+    event_tree->Draw("fAke.wl:fAke.t","abs(fAke.z_pmt)<2000")
+    event_tree->Draw("fAke.wl:fAke.t","abs(fAke.z_pmt)>2000")   // bulk of time structure coming from photons going out the top and bottom ... reflector effect ?   
+    
+    
+    event_tree->Draw("fAke.y_pmt:fAke.x_pmt","abs(fAke.z_pmt) < 2005 " )   // circle
+    event_tree->Draw("fAke.y_pmt:fAke.x_pmt","abs(fAke.z_pmt) < 2006 " )   // disc ... demo the height in z
+     
+         
+     // peaks at 2000 and 2001 mm ... approx 10x more at 2001 ??? thho little peak effect ?      
+     event_tree->Draw("TMath::Sqrt(fAke.y_pmt*fAke.y_pmt+fAke.x_pmt*fAke.x_pmt)","abs(fAke.z_pmt) < 2005 " ) 
+           
+    
+    // 1000000 eV    
+    event_tree->Draw("TMath::Sqrt(fAke.px*fAke.px+fAke.py*fAke.py+fAke.pz*fAke.pz)" ) 
+    event_tree->Draw("TMath::Sqrt(fAke.x_pmt*fAke.x_pmt+fAke.y_pmt*fAke.y_pmt+fAke.z_pmt*fAke.z_pmt)" )     
+        
+    // position of fake hit defines a point vector from origin... find the costheta between the momentum direction and the point vector
+    // to see how much outgoing cf ingoing 
+    // ... at little over 50% in peak at 1 , the remainder mostly flat from -1 up to 1 with blip at 0     
+    event_tree->Draw("(fAke.px*fAke.x_pmt+fAke.py*fAke.y_pmt+fAke.pz*fAke.z_pmt) / (1000000* TMath::Sqrt(fAke.x_pmt*fAke.x_pmt+fAke.y_pmt*fAke.y_pmt+fAke.z_pmt*fAke.z_pmt))" )    
+    
+    
     event_tree->Draw("GetNofFakeHits(-1)"); // fake hits in all FakeSD
     event_tree->Draw("GetNofFakeHits(0)");  // fake hits in FakeSD 0
     
