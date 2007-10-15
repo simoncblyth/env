e572b2bf1455c4efe3320cb697587a9b97322324
Simon C Blyth <simoncblyth@gmail.com>
2007-10-16 06:30:25 +0800

    gaudi follow along 2 ... now with dybinst

--- dyb/core.bash
+++ dyb/core.bash
@@ -1,6 +1,10 @@
 
 
 
+
+
+
+
 core-env(){
 
   cd $LOCAL_BASE
--- dyb/dyb.bash
+++ dyb/dyb.bash
@@ -9,6 +9,19 @@ export DYB_HOME=$HOME/$DYB_BASE
 [ -r $DYB_HOME/external/external.bash ] && .  $DYB_HOME/external/external.bash
 
 
+dyb-get(){
+   mkdir -p $LOCAL_BASE/dyb
+   cd $LOCAL_BASE/dyb
+   svn export http://dayabay.ihep.ac.cn/svn/dybsvn/installation/trunk/dybinst/dybinst
+}
+
+dyb-install(){
+  cd $LOCAL_BASE/dyb
+  ./dybinst all 
+}
+
+
+
 core(){     [ -r $DYB_HOME/core.bash ]          && .  $DYB_HOME/core.bash ; }
 lcgcmt(){   [ -r $DYB_HOME/lcgcmt/lcgcmt.bash ] && .  $DYB_HOME/lcgcmt/lcgcmt.bash ; }
 
