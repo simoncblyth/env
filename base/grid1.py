cdbe77a9fdec153931f1e22069af1106a108a4a7
Simon C Blyth <simoncblyth@gmail.com>
2007-10-17 07:52:20 +0800

    attempt poor mans monitoring of grid1 with ssh and pxssh (pexpect)

---
+++ base/grid1.py
@@ -0,0 +1,26 @@
+
+import pxssh
+import os
+
+user=os.environ['USER']
+#prompt='\[%s@grid1 %s\]\$ ' % ( user , user )  
+prompt=r'\[.*\]\$$\Z' 
+
+print "prompt...%s..." % prompt 
+
+s=pxssh.pxssh()
+if not s.login("G1",  user , password="wrong" , terminal_type='ansi', original_prompts=r"\[.*\]\$ ", login_timeout=3 ):
+    print "SSH session failed on login."
+    print str(s)
+else:
+    print "SSH session login successful"
+    s.sendline ('ls -l')
+    print s
+    s.prompt()         # match the prompt
+    print s.before     # everything before the prompt.
+    s.logout()
+    print s
+                                                               
+                                                               
+
+
