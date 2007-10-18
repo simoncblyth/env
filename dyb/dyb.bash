0ddfc1548bbbf3bd23653db26eb9d02f4fd83735
Simon C Blyth <simoncblyth@gmail.com>
2007-10-18 08:59:17 +0800

    tidy up dyb area to avoid confusion

--- dyb/dyb.bash
+++ dyb/dyb.bash
@@ -3,11 +3,6 @@ DYB_BASE=$ENV_BASE/dyb
 export DYB_HOME=$HOME/$DYB_BASE
 export DYB=$LOCAL_BASE/dyb
 
-[ -r $DYB_HOME/core.bash ]   && .  $DYB_HOME/core.bash 
-
-[ -r $DYB_HOME/gaudi/gaudi.bash ]   && .  $DYB_HOME/gaudi/gaudi.bash 
-[ -r $DYB_HOME/lcgcmt/lcgcmt.bash ] && .  $DYB_HOME/lcgcmt/lcgcmt.bash
-[ -r $DYB_HOME/external/external.bash ] && .  $DYB_HOME/external/external.bash
 
 
 dyb-get(){
