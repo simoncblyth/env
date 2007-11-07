a6d0e737645ffa66d0ebf7b7ae355881a39cad5a
Simon C Blyth <simoncblyth@gmail.com>
2007-11-07 10:19:31 +0800

    no brackets in echos

--- dyb/dyb.bash
+++ dyb/dyb.bash
@@ -7,19 +7,15 @@ dyb(){      [ -r $DYB_HOME/dyb.bash ]           && . $DYB_HOME/dyb.bash ; }
 
 dyb-get(){
    local branch=${1:-trunk}
-    
- 
    mkdir -p $LOCAL_BASE/dyb
    cd $LOCAL_BASE/dyb
-   
-   
    
    if [ "X$branch" == "Xtrunk" ]; then 
      url=http://dayabay.ihep.ac.cn/svn/dybsvn/installation/trunk/dybinst/dybinst
    else
      url=http://dayabay.ihep.ac.cn/svn/dybsvn/installation/branches/inst-rozz-$branch/dybinst/dybinst
    fi
-   echo === dyb-get branch $branch url $url (see https://wiki.bnl.gov/dayabay/index.php?title=Offline_Release_rozz-0.0.4 )
+   echo === dyb-get branch $branch url $url == see https://wiki.bnl.gov/dayabay/index.php?title=Offline_Release_rozz-0.0.4 ==
    svn export $url
     
 }
