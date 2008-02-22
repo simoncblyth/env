ce97437e3535e961559c5148e548035221b08c29
Simon C Blyth <simoncblyth@gmail.com>
2008-02-22 09:41:22 +0800

    anno

--- base/iterm-ssh.sh
+++ base/iterm-ssh.sh
@@ -1,6 +1,13 @@
 
-
+##
 ## how iTerm sshs with the quick keys 
+## the agent-info file is created by the bash function
+##     ssh--agent-start 
+## that needs to be invoked following reboots 
+## this starts the agent and sets up its keys
+##
+## http://dayabay.phys.ntu.edu.tw/tracs/env/wiki/PasswordLessSSH 
+##
 source ~/.ssh-agent-info-G
 ssh -Y $1
 
