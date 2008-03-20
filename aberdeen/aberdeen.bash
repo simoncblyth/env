2dfac8bad2680cca43101d6420eaec38e6c66a22
Simon C Blyth <simoncblyth@gmail.com>
2008-03-21 05:34:38 +0800

    standardize top level functions to end with "-"

--- aberdeen/aberdeen.bash
+++ aberdeen/aberdeen.bash
@@ -6,15 +6,15 @@ aberdeen_iwd=$(pwd)
 cd $HOME/$ABERDEEN_BASE
 
 roody-(){ [ -r $HOME/$ABERDEEN_BASE/roody.bash ] && . $HOME/$ABERDEEN_BASE/roody.bash ; }
-midas(){ [ -r $HOME/$ABERDEEN_BASE/midas.bash ] && . $HOME/$ABERDEEN_BASE/midas.bash ; }
-rome(){  [ -r $HOME/$ABERDEEN_BASE/rome.bash ]  && . $HOME/$ABERDEEN_BASE/rome.bash ; }
-abd(){  [ -r $HOME/$ABERDEEN_BASE/abd.bash ]  && . $HOME/$ABERDEEN_BASE/abd.bash ; }
+midas-(){ [ -r $HOME/$ABERDEEN_BASE/midas.bash ] && . $HOME/$ABERDEEN_BASE/midas.bash ; }
+rome-(){  [ -r $HOME/$ABERDEEN_BASE/rome.bash ]  && . $HOME/$ABERDEEN_BASE/rome.bash ; }
+abd-(){  [ -r $HOME/$ABERDEEN_BASE/abd.bash ]  && . $HOME/$ABERDEEN_BASE/abd.bash ; }
 
 
 roody-
-rome
-abd
-midas
+rome-
+abd-
+midas-
 
 
 roody-env
