111021df24d34cb6ea115bf6ac913db0f0c46f86
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2008-04-28 17:00:13 +0800

    macro used to test GeoMap.C r929, they should work well

--- aberdeen/root/Display.C
+++ aberdeen/root/Display.C
@@ -16,18 +16,23 @@
 
   //SetPMTHit(Int_t pmt_no,Double_t hitpattern size)
   // 0 <= hitpattern size >= 5.
-  gm->SetPMTHit( 1 , 3 );
-  gm->SetPMTHit( 2 , 5 );
-  gm->SetPMTHit( 3,  1 );
-  gm->Refresh("World_1");      //refresh the display
-
+  gm->SetPMTHit( 1 , 3. );
+  gm->SetPMTHit( 2 , 5. );
+  gm->SetPMTHit( 3,  1. );
 
   gm->ResetPMT();
-//  gm->SetPMTHit( 5 , 3 );
-//  gm->SetPMTHit( 6 , 5 );
-//  gm->SetPMTHit( 7,  1 ); 
-  gm->Refresh("World_1");
 
+  gm->SetPMTHit( 5 , 3. );
+  gm->SetPMTHit( 6 , 5. );
+  gm->SetPMTHit( 7 , 1. );
+  gm->SetPMTHit(15 , 4. );
+
+  gm->ResetPMT();
+
+  gm->SetPMTHit( 8 , 2.);
+  gm->SetPMTHit( 1 , 5.);
+  gm->SetPMTHit( 9 , 0.7);
+  
 }
 
 
