f450fb28dc5a0199f6d633716f4af6bcae52f84a
Simon C Blyth <simoncblyth@gmail.com>
2008-05-13 11:44:56 +0800

    examples of interactive GUI development ... ex3 looks rather a good place to start

---
+++ root/gui/ex2/guitest0.C
@@ -0,0 +1,35 @@
+//
+//   use from interactive root with 
+//      .x guitest0.C
+//
+//  example from
+//    http://root.cern.ch/root/roottalk/roottalk99/1114.html
+//
+//    
+//   it fails 
+//
+
+#include <iostream.h>
+ 
+int guitest0(){
+ 
+   TGMainFrame *rMFrm = new TGMainFrame(gClient->GetRoot(), 500, 500);
+ 
+   TGCompositeFrame *rCFrm =
+     new TGCompositeFrame(rMFrm, 50, 50, kHorizontalFrame|kSunkenFrame );
+   TGButton *rPBut =
+     new TGPictureButton( rMFrm, gClient->GetPicture("beeravatar.xpm"),
+     "rAction();", 1);
+ 
+   rPBut->Associate(rMFrm);
+ 
+   rCFrm->AddFrame(rPBut, new TGLayoutHints(kLHintsExpandX|kLHintsExpandY));
+   rMFrm->AddFrame(rCFrm, new TGLayoutHints(kLHintsExpandX|kLHintsExpandY));
+   rMFrm->SetWindowName("First Rado's ROOT GUI");
+   rMFrm->MapSubwindows();
+   rMFrm->Resize(rMFrm->GetDefaultSize());
+   rMFrm->MapWindow();
+ 
+   return 0;
+}
+
