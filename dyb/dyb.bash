fac5a400454d787fc668f3efd732ed56e2013ac9
Simon C Blyth <simoncblyth@gmail.com>
2008-05-21 03:19:30 +0800

    defaults for DYB_VERSION and DYB_OPTION

--- dyb/dyb.bash
+++ dyb/dyb.bash
@@ -1,55 +1,86 @@
 
 
+dyb-usage(){
+  
+cat << EOU     
+
+  Defining this needed in common to the below ...
+
+   dybi : installation 
+   dybr : run time environment setup 
+   dybt : testing
+   dybx : xecution 
+ 
+EOU
+
+
+}
+
+
 dyb-env(){
 
-DYB_BASE=dyb
-export DYB_HOME=$ENV_HOME/$DYB_BASE
+   DYB_BASE=dyb
+   export DYB_HOME=$ENV_HOME/$DYB_BASE
 
-## formerly dyb-version
+   dyb-params
 
-  # note, sensitivity to preset DYB_VERSION ... overrides the below setting
+   export DYB_FOLDER=$LOCAL_BASE/dyb
+   export DYB=$DYB_FOLDER/$DYB_VERSION$DYB_OPTION 
+   export DYB_RELEASE=NuWa-$DYB_VERSION
+   export DDR=$DYB/$DYB_RELEASE 
  
- if [ "X$DYB_VERSION" == "X" ]; then
-   vname=DYB_VERSION_$NODE_TAG
-   eval DYB_VERSION=\$$vname
- else
-   [ "$DYB_DBG" == "1" ] && echo WARNING honouring a preset DYB_VERSION $DYB_VERSION     
- fi
-  
- if [ "X$DYB_OPTION" == "X" ]; then
-    DYB_OPTION=""
-   #DYB_OPTION="_dbg" 
- else
-    [ "$DYB_DBG" == "1" ] && echo WARNING honouring a preset DYB_OPTION $DYB_OPTION
- fi
- 
- export DYB_OPTION
- export DYB_VERSION
- export DYB_FOLDER=$LOCAL_BASE/dyb
- export DYB=$DYB_FOLDER/$DYB_VERSION$DYB_OPTION 
- export DYB_RELEASE=NuWa-$DYB_VERSION
+   [ "$NODE_TAG" == "C" ] && DDR=$HOME/NuWa-trunk
+   
+   
+   export DDI=$DYB/installation/$DYB_VERSION/dybinst/scripts   ## should this be fixed at trunk ?
 
- export DDR=$DYB/$DYB_RELEASE 
- 
- if [ "$NODE_TAG" == "C" ]; then
-    DDR=$HOME/NuWa-trunk
- fi
- 
- 
-  ## should this be fixed at trunk ?
- export DDI=$DYB/installation/$DYB_VERSION/dybinst/scripts
+   ## next time distinguish the options (particulary debug on or off status) via the folder name also 
 
- ## next time distinguish the options (particulary debug on or off status) via the folder name also 
-
- ##
- ## dybi : installation 
- ## dybr : run time environment setup 
- ## dybt : testing
- ## dybx : xecution 
- ##
-
-  
 }
+
+
+dyb-params(){
+
+    # 
+    # note determine DYB_VERSION/OPTION in order, 1st succeeds 
+	#      1) preset DYB_VERSION/OPTION  
+	#      2) preset DYB_VERSION/OPTION_<NODE_TAG>
+    #      3) defaults defined here
+	#
+
+   local vname
+ 
+   if [ "X$DYB_VERSION" == "X" ]; then
+      vname=DYB_VERSION_$NODE_TAG
+      eval DYB_VERSION=\$$vname
+	  if [ -z $DYB_VERSION ]; then
+		  DYB_VERSION=trunk   
+		  [ "$DYB_DBG" == "1" ] && echo WARNING no DYB_VERSION setting for NODE_TAG $NODE_TAG ... assumed default DYB_VERSION $DYB_VERSION 
+      fi 
+   else
+      [ "$DYB_DBG" == "1" ] && echo WARNING honouring a preset DYB_VERSION $DYB_VERSION     
+   fi
+  
+   if [ "X$DYB_OPTION" == "X" ]; then
+       
+	  vname=DYB_OPTION_$NODE_TAG
+      eval DYB_OPTION=\$$vname
+	  if [ -z $DYB_OPTION ]; then
+		  DYB_OPTION="_dbg"   
+		  [ "$DYB_DBG" == "1" ] && echo WARNING no DYB_OPTION setting for NODE_TAG $NODE_TAG ... assumed default DYB_OPTION $DYB_OPTION 
+      fi 
+
+   else
+       [ "$DYB_DBG" == "1" ] && echo WARNING honouring a preset DYB_OPTION $DYB_OPTION
+   fi
+ 
+   export DYB_OPTION
+   export DYB_VERSION
+
+}
+
+
+
 
 
 dyb(){
