bc03864c6329d59d5d734638aab21f9aa6c539f6
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2008-05-22 02:45:47 +0800

    gui import geo macro

---
+++ aberdeen/root/GUI_Import.C
@@ -0,0 +1,19 @@
+{
+
+  gm = new GeoMap();
+  gm->ImportVolume("WorldWithPMTs.root","World");
+  gm->GetVol("World_1")->Draw("ogle");
+  gm->SelectKeys("^.*PMT.*$");
+  gm->SelectKeys("^.*PMT.*$")->ls();
+
+
+  gm->SetVisibility("^.*$", kFALSE );  // everything invisible
+  gm->SetVisibility("PMT", kTRUE );
+  gm->SetVisibility("Tube", kTRUE );
+
+  gm->Refresh();
+  
+}
+
+
+
