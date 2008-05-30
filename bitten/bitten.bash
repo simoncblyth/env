43da4293747675d10edc8a6c73dd98043f2f22e3
Simon C Blyth <simoncblyth@gmail.com>
2008-05-31 07:00:36 +0800

    bitten installation testing on multiple machines

--- bitten/bitten.bash
+++ bitten/bitten.bash
@@ -7,15 +7,27 @@ cat << EOU
    \$BITTEN_ENV  : $BITTEN_ENV
 
    bitten-get   :   svn co/up of trunk is recommended, rather than releases 
-
+      ## on hfag ... svn: SSL is not supported  with /data/usr/local/svn/subversion-1.4.0/bin/svn
+      ##  BUT the chekout seems OK
 
    bitten-install :
-      ## Installed /Library/Python/2.5/site-packages/Bitten-0.6dev_r547-py2.5.egg
-
-    
+      ##  g4pb: 
+      ##       Installed /Library/Python/2.5/site-packages/Bitten-0.6dev_r547-py2.5.egg
+      ##
+      ##  hfag:  use "SUDO= bitten-install" 
+      ##       Installed /data/usr/local/python/Python-2.5.1/lib/python2.5/site-packages/Bitten-0.6dev_r547-py2.5.egg    
+      ##
+   
    bitten-test :
-     ##  Ran 202 tests in 22.584s  FAILED (errors=13)
-     ##  failures from lack of figleaf / clearsilver ...
+      ##   g4pb:
+      ##           Ran 202 tests in 22.584s  FAILED (errors=13)
+      ##           failures from lack of figleaf / clearsilver ...
+      ##
+      ##    hfag:
+      ##          after excluding "report"
+      ##         Ran 193 tests in 11.074s  FAILED (errors=64)  
+      ##
+      ##
 
 
     bitten-trac-enable  <name>  :  ini-edit in name tracitory trac.ini
@@ -37,7 +49,15 @@ EOU
 bitten-env(){
   elocal-
   export BITTEN_HOME=$HOME/bitten
-  export BITTEN_ENV=workflow
+  
+  local env
+  case $NODE_TAG in
+    H) env=env ;;
+    G) env=workflow ;;
+    *) env=unknown ;;
+  esac
+  
+  export BITTEN_ENV=$env
 }
 
 
