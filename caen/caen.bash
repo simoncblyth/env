a74a6833f493fd19a3a6f7101d9e6a8c42a8ff9d
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2008-06-04 07:04:01 +0800

    add HK localhost CAEN shared lib path

--- caen/caen.bash
+++ caen/caen.bash
@@ -3,10 +3,9 @@ caen-usage(){
 
 cat << EOU
 
-   caen-blahblah
-
-   
-
+   The fuction is used for setting enviroment variables of the VME local mechine user, who
+   doesn't have enough authority and want to test the VME software.
+   The enviroment vaviables have been setting after "caen-"
 
 
 EOU
@@ -20,9 +19,10 @@ caen-env(){
    elocal-
 
    if [ "$NODE_TAG" == "HKVME" ]; then
-     export CAEN_CD=/blah/blah
+     export CAEN_CD=/share/CAEN/V1718/SDK_Ver_3.1/VME_Bridge_Demo_and_Lib/Linux/lib
      export LD_LIBRARY_PATH=$CAEN_CD:$LD_LIBRARY_PATH
    fi
+
 
 }
 
