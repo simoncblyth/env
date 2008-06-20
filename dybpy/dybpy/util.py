c8b1fc3b8ca582d798f3f18fbd15248c37ff885b
Simon C Blyth <simoncblyth@gmail.com>
2008-06-20 09:23:25 +0800

    gentools main look

--- dybpy/dybpy/util.py
+++ dybpy/dybpy/util.py
@@ -4,5 +4,27 @@ def syspath():
     for s in sys.path:
         print s
 
+def gt():
+    """
+       dybgaudi/InstallArea/python/gentools.py
+    """
+
+    import xmldetdesc
+    xddc = xmldetdesc.XmlDetDescConfig()
+    tg = GenToolsConfig(volume="/dd/Geometry/Pool/lvFarPoolIWS")
+    import gaudimodule as gm
+    app = gm.AppMgr()
+    app.EvtSel = "NONE"
+    app.run(tg.nevents)
+
+
+if __name__=='__main__':
+    gt()
+
+
+
+
+
+
 
 
