1b855c372e525bbbcdf182d795aa1fe2bfc88324
Simon C Blyth <simoncblyth@gmail.com>
2008-06-21 14:52:00 +0800

    giving the whole shebang a repr

--- dybpy/dybpy/geneventlook.py
+++ dybpy/dybpy/geneventlook.py
@@ -13,10 +13,26 @@ app = GaudiPython.AppMgr()
 
 class Dumper(GaudiPython.PyAlgorithm):
     """  gaudi/GaudiPython/examples/read_lhcb_event_file.py  NB misusage of the name to feed in a location to dump """        
+    
     def execute(self):
-        esv = app.evtsvc()
-        print esv[self.name()]
+        dump = repr(self.esv[self.name()])
+        self.dumps.append(dump)
+        print dump
         return True
+        
+    def init(self):
+        """ cannot use an __init__ so use this instead """
+        self.esv = app.evtsvc()
+        self.dumps = []
+        return self
+
+    def __repr__(self):
+        dmp=[]
+        dmp.append("<%s> " % self.__class__.__name__)
+        for dump in self.dumps:
+            dmp.append(dump)
+        return "\n".join(dmp)
+         
 
 class GenEventLook():
     """ looking into the result of dybgaudi/InstallArea/python/gentools.py  """
@@ -35,14 +51,23 @@ class GenEventLook():
         self.esv.dump()
         self.gen = gen = app.algorithm("Generator")
         assert gen.Location == '/Event/Gen/HepMCEvents'
-        self.dumper = dumper = Dumper(gen.Location)
+        self.dumper = dumper = Dumper(gen.Location).init()
         app.addAlgorithm(dumper)
 
-    def run(self, n=1 ):
+    def run(self):
         app.run(self.conf.nevents)
         self.discovery()
+        return self
+
+    def __repr__(self):
+        """ hmmm how to control the run such that this to gives string rep of the whole shebang """
+        dmp = []
+        dmp.append( "<%s> " % self.__class__.__name__ )
+        dmp.append( repr(self.dumper) )
+        return "\n".join(dmp)
 
     def discovery(self):
+        """ all the assertions are done by genrepr when printing kco  ... still here mainly to provide objs for interactive examination """
         kco = self.kco = self.esv[self.gen.Location]
         assert kco.size() == 1 == len(kco)
         assert kco.__class__.__name__ == 'KeyedContainer<DayaBay::HepMCEvent,Containers::KeyedObjectManager<Containers::hashmap> >'
@@ -68,8 +93,9 @@ class GenEventLook():
 
 if __name__=='__main__':
     self = GenEventLook()
-    print self
     self.run()
+    print "self", self
+    
     
 
 
