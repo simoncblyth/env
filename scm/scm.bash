dcb887889c0e1dcccb54daa6d8fe5747e0c23602
Simon C Blyth <simoncblyth@gmail.com>
2008-07-04 14:22:31 +0800

    fresh new scm funcs

---
+++ scm/scm.bash
@@ -0,0 +1,16 @@
+
+scm-usage(){
+  
+   cat << EOU
+
+      An SCM is a combination of an SVN repository and a Tracitory , this provides
+      commands to create these two tied objects in a coherent way.
+
+   
+EOU
+
+}
+
+scm-env(){
+  elocal-
+}
