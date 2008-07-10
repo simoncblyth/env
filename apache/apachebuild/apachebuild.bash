d9d1bd0f1d3d3ac1ffcbb15c9ffe2cb205601294
Simon C Blyth <simoncblyth@gmail.com>
2008-07-10 12:02:11 +0800

    try to explicitly '''--enable-dav'''

--- apache/apachebuild/apachebuild.bash
+++ apache/apachebuild/apachebuild.bash
@@ -94,7 +94,7 @@ apachebuild-configure(){
 
    # $ASUDO ./configure --prefix=$(apachebuild-home) --enable-modules=most --enable-shared --enable-so
    
-   $SUDO ./configure --prefix=$(apachebuild-home) 
+   $SUDO ./configure --prefix=$(apachebuild-home) --enable-dav
    
 }
 
