09107f7ffd3b13d168eaec1b68e2a5a5b5eb4d09
Simon C Blyth <simoncblyth@gmail.com>
2008-09-10 16:02:29 +0800

    dtracebuild-get

--- dtrace/dtracebuild.bash
+++ dtrace/dtracebuild.bash
@@ -5,6 +5,7 @@ dtracebuild-usage(){
 
         dtracebuild-name : $(dtracebuild-name)
         dtracebuild-url  : $(dtracebuild-url)
+        dtracebuild-dir  : $(dtracebuild-dir) 
 
         dtracebuild-get/configure/install
 
@@ -18,14 +19,28 @@ dtracebuild-env(){
 }
 
 dtracebuild-name(){ echo dtrace-20080803 ; }
-dtracebuild-url(){  echo ftp://crisp.dynalias.com/pub/release/website/dtrace/$(dtracebuild-name).tar.bz2 ; }
+dtracebuild-ball(){ echo $(dtracebuild-name).tar.bz2 ; }
+dtracebuild-url(){  echo ftp://crisp.dynalias.com/pub/release/website/dtrace/$(dtracebuild-ball) ; }
+dtracebuild-dir(){  echo $SYSTEM_BASE/dtrace/build/$(dtracebuild-name) ; }
+dtracebuild-cd(){   cd $(dtracebuild-dir) ; }
+
 
 dtracebuild-get(){
 
-  echo -n
+    local dir=$(dtracebuild-dir)
+    local ball=$(dtracebuild-ball)
+    local url=$(dtracebuild-url)
+    local nam=$(dtracebuild-name)
 
+    mkdir -p $(dirname $dir)
+    cd $(dirname $(dirname $dir))
 
-
+    [ ! -f $ball ] && curl -O $url
+    [ ! -d build/$nam ] && ( cd build ; bunzip2 $ball ) 
 }
+
+
+
+
 
 
