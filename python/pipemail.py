192972d951bfe8e2e919282e48a95f0035cc2496
Simon C Blyth <simoncblyth@gmail.com>
2008-10-03 13:30:36 +0800

    improve annotation

--- python/pipemail.py
+++ python/pipemail.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
+"""
+   Usage :
+       echo hello | python pipemail.py user@example.com 
 
+     pipe in the content of the message from stdin, the first line 
+     will become the subject of the message, and provide the 
+     recipient email address as the first argumemnt
+"""
+ 
 def sendmail( lines , to , fr="me@localhost" ):
     """
          the first line is used as the subject of the message
@@ -17,19 +25,17 @@ def sendmail( lines , to , fr="me@localhost" ):
     import smtplib
     s = smtplib.SMTP()
     s.connect()
-    print "sendmail: to:%s fr:%s lines:%s " % ( to, fr, len(lines) )
+    print "Attempting to send email to recipient:[%s] from:[%s] message lines:[%s] " % ( to, fr, len(lines) )
     s.sendmail(fr, to, msg.as_string())
     s.close()
 
 
 if __name__=='__main__':
-    """
-        pipe in the content of the message, and provide the recipient 
-        email address as the first argumemnt
-    """
     import sys
-    sendmail( sys.stdin.readlines() , sys.argv[1] )
-
+    if len(sys.argv)>1:
+    	sendmail( sys.stdin.readlines() , sys.argv[1] )
+    else:
+	print sys.modules[__name__].__doc__
 
 
 
