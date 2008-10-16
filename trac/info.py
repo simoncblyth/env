4e5a977e0dd0885ebe3a6225bf5f4e7738c54d00
Simon C Blyth <simoncblyth@gmail.com>
2008-10-16 17:22:53 +0800

    adapt to Persistent

--- trac/info.py
+++ trac/info.py
@@ -2,7 +2,7 @@
    Provide vital statistics of the trac instances to python, using the 
    intertrac triplet source accessed from bash calls
 
-   A persistent dict is used to avoid needless rerunning of 
+   A persistent singleton is used to avoid needless rerunning of 
    the expensive bash call 
    
 """
@@ -13,27 +13,37 @@ import os
 class TracInstance(dict):
     def __init__(self, **kwa):self.update(kwa)
  
-from env.structure import PerDict
-class TracInfo(PerDict):
-    def __init__(self):
+from env.structure import Persistent
+class TracInfo(Persistent):
+    def __init__(self, *args, **kwa ):pass
+        
+    def init(self, *args, **kwa ):
         self.parse()
-        self.psave(self)
 
     def parse(self):
         from env.bash import Bash
         b = Bash()
         tps = b("trac- tracinter- tracinter-triplets-")
         import re
+        self.info={}
         for line in tps.split("\n"):
             a = re.split("\s*", line.strip() )
             if len(a) == 3:
                 i = TracInstance( tag=a[0], name=a[1], url=a[2] )
-                self[a[1]] = i
+                self.info[a[1]] = i
             else:
                 print "failed to interpret line [%s] [%s] " % ( line.strip() , a )
+
+    def __repr__(self):
+        from pprint import pformat 
+        return pformat(self.info)
+
 
 
 if __name__=='__main__':
     from env.trac import TracInfo
-    ti = TracInfo.pget()
-    print ti
+    tia = TracInfo(singleton=True)
+    tib = TracInfo(singleton=True)
+    assert tia == tib 
+    print tia
+    
