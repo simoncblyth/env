ab91586c0ec8ec4b0ee254b758dfd800f19e07dd
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2008-11-08 00:25:00 +0800

    integrating nuwa configuration and simhistexample settings waiting for further using

--- thho/NuWa/python/mygensimhist/myalg.py
+++ thho/NuWa/python/mygensimhist/myalg.py
@@ -1,17 +1,31 @@
+#!/usr/bin/env python
+
+#
+# usage: ./myalg.py [config1.py config2.py ...]
+#
+
 from GaudiPython import PyAlgorithm
+import GaudiKernel.SystemOfUnits as units
 
 class MyAlg(PyAlgorithm):
     def execute(self):
-	print 'Starting self defining algorithm'
+	print 'Starting customizing algorithm!'
         return True
 
 if __name__=="__main__":
-    print ''
-    import mysim
-    from GaudiPython import AppMgr
-    app = AppMgr()
-    print 'adding self defining algorithm'
-    app.addAlgorithm(MyAlg())
-    print 'Run!! Go fight go!!'
-    app.run(1) 
 
+        print 'Using the nuwa configuration'
+        from DybPython.Control import main
+        nuwa = main()
+
+	# Dont use nuwa.run(), using the run configuration to add
+	# self defining alforithm
+        from GaudiPython import AppMgr
+        app = AppMgr()
+        print 'Adding customizing algorithm'
+        app.addAlgorithm(MyAlg())   
+        print 'Run!! Go Fight Go!!'
+        app.EvtMax = nuwa.opts.executions
+        app.run(app.EvtMax)
+
+	pass #end
