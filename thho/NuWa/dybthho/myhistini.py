1f0ed8674bc4001a6e72d12863ec58faef8b4525
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2008-11-11 16:13:10 +0800

    add pyroot to make histogram

--- thho/NuWa/dybthho/myhistini.py
+++ thho/NuWa/dybthho/myhistini.py
@@ -1,9 +1,40 @@
 
 from GaudiPython import PyAlgorithm
+from GaudiPython import AppMgr
+from DybPython.Util import irange
 
 class MyAlg(PyAlgorithm):
 	def execute(self):
 		print 'Starting customizing algorithm!'
+
+		print 'Using PyRoot to make histogram!'
+		from ROOT import TCanvas, TFile, TH1F
+		from ROOT import gROOT, gRandom, gSystem, Double
+		gROOT.Reset()
+		c1 = TCanvas( 'c1', 'Dynamic Filling Example', 200, 10, 700, 500 )
+		hfile = TFile( 'thhohsimple.root', 'RECREATE', 'Demo ROOT file with histograms' )
+		hpx    = TH1F( 'hpx', 'This is the px distribution', 100, -4, 4 )
+		hpxFill = hpx.Fill
+
+		print 'Accessing info from TES.......'
+		app = AppMgr()
+		esv = app.evtsvc()
+		esv.dump()
+		egh = esv['/Event/Gen/GenHeader']
+		ee = egh.event()
+#		print 'looping over all particles ...'
+#		count = 0
+#		for pax in irange(ee.particles_begin(),ee.particles_end()):
+#			print pax
+#			print 'counting is ',count
+#			count = count + 1
+#			hpxFill(pax.momentum().px())
+
+		c1.Modified()
+		c1.Update()
+		del hpxFill
+		hfile.Write()
+		print 'Histogram Ready!!'
 		return True
 
 
@@ -12,7 +43,7 @@ def run(*args):
 	pass
 
 def configure():
-	from GaudiPython import AppMgr
+#	from GaudiPython import AppMgr
 	app = AppMgr()
 	print 'Adding customizing algorithm'
 	app.addAlgorithm(MyAlg())
