9754e86f92dfc287fe53aca24e39658a1890202b
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2008-12-03 20:40:03 +0800

    preliminary output sample ID with html

--- thho/xmlData/acrylics/modelDraft/present.py
+++ thho/xmlData/acrylics/modelDraft/present.py
@@ -1,4 +1,19 @@
 import elementtree.ElementTree as ET
 tree = ET.parse("samples.xml")
-ti =tree.getiterator()
-print ti[0]
+ti =tree.getiterator("sample")
+root = ET.Element("html")
+head = ET.SubElement(root, "head")
+title = ET.SubElement(head, "title")
+title.text = "Dayabay acrylic transmittance measurement"
+body = ET.SubElement(root, "body")
+for sample in ti:
+    para = ET.SubElement(body, "p")
+    sampleName = sample.tag
+    sampleId = sample.attrib.get("id")
+    sampleFull = sampleName+sampleId
+    para.text = sampleFull
+
+outputTree = ET.ElementTree(root)
+outputTree.write("testpage.html")
+
+
