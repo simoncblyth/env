06168b4b5ebea87ebf3271cd225c4436396b8f83
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2008-12-20 12:08:07 +0800

    table and file link works well, prototype xml structure almost done

--- thho/xmlData/acrylics/case3/present_with_html.py
+++ thho/xmlData/acrylics/case3/present_with_html.py
@@ -1,77 +1,149 @@
-import string
+# TODO: 1.check the file existing
+#       2.check the id consistence
+#       3.add general function could add xml texts by tags in the table
+
+
 import xml.etree.ElementTree as ET
 
+fileFormat=".html"
 
+def MakeIndexHtmlPrototype(root):
+    head =  ET.SubElement(root, "head")
+    title = ET.SubElement(head, "title")
+    body =  ET.SubElement(root, "body")
 
+    sampleTable = ET.SubElement(body, "table")
+    sampleTableRowTitle = ET.SubElement(sampleTable,"tr")
+    ET.SubElement(sampleTableRowTitle,"th").text = "sample chunk"
+    ET.SubElement(sampleTableRowTitle,"th").text = "sample piece"
 
+    title.text = "Dayabay acrylic transmittance measurement - sample chunks"
+    sampleTable.set("border","1")
+
+    return sampleTable
+    pass
+
+def MakePieceHtmlPrototype(root):
+    head =  ET.SubElement(root, "head")
+    title = ET.SubElement(head, "title")
+    body =  ET.SubElement(root, "body")
+
+    sampleTable = ET.SubElement(body, "table")
+    sampleTableRowTitle = ET.SubElement(sampleTable,"tr")
+    ET.SubElement(sampleTableRowTitle,"th").text = "sample piece"
+    ET.SubElement(sampleTableRowTitle,"th").text = "treatment and measurement"
+
+    title.text = "Dayabay acrylic transmittance measurement - sample pieces of a chunk"
+    sampleTable.set("border","1")
+
+    sampleTable
+    return sampleTable
+    pass
+
+def MakeMeasurementHtmlPrototype(root):
+    head =  ET.SubElement(root, "head")
+    title = ET.SubElement(head, "title")
+    body =  ET.SubElement(root, "body")
+
+    sampleTable = ET.SubElement(body, "table")
+    sampleTableRowTitle = ET.SubElement(sampleTable,"tr")
+    ET.SubElement(sampleTableRowTitle,"th").text = "treatment and measurement"
+    ET.SubElement(sampleTableRowTitle,"th").text = "row data file"
+
+    title.text = "Dayabay acrylic transmittance measurement - sample pieces measurement"
+    sampleTable.set("border","1")
+
+    sampleTable
+    return sampleTable
+    pass
 
 # generating the samples list
-def MakeSamplePage():
+def MakeSamplesPage():
+    root = ET.Element("html")
+    sampleTable = MakeIndexHtmlPrototype(root)
+
     tree = ET.parse("samples.xml")
     ti = tree.getiterator("sample")
-    plti = tree.getiterator("piecesLocation")
-    root = ET.Element("html")
-    head = ET.SubElement(root, "head")
-    title = ET.SubElement(head, "title")
-    title.text = "Dayabay acrylic transmittance measurement"
-    body = ET.SubElement(root, "body")
+
     for sample in ti:
-    
-        para = ET.SubElement(body, "p")
-        sampleId = sample.attrib.get("id")
-        print "sampleId is ", sampleId
-        sampleFull = sample.tag+sample.attrib.get("id")
-        para.text = sampleFull
-    
-        pie = plti[string.atoi(sampleId)]
-        pieceLink = pie.attrib.get("href")
-        piecehref = ET.SubElement(para,"a")
-        piecehref.set("href",pieceLink)
-        piecehref.text = pie.text
+        rowA = ET.SubElement(sampleTable,"tr")
+        ET.SubElement(rowA,"td").text = sample.tag+sample.attrib.get("sampleId")
+        colB = ET.SubElement(rowA,"td")
+        for piece in sample.getiterator("piece"):
+            linkFileName = sample.tag+sample.attrib.get("sampleId") + "_" + piece.tag + piece.attrib.get("pieceId") 
+            href = ET.SubElement(ET.SubElement(colB,"p"),"a")
+            href.set("href",linkFileName + fileFormat)
+            href.text = linkFileName
+            MakePiecesPage(sample,piece,linkFileName + fileFormat)
 
-        outputTree = ET.ElementTree(root)
-        outputTree.write("testpageSample.html")
+    outputTree = ET.ElementTree(root)
+    outputTree.write("index.html")
     pass
-
 
 # generating the pieces list
-def MakePiecePage():
-    pieceTree = ET.parse("pieces.xml")
-    pieceTi = pieceTree.getiterator("testPiece")
-    pieceRoot = ET.Element("html")
-    pieceHead = ET.SubElement(pieceRoot, "head")
-    pieceTitle = ET.SubElement(pieceHead, "title")
-    pieceTitle.text = "Dayabay acrylic transmittance measurement"
-    pieceBody = ET.SubElement(pieceRoot,"body")
-    for piece in pieceTi:
-    
-        piecePara = ET.SubElement(pieceBody,"p")
-        pieceName = piece.tag + " " + piece.attrib.get("pieceId") + " of sample " + piece.attrib.get("sampleId")
-        print pieceName
-    
-        pieceParaMea = ET.SubElement(piecePara,"p")
-        pieceParaMeahref = piece.getiterator("measurement")
-        for mea in pieceParaMeahref:
-            pieceParaMeaLink = ET.SubElement(pieceParaMea,"a")
-            pieceParaMeaLink.set("href",mea.attrib.get("href"))
-            pieceParaMeaLink.text = pieceName + " measurement " + mea.attrib.get("meaId")
-    pieceOutPutTree = ET.ElementTree(pieceRoot)
-    pieceOutPutTree.write("testpagePiece.html")
+def MakePiecesPage(isample,ipiece,ifile):
+    root = ET.Element("html")
+    sampleTable = MakePieceHtmlPrototype(root)
+
+    tree = ET.parse("pieces.xml")
+    ti = tree.getiterator("piece")
+
+    for piece in ti:
+        rowA = ET.SubElement(sampleTable,"tr")
+        # should check the sampleId in sample.xml and piece.xml consistent,not yet complete
+        if (isample.attrib.get("sampleId") == piece.attrib.get("sampleId")) and \
+           (ipiece.attrib.get("pieceId") == piece.attrib.get("pieceId")):
+            sample = isample
+            ET.SubElement(rowA,"td").text = sample.tag + sample.attrib.get("sampleId") + "_" + piece.tag + piece.attrib.get("pieceId")
+            colB = ET.SubElement(rowA,"td")
+            for measurement in piece.getiterator("measurement"):
+                linkFileName = piece.tag + piece.attrib.get("pieceId") + "_" + measurement.tag + measurement.attrib.get("measurementId")
+                MakeMeasurementsPage(sample,piece,measurement,linkFileName + fileFormat)
+                href = ET.SubElement(ET.SubElement(colB,"p"),"a")
+                href.set("href",linkFileName + fileFormat)
+                href.text = linkFileName
+
+    outputTree = ET.ElementTree(root)
+    outputTree.write(ifile)
     pass
+
 
 
 # generating the measurement list
-def MakeMeasurementPage():
+def MakeMeasurementsPage(isample,ipiece,imeasurment,ifile):
+    root = ET.Element("html")
+    sampleTable = MakeMeasurementHtmlPrototype(root)
+
+    tree = ET.parse("measurement.xml")
+    ti = tree.getiterator("measurement")
+
+    for measurement in ti:
+        rowA = ET.SubElement(sampleTable,"tr")
+        print measurment
+        # should check the sampleId in sample.xml and piece.xml consistent,not yet complete
+        if (isample.attrib.get("sampleId") == measurment.attrib.get("sampleId")) and \
+           (ipiece.attrib.get("pieceId") == measurment.attrib.get("pieceId")) and \
+           (imeasurment.attrib.get("measurmentId") == measurement.attrib.get("measurementId")):
+            sample = isample
+            piece = ipiece
+            ET.SubElement(rowA,"td").text = sample.tag + sample.attrib.get("sampleId") + "_" \
+                                            + piece.tag + piece.attrib.get("pieceId") + "_" \
+                                            + measurment.tag + measurment.attrib.get("measurmentId")
+            colB = ET.SubElement(rowA,"td")
+            for rowData in measurment.getiterator("rowData"):
+                linkFileName = sample.attrib.get("sampleId") + piece.attrib.get("pieceId") + measurement.attrib.get("measurementId")
+                href = ET.SubElement(ET.SubElement(colB,"p"),"a")
+                href.set("href",linkFileName + fileFormat)
+                href.text = linkFileName
+
+    outputTree = ET.ElementTree(root)
+    outputTree.write(ifile)
     pass
 
+# general function to access xml data by tag and put it in the table
+def GetXmlTextByTag():
+    pass
 
-
-
-
-
-
-MakeSamplePage()
-MakePiecePage()
-MakeMeasurementPage()
+MakeSamplesPage()
 
 
