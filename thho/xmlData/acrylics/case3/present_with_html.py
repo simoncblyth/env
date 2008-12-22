e133551c17c34e81ae2b9b7850c8f1d8a7e4deb3
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2008-12-22 23:56:20 +0800

    add file providing items in the table, almost complete the presentation script

--- thho/xmlData/acrylics/case3/present_with_html.py
+++ thho/xmlData/acrylics/case3/present_with_html.py
@@ -10,14 +10,8 @@ Rountine example:
     1. Edit the info according to tags in samples.xml, pieces.xml or measurements.xml.
         If these is no tag you need, add new one and edit this info on the wiki page.
         It YOUR duty to make sure there is no duplicate tag in the xml files.
-    2. Decide what are the extra info tags you would like to show. Add functions like:
-        ......
-        GetSampleXmlTextByTag(row,sample,"author")
-        ......
-        GetPieceXmlTextByTag(row,piece,"history")
-        ......
-        GetMeasurementXmlTextByTag(row,measurement,"people")
-        ......
+    2. Decide what are the extra info tags you would like to show. Add tags in the list, tableItem like:
+        tableItem = ['sampleAuthor','history','pieceAuthor','measurementAuthor','otherFile']
     3. Rerun this script, then it will generate the html pages containing info table
         indicating where are the row data files and other info, e.g. an image or a histogram.
 
@@ -28,9 +22,10 @@ Rountine example:
 #
 # TODO: 1.check the file existing
 #       2.check the id consistence
-#       3.add general function could add xml texts by tags in the table
+#       Done3.add general function could add xml texts by tags in the table
 #       4.add duplicate Id checking function in another py script. import it
-#       5.add __main__ and doc
+#       Done5.add __main__ and doc
+#       6. modify doc
 
 
 import xml.etree.ElementTree as ET
@@ -39,8 +34,13 @@ import xml.etree.ElementTree as ET
 pageFormat = ".html"
 rowDataFormat = ".asc"
 imageFormat = ".png"
+tableItem = ['sampleAuthor','history','pieceAuthor','measurementAuthor','otherFile','sampleDimensions','pieceDimensions']
 
 def MakeIndexHtmlPrototype(root):
+
+    tree = ET.parse("sampleTitle.xml")
+    sample = tree.find("sample")
+
     head =  ET.SubElement(root, "head")
     title = ET.SubElement(head, "title")
     body =  ET.SubElement(root, "body")
@@ -49,7 +49,9 @@ def MakeIndexHtmlPrototype(root):
     sampleTableRowTitle = ET.SubElement(sampleTable,"tr")
     ET.SubElement(sampleTableRowTitle,"th").text = "sample chunk"
     ET.SubElement(sampleTableRowTitle,"th").text = "sample piece"
-
+    for tag in tableItem:
+        if sample.find(tag) != None:
+            ET.SubElement(sampleTableRowTitle,"th").text = sample.find(tag).text
     title.text = "Dayabay acrylic transmittance measurement - sample chunks"
     sampleTable.set("border","1")
 
@@ -57,6 +59,10 @@ def MakeIndexHtmlPrototype(root):
     pass
 
 def MakePieceHtmlPrototype(root):
+
+    tree = ET.parse("pieceTitle.xml")
+    piece = tree.find("piece")
+
     head =  ET.SubElement(root, "head")
     title = ET.SubElement(head, "title")
     body =  ET.SubElement(root, "body")
@@ -66,14 +72,21 @@ def MakePieceHtmlPrototype(root):
     ET.SubElement(sampleTableRowTitle,"th").text = "sample piece"
     ET.SubElement(sampleTableRowTitle,"th").text = "treatment and measurement"
 
+    for tag in tableItem:
+        if piece.find(tag) != None:
+            ET.SubElement(sampleTableRowTitle,"th").text = piece.find(tag).text
+
     title.text = "Dayabay acrylic transmittance measurement - sample pieces of a chunk"
     sampleTable.set("border","1")
 
-    sampleTable
     return sampleTable
     pass
 
 def MakeMeasurementHtmlPrototype(root):
+
+    tree = ET.parse("measurementTitle.xml")
+    measurement = tree.find("measurement")
+
     head =  ET.SubElement(root, "head")
     title = ET.SubElement(head, "title")
     body =  ET.SubElement(root, "body")
@@ -83,10 +96,13 @@ def MakeMeasurementHtmlPrototype(root):
     ET.SubElement(sampleTableRowTitle,"th").text = "treatment and measurement"
     ET.SubElement(sampleTableRowTitle,"th").text = "row data file"
 
+    for tag in tableItem:
+        if measurement.find(tag) != None:
+            ET.SubElement(sampleTableRowTitle,"th").text = measurement.find(tag).text
+
     title.text = "Dayabay acrylic transmittance measurement - sample pieces measurement"
     sampleTable.set("border","1")
 
-    sampleTable
     return sampleTable
     pass
 
@@ -102,19 +118,19 @@ def MakeSamplesPage():
         row = ET.SubElement(sampleTable,"tr")
         ET.SubElement(row,"td").text = sample.tag+sample.attrib.get("sampleId")
         colB = ET.SubElement(row,"td")
+        # looping default column info
         for piece in sample.getiterator("piece"):
             linkFileName = sample.tag+sample.attrib.get("sampleId") + "_" + piece.tag + piece.attrib.get("pieceId") 
             href = ET.SubElement(ET.SubElement(colB,"p"),"a")
             href.set("href",linkFileName + pageFormat)
-            href.text = linkFileName
+            if piece.text == None:
+                href.text = linkFileName
+            else: href.text = linkFileName + " " + piece.text
             MakePiecesPage(sample,piece,linkFileName + pageFormat)
-        #######################################################################################
-        # Use GetSampleXmlTextByTag function here to add more info into the presentation table#
-        #                                                                                     #
-        #                                                                                     #
-        #GetSampleXmlTextByTag(row,sample,"author")
-        #######################################################################################
-
+        # looping option column info
+        for tag in tableItem:
+             if sample.find(tag) != None:
+                 ET.SubElement(row,"td").text = sample.find(tag).text
     outputTree = ET.ElementTree(root)
     outputTree.write("index.html")
     pass
@@ -135,6 +151,7 @@ def MakePiecesPage(isample,ipiece,ifile):
             sample = isample
             ET.SubElement(row,"td").text = sample.tag + sample.attrib.get("sampleId") + "_" + piece.tag + piece.attrib.get("pieceId")
             colB = ET.SubElement(row,"td")
+            # looping default column info
             for measurement in piece.getiterator("measurement"):
                 linkFileName = sample.tag + sample.attrib.get("sampleId") + "_"\
                                 + piece.tag + piece.attrib.get("pieceId") + "_"\
@@ -142,13 +159,14 @@ def MakePiecesPage(isample,ipiece,ifile):
                 href = ET.SubElement(ET.SubElement(colB,"p"),"a")
                 href.set("href",linkFileName + pageFormat)
                 href.text = linkFileName
+                if measurement.text == None:
+                    href.text = linkFileName
+                else: href.text = linkFileName + " " + measurement.text
                 MakeMeasurementsPage(sample,piece,measurement,linkFileName + pageFormat)
-            #######################################################################################
-            # Use GetSampleXmlTextByTag function here to add more info into the presentation table#
-            #                                                                                     #
-            #                                                                                     #
-            GetPieceXmlTextByTag(row,piece,"history")
-            #######################################################################################
+            # looping option column info
+            for tag in tableItem:
+                if piece.find(tag) != None:
+                    ET.SubElement(row,"td").text = piece.find(tag).text
 
     outputTree = ET.ElementTree(root)
     outputTree.write(ifile)
@@ -164,6 +182,7 @@ def MakeMeasurementsPage(isample,ipiece,imeasurement,ifile):
     tree = ET.parse("measurements.xml")
     ti = tree.getiterator("measurement")
 
+
     for measurement in ti:
         row = ET.SubElement(sampleTable,"tr")
         # should check the sampleId in sample.xml and piece.xml consistent,not yet complete
@@ -176,33 +195,36 @@ def MakeMeasurementsPage(isample,ipiece,imeasurement,ifile):
                                             + piece.tag + piece.attrib.get("pieceId") + "_" \
                                             + measurement.tag + measurement.attrib.get("measurementId")
             colB = ET.SubElement(row,"td")
+            # looping default column info
             for rowData in measurement.getiterator("rowData"):
-                linkFileName = sample.attrib.get("sampleId") + piece.attrib.get("pieceId") + measurement.attrib.get("measurementId")
+                linkFileName = sample.attrib.get("sampleId")\
+                             + "-" + piece.attrib.get("pieceId")\
+                             + "-" + measurement.attrib.get("measurementId")\
+                             + "-" + rowData.attrib.get("rowDataId")
                 href = ET.SubElement(ET.SubElement(colB,"p"),"a")
                 href.set("href",linkFileName + rowDataFormat)
                 href.text = linkFileName
-            #######################################################################################
-            # Use GetSampleXmlTextByTag function here to add more info into the presentation table#
-            #                                                                                     #
-            #                                                                                     #
-            GetMeasurementXmlTextByTag(row,measurement,"people")
-            #######################################################################################
+                if rowData.text == None:
+                    href.text = linkFileName
+                else: href.text = linkFileName + " " + rowData.text
+            # looping option column info
+            for tag in tableItem:
+                # tag "otherFile" is a special tag because it would provide a link
+                if tag == "otherFile":
+                    for file in measurement.getiterator(tag):
+                        #linkFileName = file.attrib.get("fileName")
+                        href = ET.SubElement(ET.SubElement(ET.SubElement(row,"td"),"p"),"a")
+                        href.set("href",linkFileName)
+                        href.text = file.attrib.get("fileName")
+                        if file.text == None:
+                            href.text = linkFileName
+                        else: href.text = linkFileName + " " + file.text
+                if measurement.find(tag) != None:
+                    ET.SubElement(row,"td").text = measurement.find(tag).text
 
     outputTree = ET.ElementTree(root)
     outputTree.write(ifile)
     pass
-
-# general function to access xml data by tag and put it in the table
-def GetSampleXmlTextByTag(row,sample,tag):
-    ET.SubElement(row,"td").text = sample.find(tag).text
-    pass
-def GetPieceXmlTextByTag(row,piece,tag):
-    ET.SubElement(row,"td").text = piece.find(tag).text
-    pass
-def GetMeasurementXmlTextByTag(row,measurement,tag):
-    ET.SubElement(row,"td").text = measurement.find(tag).text
-    pass
-
 
 if '__main__' == __name__:
     print __doc__
