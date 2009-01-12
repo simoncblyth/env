462038d667e318f301fad2bc942a0704a6e4da99
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2009-01-12 21:40:55 +0800

    2D Newton method work

--- thho/Acrylic_Optical/acrylicsOpticalPara/FresnelAnalysis.C
+++ thho/Acrylic_Optical/acrylicsOpticalPara/FresnelAnalysis.C
@@ -16,15 +16,23 @@
 //
 
 #include <iostream>
+#include <iomanip>
 #include <complex>
 
-#define pi 3.1415926
-#define delta 1.0e-10
+#define PI 3.1415926
+#define DELTA 1.0e-10
+#define THICKNESS 10
+#define g_MAXLOOP 7
+#define g_ACCURACY 1.0e-6
 
 using namespace std;
 
 void FresnelAnalysis(Double_t n, Double_t k, Double_t d,
             Double_t lambda, Double_t Tm, Double_t Rm) {
+
+    // some printing out and value precision stuff
+    cout.setf(ios::fixed | ios::showpoint);
+    cout.precision(5);
 
     // initial Tmc and Rmc
     Double_t Tmc=0;
@@ -34,25 +42,37 @@ void FresnelAnalysis(Double_t n, Double_t k, Double_t d,
     printf("\nTm\tRm\tTmc\tRmc\t\n");
     printf("%f\t%f\t%f\t%f\n",Tm,Rm,Tmc,Rmc);
 
-    if(newton(n, 7, 1.0e-6,k,Tm))
-       cout << "\n     root n = " << n << ", test of f(x) = " << func(n,k,Tm);
-    else cout << "\n    failed to find root ";
+    cout << "\nStarting using Newton method..........\n" << endl;
+
+//    if(RunNewtonOneD(n, k, 7, 1.0e-6,Rm))
+//       cout << "\nroot n = " << n << ", test of f(x) = " << RFunc(n,k,Rm);
+//    else cout << "\nfailed to find root ";
+
+    cout<<"  loop       n       k       TFunc       RFunc         dx\          dy" << endl;
+
+    if(RunNewtonTwoD(n,k,g_MAXLOOP,g_ACCURACY,Tm,Rm,d,lambda)) {
+        cout<< " \n successfully finding the root!" << endl;
+    } else {
+        cout << "\nfailed to find root " << endl;
+    }
 
 }
 
-//////// Newton method ///////////////////////////////////////////////////////
-Int_t newton(Double_t &x, Int_t max_loop, const Double_t accuracy,
-		Double_t k, Double_t Tm) {
+////////////////////////////////////////////////////////////////////////////
+//////// Newton method /////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+// 1D Newton method to check whether the result is resonable or not
+Int_t RunNewtonOneD(Double_t &x, const Double_t k, Int_t max_loop,
+                const Double_t accuracy, const Double_t Rm) {
     Double_t term;
     do
         {
          // calculate next term f(x) / f'(x) then subtract from current root
-         term = func(x, k, Tm) / fdiv(x, k, Tm);
-	 cout << "term " << term << " and n is " << x << endl;
+         term = RFunc(x, k, Rm) / RFdiv(x, k, Rm);
          x = x - term;// new root
-	 cout << "new n is " << x << endl;
-	 cout << "accu is  " << fabs(term/x) << endl;
-	 
+         cout << "new n is " << x << "\taccu is " << fabs(term/x) << endl;
+         
         }
     // check if term is within required accuracy or loop limit is exceeded
     while ((fabs(term / x) > accuracy) && (--max_loop));
@@ -60,21 +80,117 @@ Int_t newton(Double_t &x, Int_t max_loop, const Double_t accuracy,
     return max_loop;
 }
 
-Double_t func(Double_t n, Double_t k, Double_t Tm) {
+// 2D Newton method
+Int_t RunNewtonTwoD(Double_t &n, Double_t &k, Int_t maxLoop,
+                const Double_t accuracy, const Double_t Tm, 
+                const Double_t Rm, const Double_t d,
+                const Double_t lambda) {
+    Double_t term;
+    Double_t Tmf(0), Rmf(0), Tmn(0), Rmn(0), Tmk(0), Rmk(0);
+    Double_t del(0), newn(0), newk(0), dn(0), dk(0);
+    Int_t i(0);
+    do
+        {
+        i++;
+        InitializeFunc(n,k,d,Tm,Rm,Tmf,Rmf,Tmn,Rmn,Tmk,Rmk,lambda);
+        GetCoupledJacobianFunc(n,k,Tmf,Rmf,Tmn,Rmn,Tmk,Rmk,
+                                del,newn,newk,dn,dk);
+        cout<<setw(3)<<i<<setw(12)<<n<<setw(12)<<k<<setw(12)<<setw(12)
+            <<Tmf<<setw(12)<<Rmf<<setw(12)<<dn<<setw(12)<<dk<<endl;
+        cout << "accuracy for n is " << fabs(dn/n) << " for k is "
+                << fabs(dk/k) << "\n" << endl;
+        }
+    while(fabs(dn/n) >= accuracy && fabs(dk/k) >= accuracy && (--maxLoop));
 
-    return GetFR(n,k) - Tm;
+    // return the maxLoop as the flag to express finding
+    // a root successfully
+    return maxLoop;
 
 }
 
-Double_t fdiv(Double_t n, Double_t k, Double_t Tm) {
+// constrain of R
+Double_t RFunc(Double_t n, Double_t k, Double_t Rm) {
 
-    Double_t y = (func(n+delta,k,Tm)-func(n,k,Tm))/delta;
+    // wrong!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    return GetFR(n,k) - Rm;
+
+}
+
+Double_t RFdiv(Double_t n, Double_t k, Double_t Tm) {
+
+    return RFpartialn(n,k,Tm);
+
+}
+
+Double_t RPartialn(Double_t n, Double_t k, Double_t Tm) {
+
+    Double_t y = (RFunc(n+DELTA/2,k,Tm)-RFunc(n-DELTA/2,k,Tm))/DELTA;
     return y;
 
 }
-//////////////////////////////////////////////////////////////////////////////
 
-//////// Fresnel relationship ////////////////////////////////////////////////
+Double_t RPartialk(Double_t n, Double_t k, Double_t Tm) {
+
+    Double_t y = (RFunc(n,k+DELTA/2,Tm)-RFunc(n,k-DELTA/2,Tm))/DELTA;
+    return y;
+}
+
+// wrong !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+Double_t TFunc(Double_t n, Double_t k, Double_t d, Double_t lambda, Double_t Tm) {
+
+    return GetIT(k, d, lambda) -Tm;
+
+}
+
+Double_t TPartialn(Double_t k,const Double_t d, const Double_t lambda) {
+
+    Double_t y = 0;
+    return y;
+
+}
+
+Double_t TPartialk(Double_t n, Double_t k,const Double_t d,
+                    const Double_t lambda, const Double_t Tm) {
+
+    Double_t y = (TFunc(n,k+DELTA/2,d,lambda,Tm)
+                - TFunc(n,k-DELTA/2,d,lambda,Tm)) / DELTA;
+    return y;
+
+}
+
+void InitializeFunc(Double_t &n, Double_t &k,const Double_t d,
+                const Double_t Tm, const Double_t Rm,
+                Double_t &Tmf, Double_t &Rmf, Double_t &Tmn, Double_t &Rmn,
+                Double_t &Tmk, Double_t &Rmk, const Double_t lambda) {
+
+        // wrong!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        Tmf = TFunc(n,k,d,lambda,Tm);
+        Rmf = RFunc(n,k,Rm);
+        Tmn = TPartialn(k,THICKNESS,lambda);
+        Rmn = RPartialn(n,k,Tm);
+        Tmk = TPartialk(n,k,THICKNESS,lambda,Tm);
+        Rmk = RPartialk(n,k,Rm);
+
+}
+
+void GetCoupledJacobianFunc(Double_t &n,Double_t &k,
+            Double_t &Tmf, Double_t &Rmf, Double_t &Tmn, Double_t &Rmn,
+            Double_t &Tmk, Double_t &Rmk, Double_t &del, Double_t &newn,
+            Double_t &newk, Double_t &dn, Double_t &dk) {
+
+    del = Tmn*Rmk - Tmk*Rmn;
+    dn = (Tmk*Rmf - Tmn*Rmf)/del;
+    dk = (Tmf*Rmn - Tmn*Rmf)/del;
+    newn = n + dn;
+    newk = k + dk;
+
+    n = newn;
+    k = newk;
+    
+}
+
+////////////////////////////////////////////////////////////////////////////
+//////// Fresnel relationship //////////////////////////////////////////////
 Double_t GetFR(Double_t n, Double_t k) {
 
     Double_t FR = ((n-1)*(n-1)+k*k)/((n+1)*(n+1)+k*k);
@@ -84,14 +200,14 @@ Double_t GetFR(Double_t n, Double_t k) {
 
 Double_t GetIT(Double_t k, Double_t d, Double_t lambda) {
 
-    Double_t IT = exp((-4*pi*k*d)/lambda);
+    Double_t IT = exp((-4*PI*k*d)/lambda);
     return IT;
 
 }
-///////////////////////////////////////////////////////////////////////////////
-
-//////// Optical Model ////////////////////////////////////////////////////////
-void GetOpticalModelRValue(Double_t &Tmc, Double_t &Rmc, Double_t IT, Double_t FR) {
+////////////////////////////////////////////////////////////////////////////
+//////// Optical Model /////////////////////////////////////////////////////
+void GetOpticalModelRValue(Double_t &Tmc, Double_t &Rmc,
+                Double_t IT, Double_t FR) {
 
     Rmc = FR*(1+IT*(Tmc));
 
@@ -100,7 +216,6 @@ void GetOpticalModelRValue(Double_t &Tmc, Double_t &Rmc, Double_t IT, Double_t F
 void GetOpticalModelTValue(Double_t &Tmc, Double_t IT, Double_t FR) {
 
     Tmc = ((1-FR)*(1-FR)*IT)/(1-FR*FR*IT*IT);
-    cout << Tmc << endl;
 
 }
 
@@ -110,4 +225,8 @@ void GetOpticalModelValue(Double_t &Tmc, Double_t &Rmc, Double_t IT, Double_t FR
     GetOpticalModelRValue(Tmc, Rmc, IT, FR);
 
 }
-////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
+
+
