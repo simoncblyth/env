fd25d205909a4595f6d505262e5e50bd82113aea
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2009-02-01 13:48:44 +0800

    redo to add new AcylicOpticalSim

--- thho/NuWa/setup.py
+++ thho/NuWa/setup.py
@@ -21,17 +21,35 @@ Processing dependencies for dybthho==0.0.1
 Finished processing dependencies for dybthho==0.0.1
 thho@thho-laptop:~/env/thho/NuWa$
 
+
+20090201
+[thho@hep52 NuWa]$ python setup.py develop
+running develop
+running egg_info
+writing ThhoNuWa.egg-info/PKG-INFO
+writing top-level names to ThhoNuWa.egg-info/top_level.txt
+writing dependency_links to ThhoNuWa.egg-info/dependency_links.txt
+writing manifest file 'ThhoNuWa.egg-info/SOURCES.txt'
+running build_ext
+Creating /data/dyb/NuWa/1.1.0/external/Python/2.5.2/slc4_amd64_gcc346/lib/python2.5/site-packages/ThhoNuWa.egg-link (link to .)
+ThhoNuWa 0.0.1 is already the active version in easy-install.pth
+
+Installed /misc/home/hep/thho/env/thho/NuWa
+Processing dependencies for ThhoNuWa==0.0.1
+Finished processing dependencies for ThhoNuWa==0.0.1
+[thho@hep52 NuWa]$
+
    
 """
 
 from setuptools import setup
 
 setup(
-    name='dybthho',
+    name='ThhoNuWa',
     version='0.0.1',
     author='Taihsiang',
     author_email = 'thho@hep1.phys.ntu.edu.tw',
-    description = 'dybthho utilities ',
+    description = 'Thho Dayabay NuWa utilities',
     license = 'None',
-    packages = ['dybthho'],
+    packages = ['dybthho']
     )
