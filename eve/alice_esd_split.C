8ea5ee83458ef2e4c593ed0523ea19931f32c447
Simon C Blyth <simoncblyth@gmail.com>
2009-02-19 19:49:01 +0800

    Major refactoring to separate detector specifics ... currently broken, project not being made

--- eve/alice_esd_split.C
+++ eve/alice_esd_split.C
@@ -13,341 +13,186 @@ TEveGeoShape *gGeoShape;
 TGTextEntry *gTextEntry;
 TGHProgressBar *gProgress;
 
+class EvNav ;
+EvNav* gEvNav = 0 ;
+
+class EvD ;
+EvD* gEvD = 0 ;
 
 
+//
+//  abstract base class 
+//  defining the interface that all Event Readers 
+//  must provide
+//
 
 
-// Forward declarations.
+class IEvReader
+{
+  public:
+     virtual void Read() = 0 ;
+     virtual Bool_t LoadProject(const char* file, const char* project) = 0 ;
+     virtual Bool_t InitProject(const char* file ) = 0 ;
+
+     virtual ~IEvReader() ;
+     ClassDef(IEvReader, 1 )     
+};
+
+ClassImp(IEvReader)
+IEvReader::~IEvReader(){}
+
+
+// Alice Event Reader 
 
 class AliESDEvent;
 class AliESDfriend;
 class AliESDtrack;
 class AliExternalTrackParam;
 
-Bool_t     alice_esd_loadlib(const char* file, const char* project);
-void       load_event();
-
-void       alice_esd_read();
-TEveTrack* esd_make_track(TEveTrackPropagator* trkProp, Int_t index, AliESDtrack* at,
-			  AliExternalTrackParam* tp=0);
-Bool_t     trackIsOn(AliESDtrack* t, Int_t mask);
-void       trackGetPos(AliExternalTrackParam* tp, Double_t r[3]);
-void       trackGetMomentum(AliExternalTrackParam* tp, Double_t p[3]);
-Double_t   trackGetP(AliExternalTrackParam* tp);
-
-
-// Configuration and global variables.
-
-
-
-TTree *esd_tree          = 0;
-
-AliESDEvent  *esd        = 0;
-AliESDfriend *esd_friend = 0;
-
-Int_t esd_event_id       = 0; // Current event id.
-
-TEveTrackList *track_list = 0;
-
-/******************************************************************************/
-// Initialization and steering functions
-/******************************************************************************/
-
-//______________________________________________________________________________
-void alice_esd_split()
+class AliEvReader : public IEvReader 
 {
-   // Main function, initializes the application.
-   //
-   // 1. Load the auto-generated library holding ESD classes and ESD dictionaries.
-   // 2. Open ESD data-files.
-   // 3. Load cartoon geometry.
-   // 4. Spawn simple GUI.
-   // 5. Load first event.
+
+   public:
+
+      enum ESDTrackFlags {
+               kITSin=0x0001,kITSout=0x0002,kITSrefit=0x0004,kITSpid=0x0008,
+               kTPCin=0x0010,kTPCout=0x0020,kTPCrefit=0x0040,kTPCpid=0x0080,
+               kTRDin=0x0100,kTRDout=0x0200,kTRDrefit=0x0400,kTRDpid=0x0800,
+               kTOFin=0x1000,kTOFout=0x2000,kTOFrefit=0x4000,kTOFpid=0x8000,
+               kHMPIDpid=0x20000,
+               kEMCALmatch=0x40000,
+               kTRDbackup=0x80000,
+               kTRDStop=0x20000000,
+               kESDpid=0x40000000,
+               kTIME=0x80000000
+      };
+
+
+     void AliEvReader() 
+     {
+         esd_tree = 0 ;
+         esd = 0 ;
+         esd_friend = 0 ;
+     }
+
+     void Read();   // updates the track_list
+     Bool_t LoadProject(const char* file , const char* project);
+     Bool_t InitProject(const char* file );
+
+     TEveTrack* esd_make_track(TEveTrackPropagator* trkProp, Int_t index, AliESDtrack* at, AliExternalTrackParam* tp=0);
+     Double_t trackGetP(AliExternalTrackParam* tp);
+     void     trackGetMomentum(AliExternalTrackParam* tp, Double_t p[3]);
+     void     trackGetPos(AliExternalTrackParam* tp, Double_t r[3]);
+     Bool_t   trackIsOn(AliESDtrack* t, Int_t mask);
+
+
+     static const char* esd_file_name ;
+     static const char* esd_friends_file_name ;
+     static const char* project_name ;
+
+   private:
+       TTree *  esd_tree ;
+       AliESDEvent* esd  ;
+       AliESDfriend *esd_friend ;
+
+};
+
+const char* AliEvReader::esd_file_name         = "http://root.cern.ch/files/alice_ESDs.root";
+const char* AliEvReader::esd_friends_file_name = "http://root.cern.ch/files/alice_ESDfriends.root";
+const char* AliEvReader::project_name          = "aliesd" ;
+
+Bool_t AliEvReader::InitProject(const char* file  )
+{
+
+   printf("*** AliEvReader::InitProject %s \n", file  ); 
 
    TFile::SetCacheFileDir(".");
-
-   const char* esd_file_name         = "http://root.cern.ch/files/alice_ESDs.root";
-   if (!alice_esd_loadlib(esd_file_name, "aliesd"))
+   if (!LoadProject(file , project_name ))
    {
       Error("alice_esd", "Can not load project libraries.");
-      return;
+      return kFalse;
    }
 
-   printf("*** Opening ESD ***\n");
-   TFile *esd_file          = 0;
-   esd_file = TFile::Open(esd_file_name, "CACHEREAD");
+   printf("*** AliEvReader::InitProject  Opening ESD ***\n");
+   TFile *  esd_file          = 0;
+   esd_file = TFile::Open(file , "CACHEREAD");
    if (!esd_file)
       return;
 
-   printf("*** Opening ESD-friends ***\n");
-   const char* esd_friends_file_name = "http://root.cern.ch/files/alice_ESDfriends.root";
+   printf("*** Opening ESD-friends %s ***\n", esd_friends_file_name );
    TFile *esd_friends_file  = 0;
    esd_friends_file = TFile::Open(esd_friends_file_name, "CACHEREAD");
    if (!esd_friends_file)
       return;
 
    esd_tree = (TTree*) esd_file->Get("esdTree");
-
-   esd = (AliESDEvent*) esd_tree->GetUserInfo()->FindObject("AliESDEvent");
-
-   // Set the branch addresses.
-   {
-      TIter next(esd->fESDObjects);
-      TObject *el;
-      while ((el=(TNamed*)next()))
-      {
-         TString bname(el->GetName());
-         if(bname.CompareTo("AliESDfriend")==0)
-         {
-            // AliESDfriend needs some '.' magick.
-            esd_tree->SetBranchAddress("ESDfriend.", esd->fESDObjects->GetObjectRef(el));
-         }
-         else
-         {
-            esd_tree->SetBranchAddress(bname, esd->fESDObjects->GetObjectRef(el));
-         }
-      }
+   if(!esd_tree){
+       Error("alice_esd", "cannot access tree " );
+   } else {
+       printf("*** accessed tree *** \n");  
    }
 
 
-   EvDisp::EvDisp();
-   gROOT->ProcessLine(".x evd_geometry.C");
-   EvDisp::make_gui();
-   EvDisp::import_projection_geometry();
+   esd = (AliESDEvent*) esd_tree->GetUserInfo()->FindObject("AliESDEvent");
+   if(!esd){
+       Error("alice_esd", "cannot access esd " );
+   } else {
+       printf("*** accessed esd *** \n");  
+   }
 
-   load_event();
 
-   EvDisp::update_projections();
-   gEve->Redraw3D(kTRUE); // Reset camera after the first event has been shown.
+   // Set the branch addresses.
+      
+   printf("*** start setting branch addresses  ***\n");
+   TIter next(esd->fESDObjects);
+   TNamed* el = 0 ;
+   while ((el=(TNamed*)next())){
+         TString bname(el->GetName());
+         if(bname.CompareTo("AliESDfriend")==0) {    // AliESDfriend needs some '.' magick.
+            esd_tree->SetBranchAddress("ESDfriend.", esd->fESDObjects->GetObjectRef(el));
+         } else {
+            esd_tree->SetBranchAddress(bname, esd->fESDObjects->GetObjectRef(el));
+         }
+   }
+  
+   printf("*** done setting branch addresses  ***\n");
+
 }
 
+
+
 //______________________________________________________________________________
-Bool_t alice_esd_loadlib(const char* file, const char* project)
+Bool_t AliEvReader::LoadProject(const char* file, const char* project)
 {
+   // formerly alice_esd_loadlib
    // Make sure that shared library created from the auto-generated project
    // files exists and load it.
 
    TString lib(Form("%s/%s.%s", project, project, gSystem->GetSoExt()));
+   printf("*** AliEvReader::LoadProject lib:%s file:%s  ***\n", lib.Data(), file );
+   
+   TFile::SetCacheFileDir(".");
 
    if (gSystem->AccessPathName(lib, kReadPermission)) {
       TFile* f = TFile::Open(file, "CACHEREAD");
-      if (f == 0)
+      if (f == 0){
          return kFALSE;
+      }
       f->MakeProject(project, "*", "++");
       f->Close();
       delete f;
+   } else {
+     printf("*** AliEvReader::LoadProject failed to access " ); 
    }
    return gSystem->Load(lib) >= 0;
 }
 
-//______________________________________________________________________________
-void load_event()
-{
-   // Load event specified in global esd_event_id.
-   // The contents of previous event are removed.
 
-   printf("Loading event %d.\n", esd_event_id);
-   gTextEntry->SetTextColor(0xff0000);
-   gTextEntry->SetText(Form("Loading event %d...",esd_event_id));
-   gSystem->ProcessEvents();
-
-   if (track_list)
-      track_list->DestroyElements();
-
-   esd_tree->GetEntry(esd_event_id);
-
-   alice_esd_read();  // updates the track_list
-
-   gEve->Redraw3D(kFALSE, kTRUE);
-   gTextEntry->SetTextColor(0x000000);
-   gTextEntry->SetText(Form("Event %d loaded",esd_event_id));
-   gROOT->ProcessLine("SplitGLView::UpdateSummary()");
-}
 
 //______________________________________________________________________________
-
-
-
-
-//
-// namespace class to start tidy up/structuring 
-// ... the idea being to factor all non-expt specifics into here 
-//
-// what is the state of an event display ... (mile high view )
-//      .. event id
-//
-
-class EvDisp
+void AliEvReader::Read()   
 {
-public:
-   void EvDisp(){
-       if(gEve == NULL){ 
-           TEveManager::Create();
-           FitToScreen();
-       }
-   }
-
-   void FitToScreen()
-   {
-       // Adapt the main frame to the screen size...
-       Int_t qq; 
-       UInt_t ww, hh;
-       gVirtualX->GetWindowSize(gVirtualX->GetDefaultRootWindow(), qq, qq, ww, hh);
-       Float_t screen_ratio = (Float_t)ww/(Float_t)hh;
-       if (screen_ratio > 1.5) {
-           gEve->GetBrowser()->MoveResize(100, 50, ww - 300, hh - 100);
-           //gEve->GetBrowser()->SetWMPosition(100, 50);
-       } else {
-           gEve->GetBrowser()->Move(50, 50);
-           //gEve->GetBrowser()->SetWMPosition(50, 50);
-       }
-   }
-
-
-   void import_projection_geometry()
-   {
-       if (gRPhiMgr) {
-          TEveProjectionAxes* a = new TEveProjectionAxes(gRPhiMgr);
-          a->SetNdivisions(3);
-          const TString rpp = "R-Phi Projection" ;
-          gEve->GetScenes()->FindChild(rpp)->AddElement(a);
-          gRPhiMgr->ImportElements(gGeoShape);
-       }
-       if (gRhoZMgr) {
-          TEveProjectionAxes* a = new TEveProjectionAxes(gRhoZMgr);
-          a->SetNdivisions(3);
-          const TString rzp = "Rho-Z Projection" ;
-          gEve->GetScenes()->FindChild(rzp)->AddElement(a);
-          gRhoZMgr->ImportElements(gGeoShape);
-       }
-   }
-
-
-   void update_projections()
-   {
-       // cleanup then import geometry and event 
-       // in the projection managers
-   
-       TEveElement* top = gEve->GetCurrentEvent();
-       if (gRPhiMgr && top) {
-          gRPhiMgr->DestroyElements();
-          gRPhiMgr->ImportElements(gGeoShape);
-          gRPhiMgr->ImportElements(top);
-       }
-       if (gRhoZMgr && top) {
-          gRhoZMgr->DestroyElements();
-          gRhoZMgr->ImportElements(gGeoShape);
-          gRhoZMgr->ImportElements(top);
-       }
-   }
-
-   void make_gui()
-   {
-      // Create minimal GUI for event navigation.
-
-      //gROOT->ProcessLine(".L SplitGLView.C+");
-      //gROOT->ProcessLine(".L SplitGLView.C");
-      gSystem->Load("$ENV_HOME/eve/InstallArea/$CMTCONFIG/lib/libSplitGLView.so");
-   
-
-      TEveBrowser* browser = gEve->GetBrowser();
-      browser->ExecPlugin("SplitGLView", 0, "new SplitGLView(gClient->GetRoot(), 600, 450, kTRUE)");
-
-      browser->StartEmbedding(TRootBrowser::kLeft);
-
-      TGMainFrame* frmMain = new TGMainFrame(gClient->GetRoot(), 1000, 600);
-      frmMain->SetWindowName("XX GUI");
-      frmMain->SetCleanup(kDeepCleanup);
-
-      TGHorizontalFrame* hf = new TGHorizontalFrame(frmMain);
-      {
-      
-          TString icondir( Form("%s/icons/", gSystem->Getenv("ROOTSYS")) );
-          TGPictureButton* b = 0;
-          EvNavHandler    *fh = new EvNavHandler;
-
-          b = new TGPictureButton(hf, gClient->GetPicture(icondir + "GoBack.gif"));
-          hf->AddFrame(b, new TGLayoutHints(kLHintsLeft | kLHintsCenterY, 10, 2, 10, 10));
-          b->Connect("Clicked()", "EvNavHandler", fh, "Bck()");
-
-          b = new TGPictureButton(hf, gClient->GetPicture(icondir + "GoForward.gif"));
-          hf->AddFrame(b, new TGLayoutHints(kLHintsLeft | kLHintsCenterY, 2, 10, 10, 10));
-          b->Connect("Clicked()", "EvNavHandler", fh, "Fwd()");
-
-          gTextEntry = new TGTextEntry(hf);
-          gTextEntry->SetEnabled(kFALSE);
-          hf->AddFrame(gTextEntry, new TGLayoutHints(kLHintsLeft | kLHintsCenterY  | 
-                       kLHintsExpandX, 2, 10, 10, 10));
-       }
-       frmMain->AddFrame(hf, new TGLayoutHints(kLHintsTop | kLHintsExpandX,0,0,20,0));
-
-       gProgress = new TGHProgressBar(frmMain, TGProgressBar::kFancy, 100);
-       gProgress->ShowPosition(kTRUE, kFALSE, "%.0f tracks");
-       gProgress->SetBarColor("green");
-       frmMain->AddFrame(gProgress, new TGLayoutHints(kLHintsExpandX, 10, 10, 5, 5));
-
-       frmMain->MapSubwindows();
-       frmMain->Resize();
-       frmMain->MapWindow();
-
-       browser->StopEmbedding();
-       browser->SetTabTitle("Event Control", 0);
-    }
-
-};
-
-
-
-class EvNavHandler
-{
-public:
-   void Fwd()
-   {
-      if (esd_event_id < esd_tree->GetEntries() - 1) {
-         ++esd_event_id;
-         load_event();
-         Evd::update_projections();
-      } else {
-         gTextEntry->SetTextColor(0xff0000);
-         gTextEntry->SetText("Already at last event");
-         printf("Already at last event.\n");
-      }
-   }
-   void Bck()
-   {
-      if (esd_event_id > 0) {
-         --esd_event_id;
-         load_event();
-         Evd::update_projections();
-      } else {
-         gTextEntry->SetTextColor(0xff0000);
-         gTextEntry->SetText("Already at first event");
-         printf("Already at first event.\n");
-      }
-   }
-};
-
-
-/******************************************************************************/
-// Code for reading AliESD and creating visualization objects
-/******************************************************************************/
-
-enum ESDTrackFlags {
-   kITSin=0x0001,kITSout=0x0002,kITSrefit=0x0004,kITSpid=0x0008,
-   kTPCin=0x0010,kTPCout=0x0020,kTPCrefit=0x0040,kTPCpid=0x0080,
-   kTRDin=0x0100,kTRDout=0x0200,kTRDrefit=0x0400,kTRDpid=0x0800,
-   kTOFin=0x1000,kTOFout=0x2000,kTOFrefit=0x4000,kTOFpid=0x8000,
-   kHMPIDpid=0x20000,
-   kEMCALmatch=0x40000,
-   kTRDbackup=0x80000,
-   kTRDStop=0x20000000,
-   kESDpid=0x40000000,
-   kTIME=0x80000000
-};
-
-//______________________________________________________________________________
-void alice_esd_read()
-{
+   // formerly alice_esd_read
    // Read tracks and associated clusters from current event.
 
    AliESDRun    *esdrun = (AliESDRun*)    esd->fESDObjects->FindObject("AliESDRun");
@@ -356,6 +201,9 @@ void alice_esd_read()
    // This needs further investigation. Clusters not shown.
    // AliESDfriend *frnd   = (AliESDfriend*) esd->fESDObjects->FindObject("AliESDfriend");
    // printf("Friend %p, n_tracks:%d\n", frnd, frnd->fTracks.GetEntries());
+
+
+   TEveTrackList* track_list = gEvNav->GetTrackList() ;
 
    if (track_list == 0) {
       track_list = new TEveTrackList("ESD Tracks"); 
@@ -400,7 +248,7 @@ void alice_esd_read()
 }
 
 //______________________________________________________________________________
-TEveTrack* esd_make_track(TEveTrackPropagator*   trkProp,
+TEveTrack* AliEvReader::esd_make_track(TEveTrackPropagator*   trkProp,
 			  Int_t                  index,
 			  AliESDtrack*           at,
 			  AliExternalTrackParam* tp)
@@ -436,7 +284,7 @@ TEveTrack* esd_make_track(TEveTrackPropagator*   trkProp,
 }
 
 //______________________________________________________________________________
-Bool_t trackIsOn(AliESDtrack* t, Int_t mask)
+Bool_t AliEvReader::trackIsOn(AliESDtrack* t, Int_t mask)
 {
    // Check is track-flag specified by mask are set.
 
@@ -444,7 +292,7 @@ Bool_t trackIsOn(AliESDtrack* t, Int_t mask)
 }
 
 //______________________________________________________________________________
-void trackGetPos(AliExternalTrackParam* tp, Double_t r[3])
+void AliEvReader::trackGetPos(AliExternalTrackParam* tp, Double_t r[3])
 {
    // Get global position of starting point of tp.
 
@@ -455,7 +303,7 @@ void trackGetPos(AliExternalTrackParam* tp, Double_t r[3])
 }
 
 //______________________________________________________________________________
-void trackGetMomentum(AliExternalTrackParam* tp, Double_t p[3])
+void AliEvReader::trackGetMomentum(AliExternalTrackParam* tp, Double_t p[3])
 {
    // Return global momentum vector of starting point of tp.
 
@@ -468,9 +316,152 @@ void trackGetMomentum(AliExternalTrackParam* tp, Double_t p[3])
 }
 
 //______________________________________________________________________________
-Double_t trackGetP(AliExternalTrackParam* tp)
+Double_t AliEvReader::trackGetP(AliExternalTrackParam* tp)
 {
    // Return magnitude of momentum of tp.
 
    return TMath::Sqrt(1.+ tp->fP[3]*tp->fP[3])/TMath::Abs(tp->fP[4]);
 }
+
+
+
+class EvReaderFactory
+{
+  public:
+
+     enum ReaderType { kAlice , kAberdeen };
+     static IEvReader* GetReader(ReaderType rt )
+     {
+         if( gEvReader == 0 ) gEvReader = MakeReader( rt = kAlice );
+         return gEvReader ;
+     }
+
+  private:
+
+    static IEvReader* gEvReader ; 
+    static IEvReader* MakeReader(ReaderType rt)
+    {
+       switch(rt)
+       {
+          case kAlice:
+              return new AliEvReader ;
+          default:
+              return new AliEvReader ;
+       }
+    }
+
+};
+
+
+IEvReader* EvReaderFactory::gEvReader = 0 ;
+
+
+
+class EvNav
+{
+public:
+
+   void EvNav() 
+   {
+      esd_event_id = 0 ;
+      track_list = 0 ;
+      if ( gEvNav == 0 ) gEvNav = this ; 
+   }
+
+   EvNav* GetGlobal()
+   {
+      if(gEvNav == 0 ) gEvNav = new EvNav ;
+      return gEvNav ;
+   }
+
+   void Fwd()
+   {
+      if (esd_event_id < esd_tree->GetEntries() - 1) {
+         ++esd_event_id;
+         EvNav::load_event();
+         EvD::update_projections();
+      } else {
+         gTextEntry->SetTextColor(0xff0000);
+         gTextEntry->SetText("Already at last event");
+         printf("Already at last event.\n");
+      }
+   }
+   void Bck()
+   {
+      if (esd_event_id > 0) {
+         --esd_event_id;
+         EvNav::load_event();
+         EvD::update_projections();
+      } else {
+         gTextEntry->SetTextColor(0xff0000);
+         gTextEntry->SetText("Already at first event");
+         printf("Already at first event.\n");
+      }
+   }
+
+
+   void load_event()
+   {
+   // Load event specified in global esd_event_id.
+   // The contents of previous event are removed.
+
+      printf("Loading event %d.\n", esd_event_id);
+      gTextEntry->SetTextColor(0xff0000);
+      gTextEntry->SetText(Form("Loading event %d...",esd_event_id));
+      gSystem->ProcessEvents();
+
+
+      IEvReader* er = EvReaderFactory::GetReader();
+
+
+      if (track_list)
+         track_list->DestroyElements();
+
+      esd_tree->GetEntry(esd_event_id);
+
+      er->Read();
+
+      gEve->Redraw3D(kFALSE, kTRUE);
+      gTextEntry->SetTextColor(0x000000);
+      gTextEntry->SetText(Form("Event %d loaded",esd_event_id));
+      gROOT->ProcessLine("SplitGLView::UpdateSummary()");
+   }
+
+
+   TEveTrackList* GetTrackList(){ 
+       return track_list ; 
+   }
+   Int_t GetEventId(){            
+       return esd_event_id ;  
+   }
+
+private:
+    Int_t esd_event_id  ;  // esd_ is historical ... to be changed
+    TEveTrackList* track_list ;
+    
+   
+};
+
+
+//______________________________________________________________________________
+void alice_esd_split()
+{
+
+   IEvReader* er = EvReaderFactory::GetReader(EvReaderFactory::kAlice);
+   er->InitProject( AliEvReader::esd_file_name );
+
+   gROOT->ProcessLine(".x EvD.C");
+   EvD::EvD();
+   gROOT->ProcessLine(".x evd_geometry.C");
+   EvD::make_gui();
+   EvD::import_projection_geometry();
+
+   gEvNav->load_event();
+
+   EvD::update_projections();
+
+
+   gEve->Redraw3D(kTRUE); // Reset camera after the first event has been shown.
+}
+
+
