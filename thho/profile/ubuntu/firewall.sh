1b44c7c5ab489b6841944ae03aba08b4a02ffaa6
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2009-02-24 18:18:51 +0800

    make the firewall setting default

--- thho/profile/ubuntu/firewall.sh
+++ thho/profile/ubuntu/firewall.sh
@@ -3,11 +3,14 @@
 # It's not suitable to be used on a server.
 #
 # usage:
-#   add the line:
 #
-#   source $HOME/env/thho/profile/ubuntu/firewall.sh
+#   ln -s firewall.sh /etc/init.d/firewall.sh
+#   chmod 775 firewall.sh
+#   update-rc.d -f firewall.sh defaults
 #
-#   in /etc/rc.local
+#
+#   ref:
+#       http://www.ubuntu-tw.org/modules/newbb/viewtopic.php?topic_id=10836
 #
 #
 #PATH=/sbin:/bin:/usr/sbin:/usr/bin
@@ -21,4 +24,4 @@ iptables -P FORWARD ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
-iptables -L >> $HOME/iptables_firewall.log
+#iptables -L >> $HOME/iptables_firewall.log
