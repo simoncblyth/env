5366f13c52aa2b61e1718d016f87efc2fda3a9e1
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2009-04-22 11:38:33 +0800

    add fix n and k plots

--- thho/Acrylic_Optical/acrylicsOpticalPara/OpticalModelPlotting_2D.C
+++ thho/Acrylic_Optical/acrylicsOpticalPara/OpticalModelPlotting_2D.C
@@ -25,37 +25,65 @@
 using namespace std;
 
 void OpticalModelPlotting2D(Double_t nmin, Double_t nmax, 
-                            Double_t alphamin, Double_t alphamax, Double_t d,
+                            Double_t kmin, Double_t kmax, Double_t d,
                             Double_t lambda,
-                            Int_t parNo) {
+                            Int_t parNo, Double_t nfix, Double_t kfix) {
 
     TCanvas *c1 = new TCanvas(
         "c1","Optical Model T and R",200,10,700,900);
-    title = new TPaveText(.2,0.96,.8,.995);
-    title->AddText("Optical Model T and R");
-    title->Draw();
+    c1->Divide(2,3);
+    //title = new TPaveText(.2,0.96,.8,.995);
+    //title->AddText("Optical Model T and R");
+    //title->Draw();
 
-    pad1 = new TPad("pad1","TransmittancePad",0.03,0.50,0.98,0.95,21);
-    pad2 = new TPad("pad2","ReflectancePad",0.03,0.02,0.98,0.48,21);
-    pad1->Draw();
-    pad2->Draw();
+    //pad1 = new TPad("pad1","TransmittancePad",0.03,0.50,0.98,0.95,21);
+    //pad2 = new TPad("pad2","ReflectancePad",0.03,0.02,0.98,0.48,21);
+    //pad1->Draw();
+    //pad2->Draw();
 
+    c1->cd(1);
     TF2 *tt = new
-        TF2("TRansmittance", GetTFormula,nmin,nmax,alphamin,alphamax,parNo);
-    pad1->cd();
-    pad1->SetPhi(-80);
+        TF2("Transmittance", GetTFormula,nmin,nmax,kmin,kmax,parNo);
+    //pad1->cd();
+    //pad1->SetPhi(-80);
     tt->SetParameters(d,lambda);
     // color mesh
     tt->Draw("surf1");
     // gouraud shading
     //tt->Draw("surf4");
 
-
+    c1->cd(2);
     TF2 *rr = new
-        TF2("Reflectance", GetRFormula,nmin,nmax,alphamin,alphamax,parNo);
-    pad2->cd();
+        TF2("Reflectance", GetRFormula,nmin,nmax,kmin,kmax,parNo);
+    //pad2->cd();
     rr->SetParameters(d,lambda);
     rr->Draw("surf1");
+
+    c1->cd(3);
+    TF1 *ttk = new
+        TF1("Transmittance of fix n",GetTFormulaK,kmin,kmax,parNo+1);
+    ttk->SetParameters(d,lambda,nfix);
+    ttk->Draw();
+
+    c1->cd(4);
+    TF1 *rrk = new
+        TF1("Reflectance of fix n",GetRFormulaK,kmin,kmax,parNo+1);
+    rrk->SetParameters(d,lambda,nfix);
+    rrk->Draw();
+
+    c1->cd(5);
+    TF1 *ttn = new
+        TF1("Transmittance of fix k",GetTFormulaN,nmin,nmax,parNo+1);
+    ttn->SetParameters(d,lambda,kfix);
+    ttn->Draw();
+
+    c1->cd(6);
+    TF1 *rrn = new
+        TF1("Reflectance of fix k",GetRFormulaN,nmin,nmax,parNo+1);
+    rrn->SetParameters(d,lambda,kfix);
+    rrn->Draw();
+
+
 
 }
 
@@ -93,6 +121,36 @@ Double_t GetRFormula(Double_t *x, Double_t *par) {
         GetOpticalModelTValue(GetIT(x[1],par[0],par[1]),GetFR(x[0],x[1])),
         GetIT(x[1],par[0],par[1]),
         GetFR(x[0],x[1]));
+
+}
+Double_t GetTFormulaN(Double_t *x, Double_t *par) {
+
+    return GetOpticalModelTValue(
+                GetIT(par[2],par[0],par[1]),GetFR(x[0],par[2]));
+
+}
+
+Double_t GetRFormulaN(Double_t *x, Double_t *par) {
+
+    return GetOpticalModelRValue(
+        GetOpticalModelTValue(GetIT(par[2],par[0],par[1]),GetFR(x[0],par[2])),
+        GetIT(par[2],par[0],par[1]),
+        GetFR(x[0],par[2]));
+
+}
+Double_t GetTFormulaK(Double_t *x, Double_t *par) {
+
+    return GetOpticalModelTValue(
+                GetIT(x[0],par[0],par[1]),GetFR(par[2],x[0]));
+
+}
+
+Double_t GetRFormulaK(Double_t *x, Double_t *par) {
+
+    return GetOpticalModelRValue(
+        GetOpticalModelTValue(GetIT(x[0],par[0],par[1]),GetFR(par[2],x[0])),
+        GetIT(x[0],par[0],par[1]),
+        GetFR(par[2],x[0]));
 
 }
 /////////////////////////////////////////////////////////////////////////
