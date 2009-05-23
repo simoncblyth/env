b24d46ddc91249ca5df8b50bbb86dd15d5f8c5e0
Simon C Blyth <simoncblyth@gmail.com>
2009-05-24 02:02:23 +0800

    explore python inplace editing

---
+++ offline/fix.py
@@ -0,0 +1,29 @@
+"""
+       somewhat of a perl -ism 
+          http://effbot.org/librarybook/fileinput.htm
+"""
+        
+import fileinput as fi
+
+class Edit:
+   def __call__(self,path):
+        for line in fi.input(path,inplace=1):self.filter( line,fi.lineno() )
+
+class Odd(Edit):
+    def filter(self,line,n):
+        if n in [1,3,5]:print "%s %s" % ( n , line ),
+class Even(Edit):
+    def filter(self,line,n):
+        if n in [2,4,6]:print "%s %s" % ( n , line ),
+
+
+def make(path):
+    f = file(path,"w")
+    for n in range(10,20):f.write("%s\n"%str(n))
+    f.close()
+ 
+
+if __name__=='__main__':
+    path = "odd.txt"
+    make(path)
+    Even()(path)
