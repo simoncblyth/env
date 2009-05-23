9e7a7e4f6fda2aac50061c4752cc260c5e337270
Simon C Blyth <simoncblyth@gmail.com>
2009-05-23 12:47:12 +0800

    svn-tools turns out to be needed on G ... for hotbackup.py as stock svn does not come with the tools

--- svn/svn-tools.bash
+++ svn/svn-tools.bash
@@ -1,4 +1,25 @@
+svn-tools-vi(){ vi $BASH_SOURCE ; }
+svn-tools-usage(){
+  cat << EOU
 
+      svn-tools-dir : $(svn-tools-dir)
+      svn-tools-url : $(svn-tools-url)
+      svn-tools-hotbackup : $(svn-tools-hotbackup)
+
+
+      svn-tools-build
+          do the below
+
+      svn-tools-get
+          checkout or update     
+      svn-tools-prepare
+          fill in the template to create the hotbackup tool
+
+
+
+EOU 
+
+}
 
 svn-tools-env(){
 
@@ -6,25 +27,48 @@ svn-tools-env(){
    svn-
 }
 
+
+svn-tools-dir(){       echo $(local-base)/env/svn ; }
+svn-tools-url(){       echo http://svn.collab.net/repos/svn/trunk/tools/ ; }
+svn-tools-hotbackup(){ echo $(svn-tools-dir)/backup/hot-backup.py ; } 
+
+
+svn-tools-build(){
+
+   svn-tools-get
+   svn-tools-prepare
+}
+
+
 svn-tools-get(){
 
-   local dir=$LOCAL_BASE/svn
-   [ -d $dir ] || $SUDO mkdir -p $dir && $SUDO chown $USER $dir   
+   local dir=$(svn-tools-dir)
+   [ ! -d $dir ] && $SUDO mkdir -p $dir && $SUDO chown $USER $dir   
    cd $dir
-   [ -d "tools" ] && svn update tools || svn co http://svn.collab.net/repos/svn/trunk/tools/
+   [ -d "tools" ] && svn update tools || svn co 
 
 }
 
 
-svn-tools-fill-tmpl(){
+svn-tools-prepare(){
 
-   cd $LOCAL_BASE/svn/tools/backup
+   local msg="=== $FUNCNAME :"
+   local iwd=$PWD
+   local path=$(svn-tools-hotbackup)
+   local dir=$(dirname $path)
+   local name=$(basename $path)
+
+   [ ! -f "$dir" ] && echo $msg ABORT no $dir && return 1
+
+   cd $dir
    
-   local name=hot-backup.py
-   perl -p -e 's/\@SVN_BINDIR\@/\/usr\/bin/g' $name.in  > $name
+   perl -p -e 's/\@SVN_BINDIR\@/\/usr\/bin/g' $name.in  > $name   
+   echo $msg creating $name from $name.in in $dir
+
    diff $name.in $name
    chmod u+x $name
 
    which svnlook
    which svnadmin
+   cd $iwd
 }
