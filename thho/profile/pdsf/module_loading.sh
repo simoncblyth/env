9413ddea9b3aa145bfd796c5919a5ad6891a9689
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2009-06-03 14:57:46 +0800

    thho pdsf bash profile

---
+++ thho/profile/pdsf/module_loading.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# module_loading.sh
+module load subversion/1.4.0
+module load load CMT/v1r18p20061003
---
+++ thho/profile/pdsf/tty_profile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# tty_profile
+################### prompt etc. setting ############################
+export PS1='[\u@\h \w #\#]\$ '
+################### alias setting #####################
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias ll='ls -alh --color=auto'
+alias ls='ls --color=auto'
