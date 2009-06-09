435283d08fb1a1962004dc2c8971ceab42c5cf97
Simon C Blyth <simoncblyth@gmail.com>
2009-06-09 10:54:30 +0800

    try using {{{nosetests -A attribute-expression}}}

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,34 +1,25 @@
 """
 
-nosetests -v
-test_simple.test_red ... ok
-test_simple.test_green ... ok
-test_simple.test_blue ... ok
-----------------------------------------------------------------------
-Ran 3 tests in 0.334s
-OK
+    nosetests -v
 
+    Test selection via attributes on the tests :
+        nosetests -v -a 'daily'
+        nosetests -v -a '!daily'
 
-nosetests -v -a 'daily'
-test_simple.test_blue ... ok
-----------------------------------------------------------------------
-Ran 1 test in 0.038s
-OK
-
-
-nosetests -v -a '!daily'
-test_simple.test_red ... ok
-test_simple.test_green ... ok
-----------------------------------------------------------------------
-Ran 2 tests in 0.036s
-OK
-  
+    Test selection via an expression of the attributes :
+        nosetests -v -A "minutes < 11 "
+        nosetests -v -A "minutes < 6 "
+ 
 
 """
 
 def test_red():pass
 def test_green():pass
 def test_blue():pass
+
+test_red.minutes = 5 
+test_green.minutes = 10
+test_blue.minutes = 15
 
 test_blue.daily = True
 
