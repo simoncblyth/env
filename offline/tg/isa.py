0d9088a3cb534a1833d2f18f5b8b8a80647a01fb
Simon C Blyth <simoncblyth@gmail.com>
2009-06-19 12:37:12 +0800

    SqlSoup introspection + FK joining DBI tables

--- offline/tg/isa.py
+++ offline/tg/isa.py
@@ -1,8 +1,32 @@
 
-from env.base.private import Private
-p = Private()
-from sqlalchemy import create_engine, MetaData
-e = create_engine( p('DATABASE_URL') )
-from sqlalchemy.ext.sqlsoup import SqlSoup
-db = SqlSoup( MetaData(e) )
-print db.SimPmtSpec.all()
+
+class DBI(dict):
+    def __init__(self, dburl):
+        """
+            DB access and instrospection ...
+        """
+        from sqlalchemy import create_engine, MetaData
+        e = create_engine( dburl )
+        from sqlalchemy.ext.sqlsoup import SqlSoup
+        db = SqlSoup( MetaData(e) )
+        self.db = db
+
+    def __call__(self):
+        """
+            Manually apply FK joins between payload and validity DBI tables 
+            This is required because MySQL(MyISAM) tables do not retain FK constraints
+        """
+        db = self.db
+        for t in [n[0:-3] for n in self.db.engine.table_names() if n.endswith('Vld')]:
+            self[t] = db.join( getattr(db,t), getattr(db,"%sVld"%t), getattr(db,t).SEQNO == getattr(db,"%sVld"%t).SEQNO , isouter=False )
+        return self
+
+
+
+if __name__=='__main__':
+    from env.base.private import Private
+    p = Private()
+    dbi = DBI(p('DATABASE_URL'))()
+
+    print dbi['SimPmtSpec'].first()
+
