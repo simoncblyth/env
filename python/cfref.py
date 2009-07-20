871205abb193dd8486a8bcd0ddab8709b1089ff7
Simon C Blyth <simoncblyth@gmail.com>
2009-07-20 12:45:13 +0800

    cfref refactor stream redirection/capture in preparation for gaudi

--- python/cfref.py
+++ python/cfref.py
@@ -1,49 +1,61 @@
 #!/usr/bin/env python 
 
+
+"""
+
+def test11ostreamcallback(self) :
+   def test11do_msg(s) : self.got_it = True
+   self.got_it = False
+   buf  = GaudiPython.CallbackStreamBuf(test11do_msg)
+   ostream = GaudiPython.gbl.basic_ostream('char','char_traits<char>')(buf)
+   msgSvc = g.service('MessageSvc', GaudiPython.gbl.IMessageSvc)
+   original = msgSvc.defaultStream()
+   msgSvc.setDefaultStream(ostream)
+   msgSvc.reportMessage('TEST',7,'This is a test message')
+   msgSvc.setDefaultStream(original)
+   self.failUnless(self.got_it)
+
+
+"""
 import sys
+import os
+
+class CaptureStdout(list):
+    def __init__(self):
+        from cStringIO import StringIO
+        self.buf = StringIO() 
+        sys.stdout =  self.buf
+    def __call__(self):
+        sys.stdout = sys.__stdout__ 
+        for l in self.buf.getvalue().split("\n"): self.append("%s\n" % l)
+    def __repr__(self):
+        return "".join(["%-4d %s" % (n,l)  for n,l in enumerate(self)] )
+         
 
 class CfRef(list):
-    """
-        tryout object wrapping for 
-        functionality of cf_stdout.py
-
-        pause/continue not working 
-
-    """
-    def __init__(self, path , start=True ):
+    def __init__(self, path , captor ):
         self.path = path
-        from cStringIO import StringIO
-        self.cur = StringIO()
-        if start:
-            self.start_()
+        print "%s start capture ... " % self.__class__.__name__ 
+        self.capture = captor()
 
-    def captured(self):return ["%s\n" % l  for l in self.cur.getvalue().split("\n")]
-    def start_(self):sys.stdout = self.cur    ## start capturing 
-    def __call__(self):self.stop_(compare=True)
-    def pause_(self):   self.stop_(compare=False)
-    def continue_(self):self.start_()
-
-    def stop_(self, compare=True):
-        if sys.stdout == self.cur:
-            sys.stdout = sys.__stdout__  ## reset stdout 
-        if compare:
-            self.compare()
-
-    def compare(self):
-        import os
+    def __call__(self):
+        self.capture()
+        print "%s stop capture : collected %s lines " % ( self.__class__.__name__ , len(self.capture) ) 
         if os.path.exists(self.path):
+            print "comparing ... %s " % self.path 
             self.compare_()
         else:
+            print "saving ... %s " % self.path
             self.save_()
 
     def save_(self):
         ref = file(self.path,"w")
-        ref.writelines( self.captured() )
+        ref.writelines( self.capture )
         ref.close()
 
     def compare_(self):
         from difflib import unified_diff
-        for l in unified_diff( file(self.path,"r").readlines() , self.captured() ):
+        for l in unified_diff( file(self.path,"r").readlines() , self.capture ):
             self.append( l )
          
 
@@ -56,13 +68,16 @@ def msg(arg, date=False):
 
 
 if __name__=='__main__':
-
-    cf = CfRef( __file__ + ".ref" )
+    cf = CfRef( __file__ + ".ref" , CaptureStdout )
 
     msg("hello")
     msg("hello", date=True)
 
     cf()
+
+
+    print cf.capture
+
 
     if len(cf) == 0:
         print "matched"
