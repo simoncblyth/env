d48703c3b3ff889bf7b1f55f29a1dbb9e237cc33
Simon C Blyth <simoncblyth@gmail.com>
2009-07-21 16:38:50 +0800

    insulate- revivify, see  i:#272

--- nose/package/insulate.bash
+++ nose/package/insulate.bash
@@ -4,26 +4,48 @@ insulate-usage(){
    cat << EOU
    
       http://code.google.com/p/insulatenoseplugin/wiki/Documentation
-   
-     env-rsync nose 
-     env-rsync python
-  
 
-     [blyth@cms01 NoseTests]$ nosetests -v --with-insulate 
+
+       nosetests -v --with-insulate --insulate-every-test 
 
  
 EOU
 }
 
-insulate-env(){
-  elocal-
-  package-  
-}
+insulate-env(){ elocal- ; }
+insulate-rev(){       echo 38 ; }
+insulate-patchpath(){ echo $(env-home)/trac/patch/insulate/insulate-trunk-$(insulate-rev).patch ; }
+insulate-url(){       echo http://insulatenoseplugin.googlecode.com/svn/trunk ; }
+insulate-name(){      echo insulatenoseplugin ; }
 
+insulate-dir(){       echo $(local-base)/env/nose/$(insulate-name) ; }
+insulate-srcdir(){    echo $(insulate-dir)/insulate ; }
+insulate-getdir(){    echo $(dirname $(dirname $(insulate-srcdir))) ; }
 
 insulate-get(){
-
-  easy_install -Z InsulateRunner
-
+   local msg="=== $FUNCNAME :"
+   local dir=$(insulate-getdir) 
+   mkdir -p $dir && cd $dir
+   local cmd="svn checkout $(insulate-url)@$(insulate-rev) $(insulate-name) "
+   echo $msg $cmd ... from $PWD
+   eval $cmd
+   local pp=$(insulate-patchpath)
+   [ ! -f "$pp" ] && echo $msg no patch $pp && return 0
+   cd $(insulate-dir)
+   patch -p0 < $pp
 }
 
+insulate-install(){ 
+
+   # just a link is not sufficient for nosetests -p 
+   #python-
+   #python-ln $(insulate-srcdir)  
+
+  cd $(insulate-dir)
+  python setup.py develop
+}
+
+insulate-ez(){ easy_install -Z InsulateRunner ; }
+insulate-chk(){
+  python -c "import insulate as _ ; print _.__file__ "
+}
