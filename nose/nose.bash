c6fcb678042f0a899bd83820878ea9bd3d253eda
Simon C Blyth <simoncblyth@gmail.com>
2009-07-21 14:11:51 +0800

    insulate- ... move away from using package- ... that was claimed/engulfed by trac auto building

--- nose/nose.bash
+++ nose/nose.bash
@@ -1,21 +1,15 @@
-
-
+nose-vi(){ vi $BASH_SOURCE ; }
 nose-usage(){
 
    cat << EOU
 
    hmm these are downloading into 
       eg /usr/local/env/trac/package/insulate 
-
-
    
 EOU
 
 
-
 }
-
-
 
 
 insulate-(){   . $ENV_HOME/nose/package/insulate.bash && insulate-env $* ; } 
@@ -23,9 +17,6 @@ nosenose-(){   . $ENV_HOME/nose/package/nosenose.bash && nosenose-env $* ; }
 xmlnose-(){    . $ENV_HOME/nose/package/xmlnose.bash  && xmlnose-env  $* ; } 
 
 _xmlnose(){    python $ENV_HOME/xmlnose/main.py --with-xml-output $* ; }
-
-
-
 
 
 
--- nose/package/insulate.bash
+++ nose/package/insulate.bash
@@ -1,58 +1,29 @@
+insulate-vi(){ vi $BASH_SOURCE ; }
 insulate-usage(){
    package-fn  $FUNCNAME $*
    cat << EOU
    
       http://code.google.com/p/insulatenoseplugin/wiki/Documentation
    
-   As hfag is down agin ...
-   
-   simon:fork blyth$ scp /Users/blyth/env/trac/patch/insulate/insulate-trunk-38.patch P:env/trac/patch/insulate/
-Scientific Linux CERN Release 3.0.8 (SL)
-insulate-trunk-38.patch        
-   
-   
      env-rsync nose 
      env-rsync python
-   
-   
+  
+
+     [blyth@cms01 NoseTests]$ nosetests -v --with-insulate 
+
+ 
 EOU
 }
 
 insulate-env(){
   elocal-
   package-  
-  export INSULATE_BRANCH=trunk
-}
-
-insulate-url(){     echo http://insulatenoseplugin.googlecode.com/svn/$(insulate-branch) ; }
-insulate-package(){ echo insulate ; }
-
-insulate-fix(){
-   cd $(insulate-dir)   
-   echo no fixes
 }
 
 
-insulate-develop(){   package-fn $FUNCNAME $* ; }
-insulate-makepatch(){ package-fn $FUNCNAME $* ; }
-insulate-patchpath(){ package-fn $FUNCNAME $* ; }
+insulate-get(){
 
-insulate-branch(){    package-fn $FUNCNAME $* ; }
-insulate-basename(){  package-fn $FUNCNAME $* ; }
-insulate-dir(){       package-fn $FUNCNAME $* ; } 
-insulate-egg(){       package-fn $FUNCNAME $* ; }
-insulate-get(){       package-fn $FUNCNAME $* ; }
+  easy_install -Z InsulateRunner
 
-insulate-install(){   package-fn $FUNCNAME $* ; }
-insulate-uninstall(){ package-fn $FUNCNAME $* ; }
-insulate-reinstall(){ package-fn $FUNCNAME $* ; }
-insulate-enable(){    package-fn $FUNCNAME $* ; }
+}
 
-insulate-status(){    package-fn $FUNCNAME $* ; }
-insulate-auto(){      package-fn $FUNCNAME $* ; }
-insulate-diff(){      package-fn $FUNCNAME $* ; } 
-insulate-rev(){       package-fn $FUNCNAME $* ; } 
-insulate-cd(){        package-fn $FUNCNAME $* ; }
-
-insulate-fullname(){  package-fn $FUNCNAME $* ; }
-insulate-update(){    package-fn $FUNCNAME $* ; }
