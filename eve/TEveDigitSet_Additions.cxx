f261f32ae526d99e0f651888cd106033f462b6ba
Simon C Blyth <simoncblyth@gmail.com>
2009-07-28 20:00:34 +0800

    TEveDigitSet additions with an python module, to add the methods

--- eve/TEveDigitSet_Additions.cxx
+++ eve/TEveDigitSet_Additions.cxx
@@ -1,5 +1,6 @@
 #include <TRef.h>
 #include <TEveDigitSet.h>
+#include <TEveUtil.h>
 #include <cstdio>
 
 struct DigitBase_t
@@ -13,20 +14,50 @@ struct DigitBase_t
       virtual ~DigitBase_t() {}
 };
 
-Int_t TEveDigitSet_GetValue(TEveDigitSet* ds, Int_t idx )
+
+Int_t TEveDigitSet_GetDigitValue(TEveDigitSet* ds, Int_t idx )
 {
    DigitBase_t* d = (DigitBase_t*)(ds->GetDigit(idx));
    return d->fValue ;
 }
 
-void TEveDigitSet_PrintValue(TEveDigitSet* ds, Int_t idx )
+void TEveDigitSet_PrintDigit(TEveDigitSet* ds, Int_t idx )
 {
    DigitBase_t* d = (DigitBase_t*)(ds->GetDigit(idx));
-   printf("TEveDigitSet_PrintValue - 0x%lx, id=%d, value=%d \n", (ULong_t) ds, idx, d->fValue );
+   if (ds->GetValueIsColor()){
+      UChar_t* x = (UChar_t*) & d->fValue;
+      printf("TEveDigitSet_PrintDigit - 0x%lx, id=%d, r=%d g=%d b=%d a=%d  \n", (ULong_t) ds, idx, x[0], x[1], x[2], x[3] );
+   } else {
+      printf("TEveDigitSet_PrintDigit - 0x%lx, id=%d, value=%d \n", (ULong_t) ds, idx, d->fValue );
+   }
 }
 
-void TEveDigitSet_SetValue(TEveDigitSet* ds, Int_t idx , Int_t value )
+void TEveDigitSet_SetDigitValue(TEveDigitSet* ds, Int_t idx , Int_t value )
 {
    DigitBase_t* d = (DigitBase_t*)(ds->GetDigit(idx));
    d->fValue = value ;
+}
+
+void TEveDigitSet_SetDigitColor(TEveDigitSet* ds, Int_t idx, Color_t ci )
+{
+   DigitBase_t* d = (DigitBase_t*)(ds->GetDigit(idx));
+   TEveUtil::ColorFromIdx(ci, (UChar_t*) & d->fValue, kTRUE);
+}
+
+void TEveDigitSet_SetDigitColor(TEveDigitSet* ds, Int_t idx, Color_t ci , Float_t ftrans )
+{
+   DigitBase_t* d = (DigitBase_t*)(ds->GetDigit(idx));
+   TEveUtil::ColorFromIdx(ci, (UChar_t*) & d->fValue, (UChar_t)(255*ftrans));
+}
+
+void TEveDigitSet_SetDigitColor(TEveDigitSet* ds, Int_t idx, Float_t r , Float_t g, Float_t b, Float_t a )
+{
+   DigitBase_t* d = (DigitBase_t*)(ds->GetDigit(idx));
+   UChar_t* x = (UChar_t*) & d->fValue;
+
+   x[0] = (UChar_t)(255*r); 
+   x[1] = (UChar_t)(255*g); 
+   x[2] = (UChar_t)(255*b); 
+   x[3] = (UChar_t)(255*a); 
+
 }
--- eve/TEveDigitSet_Additions.py
+++ eve/TEveDigitSet_Additions.py
@@ -1,6 +1,10 @@
 
+import os
 from ROOT import gROOT
-gROOT.ProcessLine(".L TEveDigitSet_Additions.cxx+")
+
+## how to control the dir in which the .so gets created ?
+gROOT.ProcessLine(".L %s/TEveDigitSet_Additions.cxx+" % os.path.dirname(__file__) )
+
 from ROOT import TEveDigitSet_GetDigitValue, TEveDigitSet_SetDigitValue, TEveDigitSet_PrintDigit, TEveDigitSet_SetDigitColor
 from ROOT import TEveDigitSet
 TEveDigitSet.PrintDigit     = TEveDigitSet_PrintDigit
