ec8fa6ae20fe835afba80b4fa84ac0fbb69fb5b6
Simon C Blyth <simoncblyth@gmail.com>
2009-07-30 20:06:38 +0800

    KeyHandler is in EvManager for convenience

--- root/gui/keyhandler/evcore.py
+++ root/gui/keyhandler/evcore.py
@@ -3,6 +3,17 @@ ROOT.PyConfig.GUIThreadScheduleOnce += [ ROOT.TEveManager.Create ]
 ROOT.gROOT.ProcessLine(".L EvManager.cxx+")
 from ROOT import EvManager, g_
 
+"""
+   How to get messages from the C++ side ... like load next event 
+   onto the py side ?
+
+
+"""
+def Handler():
+   btn = ROOT.BindObject( ROOT.gTQSender, ROOT.TGTextButton )
+
+m = ROOT.TPyDispatcher( Handler )
+
 if __name__=='__main__':
     ROOT.PyGUIThread.finishSchedule()
     from ROOT import gEve
