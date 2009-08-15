57d22b191ec161028661cabf3a2a9f0a05f7be22
Simon C Blyth <simoncblyth@gmail.com>
2009-08-16 00:25:25 +0800

    being forceful with the {{{connection}}} succeeds to get both databases created, but all tables are created in each : '''syncdb''' does not consult the custom manager ... not a major issue, as will not be creating, just reading

--- offline/dj/multidb/util/management/commands/multisyncdb.py
+++ offline/dj/multidb/util/management/commands/multisyncdb.py
@@ -3,19 +3,21 @@ from django.core.management.base import NoArgsCommand
 from django.core.management import call_command
 from django.conf import settings
 
+from multidb.util.manager import MultiDBManager
+
+
 class Command(NoArgsCommand):
     help = "Sync multiple databases."
 
     def handle_noargs(self, **options):
+        verbosity = int(options.get('verbosity', 1))
         for name, database in settings.DATABASES.iteritems():
-            print "Running syncdb for %s " % (name,)
-            for key, value in database.iteritems():
-                print "setting %s %s " % ( key, value )
-                setattr(settings, key, value)
-            #from django.conf import settings
-            from django.db import connection
-            print "connection settings: %s connection:%s " % ( connection.settings_dict, connection )
-            call_command('syncdb')
+            if verbosity > 0:
+                print "Running syncdb for %s " % (name,)
+            manager = MultiDBManager(name)
+            import django.db as db  
+            setattr( db , 'connection' ,  manager.get_db_wrapper() )
+            call_command('syncdb', verbosity=verbosity )
 
 
 
