a725f083f67c490157b2527854337905e1210d34
Taihsiang Ho <thho@hep1.phys.ntu.edu.tw>
2009-08-18 19:13:10 +0800

    clean bash script

---
+++ thho/thesis/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -f *.aux
+rm -f *.bbl
+rm -f *.blg
+rm -f *.lof
+rm -f *.log
+rm -f *.toc
+
+
+rm -f ./contents/*.aux
+rm -f ./contents/*.bbl
+rm -f ./contents/*.blg
+rm -f ./contents/*.lof
+rm -f ./contents/*.log
+
