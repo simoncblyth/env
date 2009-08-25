327d8296b4f7dadd76ec59068f48ba4d0fb8970d
Simon C Blyth <simoncblyth@gmail.com>
2009-08-25 21:23:51 +0800

    map to a joined {{{(payload,validity)}}} table allowing all columns to be selected on ... without traversing a relationship (which rum does not currently support.. i think)

--- rum/reflect/a.py
+++ rum/reflect/a.py
@@ -61,12 +61,82 @@ def field_fix( app ):
     """
         attempt to make the fields readonly and all displayed ...
     """
-    for cls,opt in app.resources.items():
+    for cls in app.resources.keys():
         for f in app.fields_for_resource( cls ):
             f.searchable = True
             f.read_only = True
             f.auto = False       ## succeeds to get ROW_COUNTER to appear on payload tables and SEQNO to appear on Vld tables 
-            #print f
+            print f
+
+
+
+class Mapr(dict):
+    """
+        For checking on the mappers that the app is using 
+    """
+    def __init__(self, app):
+        from rumalchemy.util import get_mapper
+        for cls in app.resources.keys():
+            if hasattr(cls._table.c, 'SEQNO' ):
+                print repr(cls._table.c.SEQNO)
+            mapr = get_mapper(cls)
+            self[cls.__name__] = mapr
+            print mapr.c.keys()
+
+class Qry(dict):
+    """
+        For doing test queries using the session etc... hooked up into the app 
+
+      In [2]: q['Simpmtspec']
+      Out[2]: <sqlalchemy.orm.query.Query object at 0x1e31f50>
+      In [3]: q['Simpmtspec'].first()
+      In [4]: q['Simpmtspec'].count()
+      Out[4]: 3169L
+
+    """
+    def __init__(self, app ): 
+        sf = app.repositoryfactory.session_factory
+        for cls in app.resources.keys():
+            self[cls.__name__] = sf.query(cls)
+            print sf.query(cls).first()
+
+
+
+
+
+
+class Repo(dict):
+    """
+           r = Repo(app)
+           print r['Simpmtspec'].get( (1,1 ) )   ## CPK get 
+
+         
+
+    """
+    def __init__(self, app ):
+        for cls in app.resources.keys():
+            repo = app.repositoryfactory( cls )
+            self[cls.__name__] = repo
+            
+            if cls.__name__ == 'Simpmtspec':
+            	qf = repo.queryfactory.get( cls )
+            	from rum.query import Query, eq 
+                
+                # should not need to descend to the SA specialization 
+                # from rumalchemy.query import SAQuery
+            	# saq = SAQuery( eq('ROW', 10 ) , resource=cls ) 
+            	# print list( saq.filter( qs['Simpmtspec'] ) )
+
+                q = Query( eq('ROW', 10 ) )
+                qq = repo.make_query( request_args=q.as_dict() )
+                ss = repo.select(qq)
+                print list(ss)
+
+    def query( self , repo , q ): 
+        return repo.select( repo.queryfactory( repo.resource , request_args = q.as_dict() ) )
+
+
+
 
 
 if __name__=='__main__':
@@ -77,14 +147,15 @@ if __name__=='__main__':
     app = load_app(opts.url,  debug=True )
     field_fix( app )
     app.finalize()
-    rf = app.repositoryfactory  
-    for cls in rf.resources.keys():print repr(cls._table.c.SEQNO)
+
+    m = Mapr( app )
+    q = Qry(  app )
+    r = Repo(  app )
 
     server = loadserver('egg:Paste#http')
     try:
         server(app)
     except (KeyboardInterrupt, SystemExit):
         print "Bye!"
-
 
 
--- vdbi/vdbi/rumalchemy/repository.py
+++ vdbi/vdbi/rumalchemy/repository.py
@@ -6,6 +6,8 @@ from rumalchemy.util import get_mapper, get_foreign_keys
 from sqlalchemy.orm.properties import ColumnProperty
 from sqlalchemy.sql import expression
 from sqlalchemy import MetaData, Table
+from sqlalchemy.orm import column_property
+from sqlalchemy import join 
 
 import os
 
@@ -14,10 +16,53 @@ SKIP_COLUMNS = ('SEQNO','ROW_COUNTER')
 
 class DbiSARepositoryFactory(SARepositoryFactory):
    
-    def dbi_fkc( self, metadata ):
+   
+    def dbi_fk_ojoins(self, soup ):
+        """
+            Manually apply FK joins between payload and validity DBI tables 
+            This is required because MySQL(MyISAM) tables do not retain FK constraints
+            
+            this is joining via the  object level ... hence the lowercased vld
+            
+            the rumalchemy sqlsoup does not have the join method
+            
+        """
+        joins = []
+        for t in [n[0:-3] for n in soup._metadata.tables.keys() if n.endswith('Vld')]:
+            payn = t
+            vldn = payn + "Vld"
+            payo = self.entity( soup, payn )
+            vldo = self.entity( soup, vldn )
+            print "joining %s and %s " % ( repr(payo) , repr(vldo) )
+            ## failing with a unicode error
+            jo = join( payo , vldo , payo.SEQ == vldo.SEQ , isouter=False )
+            mjo = soup.map( jo )
+            joins.append( mjo )
+        return joins
+
+    def dbi_fk_tjoins(self, metadata ):
+         """
+              Try to join at the table level, then map to the join ?
+              
+         """
+         tjo = []
+         pay_tables = [n[0:-3] for n,t in metadata.tables.items() if n.endswith('Vld')]
+         vld_tables = ["%sVld" % n for n in pay_tables]
+         for p,v in zip(pay_tables,vld_tables): 
+             pay_t = metadata.tables.get(p, None )
+             vld_t = metadata.tables.get(v, None )
+             jo = join( pay_t , vld_t , pay_t.c.SEQNO == vld_t.c.SEQNO , isouter=False )
+             jo.name = "%s_" % p
+             tjo.append( jo )
+         return tjo
+   
+    def dbi_fk_constraint( self, metadata ):
         """
            Append FK constraints to the SA tables as MySQL dont know em 
            and returns the names of the paired DBI tables found
+           
+           NB the vld table names are returned first, in order to be able to use
+           them when mapping the payload tables 
         """
         from sqlalchemy import ForeignKeyConstraint
         pay_tables = [n[0:-3] for n,t in metadata.tables.items() if n.endswith('Vld')]
@@ -29,7 +74,7 @@ class DbiSARepositoryFactory(SARepositoryFactory):
                 print "skipping tables %s " % n
                 continue
             pay.append_constraint( ForeignKeyConstraint( ['SEQNO'] , ['%s.SEQNO' % v ] ) )
-        return pay_tables + vld_tables
+        return vld_tables + pay_tables 
               
     def entity(self, soup, attr ):
         """ 
@@ -70,6 +115,14 @@ class DbiSARepositoryFactory(SARepositoryFactory):
                 for col in table.columns:
                     properties[attrname(col)] = col
                 
+                if not(table.name.endswith("Vld")):
+                    vt = soup._cache.get("%sVld" % table.name, None )
+                    assert vt
+                    print "paired vld mapped class vt : %s " % vt 
+                    for vc in vt._table.columns:
+                        vn = "v_%s" % vc.name
+                        properties[vn] = column_property( (vc).label(vn) )
+                
                 kwargs = { 'properties':properties  }
                 t = soup.class_for_table(table , **kwargs)
 
@@ -88,8 +141,9 @@ class DbiSARepositoryFactory(SARepositoryFactory):
         print "_reflect_models ... customized in %s " % self.__class__.__name__
         metadata = MetaData(self.engine)
         metadata.reflect()
+        self.metadata = metadata
    
-        dbi_tables = self.dbi_fkc( metadata )     
+        dbi_tables = self.dbi_fk_constraint( metadata )     
          
         # workaround: sqlalchemy does unicode names reflecting and SqlSoup
         #             doesn't like it
@@ -105,14 +159,18 @@ class DbiSARepositoryFactory(SARepositoryFactory):
             table_names = self.reflect
         
         db = sqlsoup.SqlSoup(metadata,self.session_factory)
+        self.soup = db  
 
         entities=dict()
         for table_name in table_names:
             try:
+                ## pulling thru the entity maps the classes into existance 
                 #entities[table_name]=db.entity(table_name)
                 entities[table_name]=self.entity(db, table_name )
             except sqlsoup.PKNotFoundError:
                 log.warn("reflection: skipping table "+table_name+ "...")
+
+ 
 
         mappers = dict((e, get_mapper(e)) for e in entities.itervalues())
         # autogenerate relations
@@ -142,11 +200,22 @@ class DbiSARepositoryFactory(SARepositoryFactory):
                             self.names_for_resource(entity)[1],entity,
                             backref=self.names_for_resource(parent)[0],
                             **relation_kwds)
-        self.soup = db  
+ 
+ 
+        ## do not need relations from the joined tables ... so can do here  ?
+        tjo = self.dbi_fk_tjoins( metadata )
+        for tj in tjo:
+            entities[tj.name] = db.map( tj )
+
+        ## update the mappers 
+        for e in entities.itervalues():
+            if not(mappers.has_key(e)):
+                mappers[e] = get_mapper(e)
+ 
         self.mappers = mappers 
         self.entities = entities 
         
-        return entities.values()
+        return entities.values() 
 
 
 
@@ -170,5 +239,10 @@ if __name__=='__main__':
         for cp in mapr.iterate_properties:
             print cp
 
+    
+    metadata = factory.soup._metadata
+    pay_t = metadata.tables.get("SimPmtSpec")
+    vld_t = metadata.tables.get("SimPmtSpecVld")
+    pv_t = join( pay_t , vld_t , pay_t.c.SEQNO == vld_t.c.SEQNO , isouter=False )
 
 
