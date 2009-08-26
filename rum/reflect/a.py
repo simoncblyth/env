351dc8f21d5ab2b4483607776841971debbf8024
Simon C Blyth <simoncblyth@gmail.com>
2009-08-26 13:52:48 +0800

    policy registering here has no effect ... must do via egg entrypoint, as in prior commit

--- rum/reflect/a.py
+++ rum/reflect/a.py
@@ -88,21 +88,6 @@ def field_fix( app ):
 
 
 
-from rum.policy import Policy, Denial
-class DbiPolicy(Policy):pass
-#
-#    def has_permission(self, obj, action, attr=None, user=None):
-#         return True
-
-
-def anyone(policy, obj, action,  attr, user):
-    print "dbipolicy callable called ... "
-    return True
-
-DbiPolicy.register(anyone)
-
-
-
 class Mapr(dict):
     """
         For checking on the mappers that the app is using 
