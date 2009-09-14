16f1e15ec1a2f44da0e0627e29fd5aaa9ed27280
Simon C Blyth <simoncblyth@gmail.com>
2009-09-14 21:19:24 +0800

    tidy garbage : moral : '''do as little js as possible from the py side'''

--- vdbi/vdbi/tw/rum/static/vdbi_plotter.js
+++ vdbi/vdbi/tw/rum/static/vdbi_plotter.js
@@ -1,18 +1,20 @@
 
 
-Plotter = function ( plot_id, data_url , data_kw ){
+Plotter = function ( plot_id, data_url , data_kw , opts ){
    
    this.plot_id = plot_id 
    this.data_url = data_url
    this.data_kw = data_kw
+   this.opts = opts
    
-   this.handle_data = function(data){
+   
+   this.handle_data = function(data){  // invoked by load_data when the data arrives 
       var cols = [];
       for( _i = 0 ; _i < data.items.length ; _i++ ){
       	  item = data.items[_i];
        	  cols.push( [item.ROW, item.DARKRATE] );    
        }
-       this.plot = $.jqplot( this.plot_id , [cols] , {} )
+       this.plot = $.jqplot( this.plot_id , [cols] , this.opts )
    }
    
    this.load_data = function(){
@@ -36,8 +38,9 @@ Plotter.setup = function(params){
     param_default( "plot_id"  , "plotid" )
     param_default( "data_url" , "http://localhost:8080/SimPmtSpecDbis.json?")
     param_default( "data_kw"  , "" )
+    param_default( "opts"     , {'title':"Default Plot Title"} )
     
-    plotr = new Plotter( params["plot_id"], params["data_url"], params["data_kw"] )
+    plotr = new Plotter( params["plot_id"], params["data_url"], params["data_kw"], params["opts"] )
     plotr.load_data()
     return plotr 
     
