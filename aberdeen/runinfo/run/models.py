ee9a7d6fcc515ba4a2dee1796de89a1bb000c7f8
Simon C Blyth <simoncblyth@gmail.com>
2009-10-05 19:29:57 +0800

    create a starting point Run model

--- aberdeen/runinfo/run/models.py
+++ aberdeen/runinfo/run/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+operator_ = {
+  'choices':(
+              ( 0 , 'Jimmy Ngai'),
+              ( 1 , 'Talent'),
+              ( 2 , 'Soap'),
+              ( 3,  'KamBiu'),
+            ),
+   'default':0,
+}
+
+source_ = {
+ 'choices':(  
+             ( 0 , 'None'),
+             ( 1 , 'Co-60'),
+             ( 2,  'Co-60 (Cal Box)'),
+           ),
+  'default':0,
+}
+
+trigger_ = {
+  'choices':(
+              (0, 'Default'),
+              (1, '16 out of 16'),
+           ),
+  'default':0,
+}
+
+
+
+class Run(models.Model):
+    number    = models.PositiveIntegerField( primary_key=True)
+    start     = models.DateTimeField()
+    stop      = models.DateTimeField()
+    events    = models.PositiveIntegerField()
+    operator  = models.PositiveSmallIntegerField( **operator_ )
+    tkoffset  = models.PositiveIntegerField()
+    source    = models.PositiveSmallIntegerField( **source_ )
+    pmtgain   = models.FloatField(),
+    trigger   = models.PositiveSmallIntegerField( **trigger_ )
+    temperature = models.FloatField(),
+    humidity    = models.FloatField(),
+    comment     = models.CharField(max_length=200)
+    frontendhost  = models.CharField(max_length=40)
+    frontendname  = models.CharField(max_length=40)
+
+    created   = models.DateTimeField('created', default=datetime.now)
+    class Meta:
+        ordering = ['-created']
+        get_latest_by = 'created'
+
+
+
