ff703d67e73e8f294979c60dfb0c66e04e4011e5
Simon C Blyth <simoncblyth@gmail.com>
2009-10-22 13:30:05 +0800

    make private its own mini-package ... this is backward incompatible with {{{from env.base.private ... }}} instead : {{{from private .. }}} after install, for motivation see wiki:PipInstall

---
+++ private/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='private',
+    version='0.1',
+    description='',
+    author='',
+    author_email='',
+    url='',
+    packages=['private'],
+    zip_safe=False,
+)
+
+
