56a6572e6760df5588b602cf97bbdb58f20b5681
Simon C Blyth <simoncblyth@gmail.com>
2009-11-16 20:53:51 +0800

    relocated djroot.py from aberdeen DataModel and starting de-serialization and DB updating

--- aberdeen/runinfo/abtruninfo/messaging.py
+++ aberdeen/runinfo/abtruninfo/messaging.py
@@ -2,7 +2,7 @@
 from carrot.connection import DjangoBrokerConnection
 from carrot.messaging import Publisher, Consumer
 from abtruninfo.models import AbtRunInfo
-
+import cjson
 
 def send_dummy_runinfo(run_info):
     """Send a dummy message for adding a runinfo entry ."""
@@ -18,6 +18,28 @@ def send_dummy_runinfo(run_info):
     connection.close()
 
 
+def datetimes_( d ):
+    from datetime import datetime
+    u = {}
+    for k,v in d.items():
+        if k.endswith("Time"):
+            u[k] = datetime.strptime( v , "%c" )
+    d.update( u )
+    return d
+
+def skips_(d): 
+    for k, v in d.items():
+        if type(v) == str and v.startswith("SKIPPED"):del d[k]
+    return d      
+
+def deserialize( body ):
+    d = None
+    try:
+        d = cjson.decode( body )
+    except:
+        print "failed to decode %s " % body
+    return d 
+     
 def process_abtruninfo():
     """Process all currently gathered runinfo by saving them to the
     database."""
@@ -29,8 +51,14 @@ def process_abtruninfo():
                         exchange_type="direct")
 
     for message in consumer.iterqueue():
-        print message, message.body
-        message.ack()
+        d = deserialize( message.body )
+        d = datetimes_(d)
+        d = skips_(d)
+        print message, d 
+        if d:
+            ari = AbtRunInfo( **d )
+            print ari 
+        #message.ack()
 
     #  usual django db updating 
     #for url, click_count in clicks_for_urls.items():
