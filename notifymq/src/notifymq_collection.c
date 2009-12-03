fb6754f1dc3444df61439acaa15fa852a462de7f
Simon C Blyth <simoncblyth@gmail.com>
2009-12-03 18:16:10 +0800

    groundwork for trying hash of hashes structure instead of hash of dqueues, this would allow easy absolute access ... but probably need ref counting

--- notifymq/src/notifymq_collection.c
+++ notifymq/src/notifymq_collection.c
@@ -21,13 +21,22 @@ G_LOCK_DEFINE( notifymq_collection );
 
 void notifymq_collection_hash_dumper_(gpointer key, gpointer value, gpointer user_data);
 
+ 
 
 typedef struct notifymq_collection_queue_t_ { 
-   GQueue* queue  ; 
+   char kind ;
+   union {
+     GQueue*     queue  ;
+     GHashTable* hash  ;
+     void*       other  ;
+   } v ;
    uint64_t received ; 
    uint64_t read ; 
+   uint64_t lastread ; 
+   uint64_t lastadd ; 
    bool_t   updated ;           // since the last read 
 } notifymq_collection_queue_t ; 
+
 
 // internal funcs, without thread protection 
 notifymq_collection_queue_t* notifymq_collection_getq_( const char* key );
@@ -53,18 +62,28 @@ int notifymq_collection_add( notifymq_basic_msg_t * msg )
 {
     G_LOCK(notifymq_collection);
     notifymq_collection_queue_t* q =  notifymq_collection_getq_or_create_( msg->key );
-    guint length = g_queue_get_length( q->queue );
-    if(length == notifymq_collection_max ){
-        if( notifymq_dbg > 1 )
-            printf("_collection_add reached max %d popping tail \n" , length );
-        notifymq_basic_msg_t* d = (notifymq_basic_msg_t*)g_queue_pop_tail( q->queue );
-        notifymq_basic_msg_free( d );
+    guint length ;
+    switch (q->kind){
+       case 'Q':
+           length = g_queue_get_length( q->v.queue );
+           if(length == notifymq_collection_max ){
+               if( notifymq_dbg > 1 )
+                  printf("_collection_add reached max %d popping tail \n" , length );
+               notifymq_basic_msg_t* d = (notifymq_basic_msg_t*)g_queue_pop_tail( q->v.queue );
+               notifymq_basic_msg_free( d );
+           }
+           g_queue_push_head( q->v.queue , msg );
+           break ;
+       case 'H':
+           break ;
+        default:
+           break ; 
     }
 
+    q->lastadd = msg->index ;
     q->received += 1 ;
-    q->updated = 1 ;    
-
-    g_queue_push_head( q->queue , msg );
+    q->updated = 1 ;
+    
     G_UNLOCK(notifymq_collection);
     return EXIT_SUCCESS ;
 }
@@ -93,7 +112,17 @@ int notifymq_collection_queue_length( const char* key )
     notifymq_collection_queue_t* q = notifymq_collection_getq_( key );
     if( q == NULL )
         printf("_collection_length ERROR no q for key \"%s\" \n", key );
-    int len = q == NULL ? -1 : (int)g_queue_get_length( q->queue );
+
+    int len ;
+    switch (q->kind){
+       case 'Q':
+          len = q == NULL ? -1 : (int)g_queue_get_length( q->v.queue );
+       case 'H':
+          break;
+       default:
+          break;
+    }
+
     G_UNLOCK(notifymq_collection);
     return len ;
 }
@@ -105,10 +134,23 @@ notifymq_basic_msg_t* notifymq_collection_get( const char* key , int n )
     if( q == NULL )
        printf("_collection_get ERROR no q for key \"%s\" \n", key );
 
-    q->updated = 0 ;    
-    q->read += 1 ;
+    notifymq_basic_msg_t* msg = NULL ;
 
-    notifymq_basic_msg_t* msg = q == NULL ? NULL :  (notifymq_basic_msg_t*)g_queue_peek_nth( q->queue , n  ) ;   
+    switch (q->kind){
+       case 'Q':
+          msg = q == NULL ? NULL : (notifymq_basic_msg_t*)g_queue_peek_nth( q->v.queue , n  ) ;   
+       case 'H':
+          break;
+       default:
+          break;
+    }
+
+    if( msg ){
+        q->updated = 0 ;    
+        q->read += 1 ;
+        q->lastread = msg->index ;
+    }
+
     G_UNLOCK(notifymq_collection);
     return msg ; 
 }
@@ -127,10 +169,27 @@ notifymq_collection_queue_t* notifymq_collection_getq_( const char* key )
 notifymq_collection_queue_t* notifymq_collection_queue_alloc_()
 {
     notifymq_collection_queue_t* q = (notifymq_collection_queue_t*)malloc( sizeof( notifymq_collection_queue_t ) );
-    q->queue = g_queue_new();
+   
+    char kind = *"Q" ;
+    q->kind  = kind ; 
+   
+    switch ( q->kind ){
+        case 'Q':
+           q->v.queue = g_queue_new();
+           break ;
+        case 'H':
+           q->v.hash  = g_hash_table_new( NULL , NULL );
+           break ;
+         default:
+           q->v.other = NULL ;
+       } 
+
     q->received = 0 ;
-    q->read= 0 ;
+    q->read = 0 ;
     q->updated = 0 ;
+    q->lastread= 0 ;
+    q->lastadd = 0 ;
+
     return q ;
 }
 
@@ -144,8 +203,8 @@ void notifymq_collection_queue_dump_(notifymq_collection_queue_t* q )
         printf("_collection_queue_dump NULL q\n");
         return ;
     }
-    guint length = g_queue_get_length(q->queue);
-    printf("_collection_queue_dump : length %d received %lld read %lld updated %d \n", length, q->received, q->read, q->updated ); 
+    guint length = g_queue_get_length(q->v.queue);
+    printf("_collection_queue_dump : length %d received %lld read %lld updated %d lastread %lld lastadd %lld \n", length, q->received, q->read, q->updated, q->lastread, q->lastadd ); 
 }
 
 notifymq_collection_queue_t* notifymq_collection_getq_or_create_( const char* key )
@@ -154,8 +213,10 @@ notifymq_collection_queue_t* notifymq_collection_getq_or_create_( const char* ke
     if( q == NULL ){      
        if( notifymq_dbg > 0 )
            printf("_collection_getq_or_create : creating dq for key \"%s\" \n", key ); 
+
        g_hash_table_insert( notifymq_collection , g_strdup( key ) , notifymq_collection_queue_alloc_() );
-       q =  (notifymq_collection_queue_t*)g_hash_table_lookup( notifymq_collection , key );
+       q =  notifymq_collection_getq_( key );
+    
     } else {
        if( notifymq_dbg > 1 )
            printf("_collection_getq_or_create using pre-existing dq for key \"%s\" \n", key ); 
@@ -166,18 +227,31 @@ notifymq_collection_queue_t* notifymq_collection_getq_or_create_( const char* ke
 void notifymq_collection_hash_dumper_(gpointer key, gpointer value, gpointer user_data)
 {
    notifymq_collection_queue_t* q = (notifymq_collection_queue_t*)value ;
-   guint length = g_queue_get_length(q->queue);
+   guint length = g_queue_get_length(q->v.queue);
    printf("_hash_dumper key \"%s\" length %d \n", (char*)key, length );
    notifymq_collection_queue_dump_( q );
 
    guint n ;
    char label[50] ;
    for( n = 0 ; n < length ; n++ ){
-      sprintf( label, "peek_nth %d ", n );
-      notifymq_basic_msg_dump(  (notifymq_basic_msg_t*)g_queue_peek_nth(q->queue, n ), 0 , label  ); 
+        switch ( q->kind ){
+           case 'Q':
+               sprintf( label, "queue_peek_nth %d ", n );
+               notifymq_basic_msg_dump(  (notifymq_basic_msg_t*)g_queue_peek_nth(q->v.queue, n ), 0 , label  ); 
+               break ;
+           case 'H':
+               break ;
+        }     
    }
-   notifymq_basic_msg_dump( (notifymq_basic_msg_t*)g_queue_peek_head( q->queue ) , 0 , "peek_head" ); 
-   notifymq_basic_msg_dump( (notifymq_basic_msg_t*)g_queue_peek_tail( q->queue ) , 0 , "peek_tail" ); 
+
+   switch ( q->kind ){
+      case 'Q':
+          notifymq_basic_msg_dump( (notifymq_basic_msg_t*)g_queue_peek_head( q->v.queue ) , 0 , "peek_head" ); 
+          notifymq_basic_msg_dump( (notifymq_basic_msg_t*)g_queue_peek_tail( q->v.queue ) , 0 , "peek_tail" ); 
+          break ;
+   }
+
+
 }
 
 
