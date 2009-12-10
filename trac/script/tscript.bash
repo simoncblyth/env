9a68c72ac2ee58144bff85f5e1a8bb7df03354ed
Simon C Blyth <simoncblyth@gmail.com>
2009-12-10 17:36:04 +0800

    annotation re the python version to run tscript-t2l under

--- trac/script/tscript.bash
+++ trac/script/tscript.bash
@@ -1,8 +1,8 @@
 
-tscript-env(){
-  echo -n
-}
-
+tscript-env(){    echo -n ; }
+tscript-src(){    echo trac/script/tscript.bash ; }
+tscript-source(){ echo ${BASH_SOURCE:-$(env-home)/$(tscript-src)} ; }  
+tscript-vi(){     vi $(tscript-source) ; }
 tscript-usage(){
   cat << EOU
 
@@ -11,18 +11,18 @@ tscript-usage(){
 
     echo "== Hello ==" | tscript-t2l
 
-EOU
+   this needs to run under the same python that Trac is using otherwise
+   will get no such module "trac2latex"
 
+EOU
 }
 
-tscript-t2l-py(){ echo $ENV_HOME/trac/script/t2l.py ; }
-
+tscript-t2l-py(){ echo $(env-home)/trac/script/t2l.py ; }
 tscript-t2l(){
    case "$#" in
       0) cat - | python `tscript-t2l-py` ;;
       *) python `tscript-t2l-py` $* ;;
    esac  
-
 }
 
 tscript-t2l-test(){
