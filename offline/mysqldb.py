7146db7f8e6a928d39ca9c48325b9cabe3cb8cb1
Simon C Blyth <simoncblyth@gmail.com>
2009-12-18 10:56:22 +0800

    private access via separate python pkg

--- offline/mysqldb.py
+++ offline/mysqldb.py
@@ -2,7 +2,7 @@ import MySQLdb
 
 class DBP(dict):
     def __init__(self):
-        from env.base.private import Private
+        from private import Private
         p = Private()
         self['host'] = p('DATABASE_HOST') 
         self['db'] = p('DATABASE_NAME')  
