2cdb50831ffd42a24ea457fdc924fde266a1b51b
Simon C Blyth <simoncblyth@gmail.com>
2010-01-18 18:16:39 +0800

    split off DBConf from mysqldb

---
+++ offline/dbconf.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+"""
+   Preview environment setup read from .ini 
+       ./dbconf.py --path demo.ini --sect testdb  
+
+   Equip your environment with ENV_TSQL_ envvars 
+   by placing the below into your .bash_profile where the section name
+   matches a section name in your $HOME/.dybdb.ini
+ 
+       eval $(./dbconf.py --sect testdb  )
+
+"""
+import os
+
+class DBConf(dict):
+    """
+        Reads ini format config file from <path>, storing key,value pairs 
+        from <section> into this dict     
+        The standard python ConfigParser module is used
+            http://docs.python.org/library/configparser.html
+        which supports %(name)s style replacements in other values 
+ 
+        Usage example :
+             cfg = DBConf(path=os.path.expanduser('~/.dybdb.ini') , sect="testdb" , epfx=None )
+
+        NB as the config file contains passwords it needs to be treated with care
+            * protect it with chmod go-rw (this is enforced )
+            * DO NOT COMMIT into any repository 
+
+        If epfx is supplied and it matches the start of the keys, then the key, value
+        pair is exported into the envriroment.
+
+    """
+
+    def __init__(self, path=None, sect=None, epfx=None, src=None ): 
+        
+        from ConfigParser import ConfigParser
+        cfg = ConfigParser()
+        cfg.optionxform = str   ## avoid lowercasing keys, making the keys case sensitive
+
+        if not(src):
+            ## make sure the config file exists and has appropriate permissions 
+            assert os.path.exists( path ), "config path %s does not exist " % path
+            from stat import S_IMODE, S_IRUSR, S_IWUSR
+            s = os.stat(path)
+            assert S_IMODE( s.st_mode ) == S_IRUSR | S_IWUSR , "incorrect permissions, config file must be protected with : chmod go-rw \"%s\" " %  path
+            cfg.read(path)
+        else:
+            from StringIO import StringIO
+            cfg.readfp( StringIO(src) )
+
+        secs = cfg.sections()
+        assert sect in secs , "section %s is not one of these : %s configured in %s " % (section,  secs, path ) 
+        for k in cfg.options(sect):
+            v = cfg.get(sect,k)
+            self[k] = v
+            if epfx and k.startswith(epfx):os.environ[k] = v
+    
+        self.cfg = cfg
+        self.sect = sect
+        self.path = path
+        self.epfx = epfx
+
+    def __repr__(self):
+        return "# ./dbconf.py --path %s --sect %s --epfx %s " % ( self.path, self.sect, self.epfx ) 
+
+    def export(self, shell='bash'):
+        epfx = self.epfx
+        o = []
+        for k,v in self.items():
+            if epfx and k.startswith(epfx):o.append( "export %s='%s' " % (k, v) )
+        return ";".join(o)
+
+
+
+if __name__=='__main__':
+
+    from optparse import OptionParser
+    parser = OptionParser()
+    defaults = { 'path':os.path.expanduser('~/.dybdb.ini') , 'sect':"testdb" , 'epfx':"ENV_TSQL_" }
+
+    parser.add_option("-p", "--path", dest="path", help="path to ini file, default: %s " % defaults['path'], metavar="PATH")
+    parser.add_option("-s", "--sect", dest="sect", help="active section in the ini file, default: %s " % defaults['sect'], metavar="SECT")
+    parser.add_option("-e", "--epfx", dest="epfx", help="export envvars with names starting with this prefix, default: %s  " % defaults['epfx'] , metavar="EPFX")
+    parser.set_defaults( **defaults )
+    (opts, args) = parser.parse_args()
+
+    cfg = DBConf( path=opts.path , sect=opts.sect , epfx=opts.epfx )  
+    print cfg.export(), cfg
+
+
+
+  
--- offline/mysqldb.py
+++ offline/mysqldb.py
@@ -1,121 +1,11 @@
-"""
-
- An error while {{{import MySQLdb}}} like :
-
-{{{
-  File "/home/blyth/local/mysql-python/MySQL-python-1.2.3c1/MySQLdb/__init__.py", line 19, in <module>
-    import _mysql
-ImportError: libmysqlclient_r.so.16: cannot open shared object file: No such file or directory
-}}}
-
- indicates a library path issue :  
-     export LD_LIBRARY_PATH=/usr/local/mysql/lib/mysql:$LD_LIBRARY_PATH
-"""
 
 import MySQLdb
-import os
 
-demo_ini = """
-#
-#  parsability with bash cfg.parser imposes restrictions :
-#     * comments use # not ;
-#     * values cannot include #
-#     * values cannot include spaces
-#     * extraneous space after section declarations should be avoided (still?)
-#     * values using python style interpolation need to be in "double_quotes"
-#
-#  this is included inside source code for demonstration only, 
-#  it is not a good practice to do this for real configuration parameters 
-#  as source belongs in a repository and passwords/config do not
-#
-
-[testdb]
-
-# these key names correspond to keyword arguments accepted by MySQLdb.connect
-host = 127.0.0.1
-db   = the_db
-user = the_db_user
-passwd = the_db_pass
-
-[otherdb]        
-
-host = 127.0.0.1
-db   = otherdb
-user = the_db_user
-passwd = the_db_pass
-
-# export these into environment using argument envpfx='ENV_TSQL_' 
-# use interpolation to avoid duplication and associated errors
-
-ENV_TSQL_URL  = "mysql://%(host)s/%(db)s;"
-ENV_TSQL_USER = "%(user)s"
-ENV_TSQL_PSWD = "%(passwd)s"
-
-[mycascade]
-
-ENV_TSQL_URL  = "mysql://wherever/testdb;mysql://wherever/otherdb"
-ENV_TSQL_USER = "testuser;otheruser"
-ENV_TSQL_PSWD = "testpass;otherpass"
-
-"""
-
-class DBP(dict):
-    """
-        Reads ini format config file from <path>, storing key,value pairs 
-        from <section> into this dict     
-        The standard python ConfigParser module is used
-            http://docs.python.org/library/configparser.html
-        which supports %(name)s style replacements in other values 
- 
-        Usage example :
-             dbp = DBP(path=os.path.expanduser('~/.dybdb.ini') , section="testdb" , envpfx=None )
-             print dbp
-                 > {'passwd': 'the_db_pass', 'host': '127.0.0.1', 'db': 'the_db', 'user': 'the_db_user'}
-
-        NB as the config file (eg ~/.mydb.cfg ) contains passwords it needs to be treated with care
-            * protect it with chmod go-rw (this is enforced )
-            * DO NOT COMMIT into any repository 
-
-        If envpfx is supplied and it matches the start of the keys, then the key, value
-        pair is exported into the envrionent.
-
-        Example config file with multiple sections to easy config swapping 
-
-    """
-
-    def __init__(self, path=None, section=None, envpfx=None, src=None ): 
-        
-        from ConfigParser import ConfigParser
-        cfg = ConfigParser()
-        cfg.optionxform = str   ## avoid lowercasing keys, making the keys case sensitive
- 
-        if not(src):
-            ## make sure the config file exists and has appropriate permissions 
-            assert os.path.exists( path ), "config path %s does not exist " % path
-            from stat import S_IMODE, S_IRUSR, S_IWUSR
-            s = os.stat(path)
-            assert S_IMODE( s.st_mode ) == S_IRUSR | S_IWUSR , "incorrect permissions, config file must be protected with : chmod go-rw \"%s\" " %  path
-            cfg.read(path)
-        else:
-            from StringIO import StringIO
-            cfg.readfp( StringIO(src) )
-
-        secs = cfg.sections()
-        assert section in secs , "section %s is not one of these : %s configured in %s " % (section,  secs, path ) 
-        for k in cfg.options(section):
-            v = cfg.get(section,k)
-            self[k] = v
-            if envpfx and k.startswith(envpfx):os.environ[k] = v
-        self.cfg = cfg
-        self.section = section
-        self.path = path
-
- 
 class DB:
-    def __init__(self, **dbp ):
+    def __init__(self, **cfg ):
         try:  
-            self.conn = MySQLdb.connect( **dbp ) 
-            self.dbp = dbp
+            self.conn = MySQLdb.connect( **cfg ) 
+            self.cfg = cfg
         except MySQLdb.Error, e: 
             print "Error %d: %s " % ( e.args[0], e.args[1] )
          
@@ -145,21 +35,18 @@ class DB:
 
 
 if __name__=='__main__':
-    
-    #demo = DBP( src=demo_ini  , section="testdb" , envpfx=None )  
-    #print demo
+   
+    from dbconf import DBConf 
+    cfg = DBConf( sect="testdb" )  
+    print cfg
 
-    dbp = DBP( path=os.path.expanduser('~/.dybdb.ini') , section="testdb" , envpfx=None )  
-    print dbp
-    db = DB( **dbp )
+    db = DB( **cfg )
     rec = db.fetchone("SELECT VERSION()")
     print rec
     for rec in db("SHOW TABLES"):
         print rec 
         tab = rec.values()[0]
         print db("DESCRIBE %s" % tab )
-
-
     db.close()
 
     
