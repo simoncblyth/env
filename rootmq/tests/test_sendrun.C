5c422b2d60cd1bf5415c920c46f6c796d62fed85
Simon C Blyth <simoncblyth@gmail.com>
2010-03-15 21:05:25 +0800

    firing runs is handy for development

---
+++ rootmq/tests/test_sendrun.C
@@ -0,0 +1,27 @@
+
+// invoke by : 
+//      rootmq-
+//      rootmq-cd
+//      make test_sendobj
+//
+// the kFALSE prevents starting monitor thread, just establish connection to 
+// potentially remote queue and send it a message
+//
+
+void test_sendrun()
+{
+   gSystem->Load("librootmq");
+   MQ::Create(kFALSE);  // creates gMQ global singleton
+  
+   gSystem->Load("libAbtDataModel");
+
+   TFile* f = TFile::Open("$ABERDEEN_HOME/DataModel/sample/run00027.root");
+   TTree* t = f->Get("T") ;
+
+   AbtRunInfo* ri = (AbtRunInfo*)(t->GetUserInfo()->At(0)) ;
+   gMQ->SendObject( ri );
+
+
+   exit(0) ;
+}
+
