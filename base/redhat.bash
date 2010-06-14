a9be9924653e7e2896afb969921ed521afccc2c4
Simon C Blyth <simoncblyth@gmail.com>
2010-06-14 17:30:04 +0800

    redhat-epel4/5 for EPEL hookup ... disabled it by default on C and N to avoid accidentals

--- base/redhat.bash
+++ base/redhat.bash
@@ -12,6 +12,32 @@ redhat-usage(){
          Configure yum to have access to EPEL,  Extra Packages for Enterprise Linux  
          http://fedoraproject.org/wiki/EPEL/FAQ#howtouse
 
+     [blyth@belle7 yum.repos.d]$ redhat-vers
+     Scientific Linux SL release 5.1 (Boron)
+
+     [blyth@cms01 yum.repos.d]$ redhat-vers
+     Scientific Linux CERN SLC release 4.8 (Beryllium)
+
+     Probably best to disable it by default ... 
+           /etc/yum.repos.d/epel.d  
+     to prevent accidentals  ... see #278 #276
+
+[blyth@belle7 yum.repos.d]$ redhat-epel5
+[sudo] password for blyth: 
+Retrieving http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-3.noarch.rpm
+warning: /var/tmp/rpm-xfer.4YsNUc: Header V3 DSA signature: NOKEY, key ID 217521f6
+Preparing...                ########################################### [100%]
+   1:epel-release           ########################################### [100%]
+[blyth@belle7 yum.repos.d]$ 
+[blyth@belle7 yum.repos.d]$ 
+[blyth@belle7 yum.repos.d]$ rpm -ql epel-release-5-3
+/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL
+/etc/yum.repos.d/epel-testing.repo
+/etc/yum.repos.d/epel.repo
+/usr/share/doc/epel-release-5
+/usr/share/doc/epel-release-5/GPL
+
+
 EOU
 }
 redhat-dir(){ echo $(local-base)/env/base/base-redhat ; }
@@ -26,11 +52,11 @@ redhat-vers(){
   cat /etc/redhat-release
 }
 
-
-redhat-epel(){
+redhat-epel4(){
    sudo rpm -Uvh http://download.fedora.redhat.com/pub/epel/4/i386/epel-release-4-9.noarch.rpm
-
 }
 
-
+redhat-epel5(){
+  sudo rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-3.noarch.rpm
+}
 
