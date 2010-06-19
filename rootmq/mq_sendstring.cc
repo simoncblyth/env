0ab0e038279cd307b8bda13fefb17c5c5ad61a36
Simon C Blyth <simoncblyth@gmail.com>
2010-06-19 12:51:30 +0800

    sendstring debug

--- rootmq/mq_sendstring.cc
+++ rootmq/mq_sendstring.cc
@@ -7,12 +7,11 @@ const char* default_key = "abt.test.string" ;
 
 int main(int argc, char const * const *argv) {
    MQ::Create();
-
    int n = 10 ;
    char smry[max];
    for(Int_t i = 0 ; i < n ; i++){
       const char* key = i % 2 == 0 ? default_key : other_key ;
-      snprintf( smry, max, " %s %d %s  ", gMQ->Summary() , i , key   );
+      snprintf( smry, max, " mq_sendstring i:%d key:%s  ", i , key   );
       gMQ->SendString( smry , key );
    } 
    
