a3abd18c2cee46bad4230e722d2b297771ab26ae
Simon C Blyth <simoncblyth@gmail.com>
2010-06-23 19:07:58 +0800

    annotation ... thinking about build unification

--- base/slv.bash
+++ base/slv.bash
@@ -5,25 +5,64 @@ slv-vi(){       vi $(slv-source) $(slv-runtest-path) $(slv-isotest-path) ; }
 slv-usage(){
   cat << EOU
 
+   == GOALS ==
 
+        simplify the slave- funcs ...  with an eye to doing daily builds under slave control
+           a) work in fully relative manner ... 
+           b) minimize config 
+           c) do it without the dyb__ funcs ... that are single install fixated
+           d) environment isolation ... do it in env -i for insensitivity to calling environment
+       
+           e) * .cfg must not be web-accessible so cannot live in the build dir 
+
+           f) unify builds : daily/weekly/update to use single bitten-slave machinery/reporting 
+              with minor differences 
+                    - build invokation directory 
+                          * update build into fixed dir 
+                          * weekly? green field build into empty dir
+                    - external option 
+                          * daily? build using externals shared with the update build
+
+   == fixed interval bitten-slave operation ==
+
+      IDEA A : 
+         * use single shot bitten-slave invokation
+         * make the requests to the master at the "right" time based on timeline observations 
+              * calling at fixed times would not work if coincides with commit cooldown windows
+         * observe the timeline rss feed to time the request
+              * http://www.feedparser.org/
+ 
+            ==> this requires duplication of the master slave sheduling 
+
+     IDEA B :
+
+         * at the allotted time ... start asking the master if there is a build pending 
+          ... up until the send of a time window for doing this type of build
+ 
+            ==>  could implement by adding an "askmax" option to bitten-slave  
+   
+                                     
    == TODO : ==
+
+       1) propagate NUWA_LOGURL via dybinst local config rather than environment 
+          to keep this out of the recipe and config
+
+       2) aiming towards zero-conf ... 
+
+       3) migrate the update "standard" slave run to use recipes generated here  
+          would it make sense to ...
+              ./dybinst trunk slave 
+
+
+   == DONE ==
 
        1) dybinst hookup ...
              ./dybinst trunk test rootiotest
 
 
-    == slv ==
-
-        simplify the slave- funcs ...  with an eye to doing daily builds under slave control
-           a) work in fully relative manner ... 
-           b) minimize config 
-
-           c) do it without the dyb__ funcs ... that are single install fixated
-           d) environment isolation ... do it in env -i for insensitivity to calling environment
-
+   == DEV FUNCS ==
 
      slv--
-
 
      slv-cfg-path  : $(slv-cfg-path)
      slv-cfg
@@ -32,7 +71,6 @@ slv-usage(){
      slv-recipe
            emit build/test recipe to stdout 
            normally the recipe is kept on the master ... but convenient to keep all together for dev  
-
            for svn:export of a file use "dir" attribute for the name of the file 
            current bitten on cms01 doesnt recognize username/password attributes ... so only working due to svn auth cache 
 
@@ -40,22 +78,13 @@ slv-usage(){
            emit cmd to stdout 
 
 
-
-   == fixed interval bitten-slave operation ==
-
-      IDEA :
-         * use single shot bitten-slave invokation
-         * make the requests to the master at the "right" time based on timeline observations 
-              * calling at fixed times would not work if coincides with commit cooldown windows
-         * observe the timeline rss feed to time the request
-              * http://www.feedparser.org/
- 
-
      slv-dir : $(slv-dir)
      slv-name : $(slv-name)
 
-       * .cfg must not be web-accessible so cannot live in the build dir 
        * .recipe resides on the master in normal operation 
+
+
+
 
     Run inside screen with :
           SCREEN=screen slv--
@@ -75,7 +104,6 @@ slv-env(){
    elocal-  
    private-
 }
-slv-dir(){ echo $(local-base)/env/base/slv ; }
 slv-cd(){  cd $(slv-dir); }
 slv-init(){
    local dir=$(slv-dir) &&  mkdir -p $dir && cd $dir
@@ -112,7 +140,7 @@ slv-cfg(){ cat << EOC
 path = /
 build = 1000
 config = dybinst
-revision = 8751
+revision = 8799
 
 [repo]
 url  = $(slv-repo-url)
@@ -140,10 +168,11 @@ slv-recipe(){
   #local testpkgs="gaudimessages gentools rootiotest simhistsexample dbivalidate"
   #local xexternals=""
 
-  local export=0
+  local export=1
   local stages=""
   local projs=""
-  local testpkgs="rootiotest"
+  #local testpkgs="rootiotest"
+  local testpkgs="gaudimessages gentools rootiotest simhistsexample dbivalidate"
   local xexternals=""
 
   # head
@@ -212,6 +241,43 @@ EOT
 }
 
 
+slv-mode(){ 
+  echo ${SLV_MODE:-update}   ## update/shared/green/dev
+}
+slv-layout-dev(){ slv-layout-update ; }
+slv-layout-update(){ cat << EOL
+    --work-dir=. 
+    --build-dir=. 
+EOL
+}
+slv-layout-shared(){ cat << EOL
+   --work-dir=. 
+   --build-dir="shared_\\\${${tmp}config}_\\\${${tmp}revision}" 
+EOL
+}
+slv-layout-green(){ cat << EOL
+   --work-dir=. 
+   --build-dir="green_\\\${${tmp}config}_\\\${${tmp}revision}" 
+EOL
+}
+
+slv-base(){ echo $(local-base)/slv ; }
+slv-dir(){ 
+  case $(slv-mode) in 
+    shared|dev|green) echo $(slv-base)/$(slv-mode) ;;
+              update) echo $(slv-base) ;;
+  esac
+}
+
+slv-external(){
+  case $(slv-mode) in 
+   green|update) echo -n          ;;   ## default external location in the dybinst dir
+     dev|shared) echo ../external ;;   ## one up to the update external 
+  esac 
+}
+
+
+
 
 slv-cmd(){
   local arg=$1  
@@ -220,26 +286,39 @@ $SCREEN $(which bitten-slave)
       --dry-run
       --config=$(slv-cfg-path)
       --verbose 
-      --work-dir=. 
-      --build-dir=.
       --keep-files 
       --log=$(slv-name).log 
       --user=$(slv-repo-user) --password=$(slv-repo-pass) 
-      $arg
 EOC
-
-  cat << EOD > /dev/null
-
-  local tmp="local."
-    --name=$name ... default is hostname ? 
-    --build-dir="build_\\\${${tmp}config}_\\\${${tmp}revision}" 
-
-EOD
+  slv-layout-$(slv-mode)
+  cat << EOA
+      $arg
+EOA
 }
+
+slv-label(){ echo $(slv-repo)-$(slv-mode) ; }
+
+slv-sv-(){ cat << EOS
+[program:$(slv-label)]
+directory=$(slv-dir)
+command=$(which python) $(slv-cmd $*)
+redirect_stderr=true
+redirect_stdout=true
+autostart=true
+autorestart=true
+priority=999
+user=$USER
+EOS
+}
+
+
 
 
 slv---(){
 
+  # in normal usage the recipe comes from the master 
+  # and the config is fixed once ...  
+  #
   local msg="=== $FUNCNAME : "
   echo $msg running build recipe from $PWD 
 
@@ -251,7 +330,6 @@ slv---(){
   slv-recipe > $recipe
   xmllint $recipe
   [ "$?" != "0" ] && echo invalid recipe xml && return 1 
-
 
   #local cmd=$(slv-cmd $(slv-repo-builds))    ## remote builds url 
   local cmd=$(slv-cmd $recipe)                ## local recipe for dev
@@ -268,7 +346,7 @@ slv--(){
   slv---
 }
 
-## these are dev versions operational ones now in installation/trunk/dybinst/scripts
+## these are dev versions ...  operational ones now in installation/trunk/dybinst/scripts :w
 slv-isotest-path(){ echo $(env-home)/offline/isotest.sh ; }
 slv-runtest-path(){ echo $(env-home)/offline/runtest.sh ; }
 
