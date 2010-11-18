739722fc682e23db6c9da03c8f7ff6a42944e62c
Simon C Blyth <simoncblyth@gmail.com>
2010-11-18 20:20:16 +0800

    memory issues resolved by : propert cleanup in {{{_mysql}}} usage and avoiding the leaky and slow MySQLdb cursors

--- npy/cy/npmy/test_npmy.py
+++ npy/cy/npmy/test_npmy.py
@@ -1,8 +1,47 @@
 """
 
+   MEMORY LEAK CHECKING WITH 
+
+  In [3]: MySQLdb.version_info
+  Out[3]: (1, 3, 0, 'final', 0)
+
+[blyth@cms01 MySQLdb-2.0]$ hg tip
+changeset:   83:e705129ff06f
+branch:      MySQLdb
+tag:         tip
+parent:      82:ffe9e5ca17e0
+parent:      80:6ec608cdd19c
+user:        Andy Dustman <adustman@users.sourceforge.net>
+date:        Tue Aug 31 22:28:13 2010 -0400
+summary:     Merge some Kyle stuff
+
+
+
+
+   PROGESSIVE COMMENTING / DE-COMMENTING
+
+       * skip array loading from the query result
+       * skip array creation      
+          * little change in memory/timings !!!
+       * skip doing the query
+          * memory/timings drastically reduced and flat
+       * put back array creation 
+          * timings equal-ish and memory flat when using np.zeros (ndarray is a bit quicker)
+
+       * put back the query and get_result 
+          * back to monotonic memory ==> MySQLdb/_mysql or my useage of it is culprit
+
+       * for MySQLdb 1.3.0  
+            * find that MUST cursor.close() TO AVOID LEAK
+            * connection.close() IS NOT ENOUGH  
+
+
+
    ISSUES ...
 
     * monotonic memory ... when run multiple scans in the same process
+
+
 
 
     * tinyints could be used for ladder/col/ring ... but leave at i4 initially   
@@ -95,59 +134,87 @@ class Fetch(dict):
         self.update(kwargs)
 
 
-
 class Pure(Fetch):
     """
        Problems with MySQLdb 1.2.3 ...
          * NULL columns are causing "TypeError: Cannot convert None to Decimal"
            forcing use of coalesce with default zeros for voltage and pw columns
-         * cursor.rowcount giving -1 
+
+      This has monotonic memory ???
+           cannot push either approach to big queries ... due to memory death 
 
     """
-    def __init__(self, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
-
     def __call__(self, **kwargs ):
         self.update(kwargs)
+
         conn = MySQLdb.connect( **self.connargs  )
         cursor = conn.cursor()
-        cursor.execute( self.sql )
+        cursor.execute( self.sql )          ## this includes the time-consuming get_result
 
-        if self['method'] == 0:
-
-            a = np.ndarray( ( int(self['limit']), ) , dtype=self.dtype )
+        if 0:
+            a = np.zeros( ( int(self['limit']), ) , dtype=self.dtype )
             for i,row in enumerate(cursor):
                 a[i] = tuple(row)
             if 'verbose' in kwargs:print a
             a = None
 
-        elif self['method'] == 1:
-            a = np.fromiter( (tuple(row) for row in cursor), dtype=self.dtype, count = int(self['limit']) )
-            if 'verbose' in kwargs:print a
-            a = None
+        a = np.fromiter( (tuple(row) for row in cursor), dtype=self.dtype, count = int(self['limit']) )
+        if 'verbose' in kwargs:print a
+        a = None
 
+        cursor.close()    ## NOT CLOSING THE CURSOR LEAKS MEMORY TERRIBLY 
+        conn.close()      ## LITTLE MEMORY IMPACT
+
+class Xure(Fetch):
+    """
+          Avoid the leaky cursor  
+            ... very healthy speed up only 2-3* slower than Cyth
+
+    """
+    def __call__(self, **kwargs ):
+        self.update(kwargs)
+
+        conn = _mysql.connect( **self.connargs )
+        conn.query( self.sql )
+        result = conn.get_result()      ## this takes the time
+ 
+        a = np.fromiter( result , dtype=self.dtype, count = int(self['limit']) )
+        
+        result.clear()     ## ESSENTIAL MEMORY CLEANUP
+        result = None 
         conn.close()
-        del conn
-        conn = None
+
+        if 'verbose' in kwargs:print a
+        a = None
 
 
 class Cyth(Fetch):
-    def __init__(self, *args, **kwargs ):
-        super(self.__class__, self).__init__(*args, **kwargs)
+    """
+        Factor of 10 faster than Pure* but leaks like a sieve
+        does the shovelling from mysql rows into numpy array in C
 
+        method 1 is approx twice the speed as method 0, 
+        with the same memory  ... removal of bounds checking/wraparound 
+
+    """     
     def __call__(self, **kwargs ):
         self.update(kwargs)
         
         conn = _mysql.connect( **self.connargs )
         conn.query( self.sql )
-        result = conn.get_result()
+        result = conn.get_result()   ## this takes the time
         
         a = np.zeros( int(self['limit']) , self.dtype )
         meth = getattr( npmy , "fetch_rows_into_array_%d" % self['method'] )
         meth( result, a )
+
+        result.clear()      ## ESSENTIAL MEMORY CLEANUP
+        result = None 
+        conn.close()
+        
         if 'verbose' in kwargs:print a
         a = None
-        conn.close()
+
 
 
 def test_cyth():
@@ -172,7 +239,6 @@ class Task(dict):
     name =  property( lambda self:self._name % self )
 
     def __call__(self):
-        if 'verbose' in self:print self
         timer = Timer( self.stmt, self.setup )
         try:
             time_=timer.timeit(1)  
@@ -180,12 +246,17 @@ class Task(dict):
             time_=-1
             timer.print_exc()
             sys.exit(1)
-        return dict( time_=time_, rss_=rss(), task_=self.name )
-  
+
+        res = dict( time_=time_, rss_=rss(), task_=self.name )
+        self.update( **res ) 
+        if 'verbose' in self:
+            print self
+        return res
+
 
 class LimitScan(Scan):
-    steps = 11 
-    max   = 100000   ## 1000000 Pure is dying well before get to 1M 
+    steps = 5 
+    max   = 1000000   
 
 class DebugScan(Scan):
     steps = 10 
@@ -194,39 +265,64 @@ class DebugScan(Scan):
 
 
 if __name__=="__main__":
+    pass
 
-    if 0:
-        test_pure()
-        #test_cyth()
-        import sys
-        sys.exit(0)
+if 0:
+    conn = MySQLdb.connect(  read_default_file="~/.my.cnf", read_default_group="client" )
+    cursor = conn.cursor()
 
+    q = Fetch("DcsPmtHv", limit=100 )
 
-    name, dbconf = "DcsPmtHv", "client"
+    # cursor.execute( q )   
+    # does 
+    #   _clear 
+    #   _query  ... 
+    #         _flush 
+    #         cursor._result = Result(cursor) 
+    #      BUT instantiation of the Result object 
+    #         ... pulls the rows and flushes the result
+    #             setting result to None
+    #      SO NO GOOD    
+
+    cursor._clear()
+
+    # adapt from  from cursor._query 
+    db = cursor._get_db()    ## the  _mysql.connection  instance 
+    cursor._flush()          ## clears result 
+    cursor._executed = q.sql
+    db.query(q.sql)
+
+    # adapt from  Result.__init__
+    result = db.get_result(cursor.use_result)   ## _mysql.result object
+    a = np.fromiter( result  , dtype=q.dtype )
+
+    # after usage 
+    result.clear()
+    result = None
+    print a
+
+if 0:
+    test_pure()
+
+if 0:
+    test_cyth()
+
+if 1: 
+    base = dict(name="DcsPmtHv", dbconf="client", verbose=1 , limit="*" , method=0 )
     scargs = (
-               dict( kls="Pure", method=1, limit="*" , symbol="ro" , verbose=1 , name=name, dbconf=dbconf ),
-               dict( kls="Cyth", method=0, limit="*" , symbol="go" , verbose=1 , name=name, dbconf=dbconf ),
-               dict( kls="Cyth", method=1, limit="*" , symbol="g^" , verbose=1 , name=name, dbconf=dbconf ),
-             )
-
+         dict( kls="Xure", symbol="bo" ),
+         dict( kls="Cyth",  symbol="g^" , method=1 ),
+    )
     for n, scarg in enumerate(scargs):
         if len(sys.argv) > 1 and int(sys.argv[1]) != n:continue  
-        scan = LimitScan( **scarg )
+        scan = LimitScan( **dict( base, **scarg) )
         print "starting scan %s " % repr(scan)
 
         for ctx in scan:
-
             tsk = Task(ctx) 
-
             res = tsk()
-            del tsk 
-            tsk = None
-
             scan( **res )   ## record result in the scan structure at this cursor point 
-
 
         print repr(scan.scan) 
         scan.save()
-        del scan
-        scan = None
 
