b2656cc5c93896af9cdd46172822f3926b6b74d7
Simon C Blyth <simoncblyth@gmail.com>
2010-12-02 00:45:10 +0800

    move the import_array to module level and avoid the segmentation + add the convenience Makefile and test.py to the patch

--- mysql_np/tests/test_npdescr.py
+++ mysql_np/tests/test_npdescr.py
@@ -6,15 +6,29 @@ def test_npdescr():
     r = conn.store_result()
     d = r.npdescr()
     assert d[0][1] == "|S64",  d
+    print repr(d) 
 
-def test_nparray():
+
+def test_nparray_1():
     conn = _mysql.connect(  read_default_file="~/.my.cnf", read_default_group="client" ) 
     conn.query("show tables")
     r = conn.store_result()
     a = r.fetch_nparray()
+    print repr(a) 
+    conn.close()
+
+def test_nparray_2():
+    conn = _mysql.connect(  read_default_file="~/.my.cnf", read_default_group="client" ) 
+    conn.query("select * from CalibPmtSpec limit 100")
+    r = conn.store_result()
+    a = r.fetch_nparray()
+    print repr(a) 
+    conn.close()
+
 
 
 if __name__ == '__main__':
     test_npdescr()
-    test_nparray()
+    test_nparray_1()
+    test_nparray_2()
 
