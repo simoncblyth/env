19ad1d694d6bcef93a240268855cbe7e5706aeed
Simon C Blyth <simoncblyth@gmail.com>
2011-10-14 18:51:01 +0800

    digestpath, workarounds for older python ... back to py2.3.4 at least

--- base/digestpath.py
+++ base/digestpath.py
@@ -7,14 +7,21 @@ issues with large files, usage::
 
 http://stackoverflow.com/questions/1131220/get-md5-hash-of-a-files-without-open-it-in-python
 """
-import sys, hashlib
+import sys
+
+try: 
+    from hashlib import md5
+except ImportError: 
+    from md5 import md5
+
 def digestpath( path ):
-    md5 = hashlib.md5()
+    hash = md5()
     size = 64*128   # 8192
-    with open(path,'rb') as f: 
-        for chunk in iter(lambda: f.read(size), ''): 
-            md5.update(chunk)
-    return md5.hexdigest()
+    f = open(path,'rb') 
+    for chunk in iter(lambda: f.read(size), ''): 
+        hash.update(chunk)
+    f.close()
+    return hash.hexdigest()
 
 if __name__ == '__main__':
     print digestpath(sys.argv[1])
