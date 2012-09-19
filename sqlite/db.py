71cbf617ed556121d387f2bc3ca87222327c271a
Simon C Blyth <simoncblyth@gmail.com>
2012-09-19 15:44:06 +0800

    split into general purpose pysqlite and bitten schema specific portions

---
+++ sqlite/db.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+"""
+General purpose pysqlite access to SQLite DB, put schema specifics 
+in other modules keeping this general purpose.
+
+TODO:
+
+* adopt a DBCONF equivalent approach for SQLite DB  
+
+
+"""
+from __future__ import with_statement
+import logging, sys, sqlite3
+log = logging.getLogger(__name__)
+
+class DB(object):
+    def __init__(self, path=None, skip=None):
+        """ 
+        :param path: to DB file
+        :param skip: prefix of table names to not include in counting, for faster testing
+        """ 
+        if not path:
+            path = 'dybsvn/db/trac.db'   # specific leaking in as default
+
+        log.info("opening %s " % path )
+        conn=sqlite3.connect(path)
+        cur=conn.cursor()
+        self.conn = conn
+        self.path = path
+        self.skip = skip
+        self.count = -1
+        self.tables = self.tables_() 
+
+    def __call__(self, sql):
+        return self.fetchall(sql)
+
+    def execute_(self, sql):
+        cursor = self.conn.cursor()
+        cursor.execute( sql )
+        return cursor
+
+    def fetchall(self, sql ):
+        log.debug(sql)
+        cursor = self.execute_(sql)
+        rows = cursor.fetchall()
+        self.count = cursor.rowcount
+        cursor.close()
+        return rows
+
+    def tables_(self):
+        tabs = []
+        for _ in self("select name from sqlite_master where type='table'"):
+            tabs.append(_[0])
+        return tabs
+
+    def count_(self):
+        """
+        Initially return a list of tuples, requiring double indexing to get to the count
+        """
+        count={}
+        for tab in self.tables:
+            if self.skip and tab.startswith(self.skip):continue  # skip biggies for speed
+            sql = "select count(*) from %(tab)s" % locals()
+            ret = self(sql)
+            assert len(ret) == 1, ret
+            ret = ret[0]
+            assert len(ret) == 1, ret
+            count[tab] = ret[0] 
+        return count
+
+
+    def db_open(self):
+        """
+        standin for trac/admin/console API
+        """ 
+        return self.conn
+
+    def arbitary_(self, path ):
+        """
+        http://trac.edgewall.org/pysqlite.org-mirror/ticket/259
+
+        How to test the corners of errorspace here ?
+        How to test in contentious environment ?
+        """
+        sql = open(path,"r").read()
+        cnx = self.db_open()
+        cur = cnx.cursor()
+        try:
+            print 'Running arbitary script %s sql %s against %s ...' % (script, sql, self.path),
+            cur.executescript(sql)
+        except sqlite3.Error:
+            cnx.rollback()
+            raise
+        else:
+            cnx.commit()   # Error from this commit are not caught 
+        finally:           # a SyntaxError here indicates you are using an old python, try python- to pick up source one
+            cur.close()
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.INFO)
+    n = len(sys.argv)
+    path = sys.argv[1] if n > 1 else None 
+    script = sys.argv[2] if n > 2 else None
+
+    db = DB(path, skip='bitten')
+    print db.tables
+    if script:
+        db.arbitary_(script) 
+    else:
+        cnt = db.count_()
+        print "\n".join(["%-30s : %s " % (t, cnt[t]) for t in sorted(cnt, key=lambda _:cnt[_])])
+
+
+
--- sqlite/shrink.py
+++ sqlite/shrink.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 """
-
 Extract trac.db from tarball and interactively check with::
 
 	[dayabay] /tmp/tt > tar zxf dybsvn.tar.gz dybsvn/db/trac.db
@@ -45,14 +44,6 @@ CREATE TABLE bitten_log_message (
     UNIQUE (log,line)
 );
 
-sqlite> select max(length(message)) from bitten_log_message ;
-3571
-
-sqlite> select sum(length(message)) from bitten_log_message ;
-2048409596
-                            ## 2 billion chars of log messages
-
-
 sqlite> .schema bitten_report_item  
 CREATE TABLE bitten_report_item (
     report integer,
@@ -62,76 +53,28 @@ CREATE TABLE bitten_report_item (
     UNIQUE (report,item,name)
 );
 
+sqlite> select max(length(message)) from bitten_log_message ;
+3571
 
-
-
-
-
-
-
-
-
-
-
-The build id are monotonic with rev (or nearly so) : can just kill up to a certain manually determined build id
-
-DELETE FROM bitten_build  WHERE id < 10000 
-
-DELETE FROM bitten_error  WHERE build < 10000 
-DELETE FROM bitten_step   WHERE build < 10000 
-DELETE FROM bitten_slave  WHERE build < 10000 
-
-DELETE FROM bitten_log     WHERE build < 10000 
-DELETE FROM bitten_report  WHERE build < 10000 
-
-
-DELETE
-select count(*) FROM bitten_log_message WHERE log < (SELECT max(id) FROM bitten_log WHERE build < 10000 )
+sqlite> select sum(length(message)) from bitten_log_message ;
+2048409596
+## 2 billion chars of log messages
 
 
 sqlite> select count(*) FROM bitten_log_message WHERE log < (SELECT max(id) FROM bitten_log WHERE build < 10000 ) ;
 10478700
+
 sqlite> SELECT max(id) FROM bitten_log WHERE build < 10000 ;
 148781
+
 sqlite> select count(*) FROM bitten_log_message ;
 39656123
 sqlite> 
-
-
-select count(*) FROM bitten_report_item WHERE report < (SELECT max(id) FROM bitten_report WHERE build < 10000 )
-
-
-
-
-
-
-
-
-
-
-sqlite> .schema bitten_build    
-CREATE TABLE bitten_build (
-    id integer PRIMARY KEY,
-    config text,
-    rev text,
-    rev_time integer,
-    platform integer,
-    slave text,
-    started integer,
-    stopped integer,
-    status text
-);
-CREATE INDEX bitten_build_config_rev_slave_idx ON bitten_build (config,rev,slave);
-sqlite> 
-
-
-   The rev is text, to support git and others with digest revisions
 
 sqlite> select cast(rev as integer) from bitten_build limit 10 ;
 3953
 3952
 3957
-
 
 sqlite> select count(*) from bitten_build where cast(rev as int) < 10000 ;
 3175
@@ -142,41 +85,8 @@ sqlite> select count(distinct(id)) from bitten_build where cast(rev as int) < 10
 sqlite> select count(distinct(id)) from bitten_build where cast(rev as int) > 10000 ;
 9995
 
-
-
-
 sqlite> SELECT min(rev+0),max(rev+0) FROM bitten_build  ;
 3952|17979
-
-
-sqlite> SELECT count(*) FROM bitten_build where rev+0 < 10000 ;        # is there a better way to coerce 
-3175
-sqlite> SELECT count(distinct(id)) FROM bitten_build where rev+0 < 10000 ;
-3175
-
-
-sqlite> SELECT count(*) FROM bitten_build where rev+0 > 10000 ;
-9995
-sqlite> SELECT count(distinct(id)) FROM bitten_build where rev+0 > 10000 ;
-9995
-
-
-
-
-
-
-
-
-
-
-DELETE FROM bitten_log_message WHERE log IN (SELECT id FROM bitten_log WHERE build IN (SELECT id FROM bitten_build WHERE rev < 23000 AND config = 'trunk'))
-DELETE FROM bitten_log WHERE build IN (SELECT id FROM bitten_build WHERE rev < 23000 AND config = 'trunk')
-DELETE FROM bitten_error WHERE build IN (SELECT id FROM bitten_build WHERE rev < 23000 AND config = 'trunk')
-DELETE FROM bitten_step WHERE build IN (SELECT id FROM bitten_build WHERE rev < 23000 AND config = 'trunk')
-DELETE FROM bitten_slave WHERE build IN (SELECT id FROM bitten_build WHERE rev < 23000 AND config = 'trunk')
-DELETE FROM bitten_build WHERE rev < 23000 AND config = 'trunk'
-
-
 
 
 sqlite> select distinct(config) from bitten_build;
@@ -189,104 +99,62 @@ opt.dybinst
 
 
 """
+from __future__ import with_statement
 import logging, sys, sqlite3
 log = logging.getLogger(__name__)
 
 
-class DB(object):
-    def __init__(self, path=None):
-        if not path:
-            path = 'dybsvn/db/trac.db'
-        conn=sqlite3.connect(path)
-        cur=conn.cursor()
-        self.conn = conn
-        self.path = path
-        self.count = -1
-        self.tables = self.tables_() 
+class Shrink(dict):
+    """
 
-    def __call__(self, sql):
-        return self.fetchall(sql)
+    The build id are monotonic with rev (or nearly so) : can just kill up to a certain manually determined build id
 
-    def execute_(self, sql):
-        cursor = self.conn.cursor()
-        cursor.execute( sql )
-        return cursor
+    TODO:
 
-    def fetchall(self, sql ):
-        log.debug(sql)
-        cursor = self.execute_(sql)
-        rows = cursor.fetchall()
-        self.count = cursor.rowcount
-        cursor.close()
-        return rows
+    * devise group by queries to verify the above statement
+      ( note that the rev is text, to support git and others with digest revisions )
+    * check for any cascade deleting docs wrt SQLite    
 
-    def tables_(self):
-        tabs = []
-        for _ in self("select name from sqlite_master where type='table'"):
-            tabs.append(_[0])
-        return tabs
+    Usage::
 
-    def count_(self):
-        """
-        Initially return a list of tuples, requiring double indexing to get to the count
-        """
-        count={}
-        for tab in self.tables:
-            if tab.startswith('bitten'):continue  # skip biggies for speed
-            sql = "select count(*) from %(tab)s" % locals()
-            ret = self(sql)
-            assert len(ret) == 1, ret
-            ret = ret[0]
-            assert len(ret) == 1, ret
-            count[tab] = ret[0] 
-        return count
+         shk = Shrink(action="select count(*)", bid=10000)
+         sql = repr(shk)
+         shk = Shrink(action="DELETE", bid=10000)
+         sql = repr(shk)
+
+    """
+    tmpl = r"""
+
+%(action)s FROM bitten_log_message WHERE log < (SELECT max(id) FROM bitten_log WHERE build < %(bid)s ) ;
+%(action)s FROM bitten_report_item WHERE report < (SELECT max(id) FROM bitten_report WHERE build < %(bid)s ) ;
+
+%(action)s FROM bitten_build  WHERE id < %(bid)s  ;
+
+%(action)s FROM bitten_error  WHERE build < %(bid)s ;
+%(action)s FROM bitten_step   WHERE build < %(bid)s ; 
+%(action)s FROM bitten_slave  WHERE build < %(bid)s ;
+%(action)s FROM bitten_log     WHERE build < %(bid)s ; 
+%(action)s FROM bitten_report  WHERE build < %(bid)s ;
+
+"""
+    __repr__ = lambda self:self.tmpl % self
 
 
-    def db_open(self):
-        """
-        standin for trac/admin/console API
-        """ 
-        return self.conn
-
-    def arbitary_(self, path ):
-        """
-        http://trac.edgewall.org/pysqlite.org-mirror/ticket/259
-
-        How to test the corners of errorspace here ?
-        How to test in contentious environment ?
-        """
-        sql = open(path,"r").read()
-        cnx = self.db_open()
-        cur = cnx.cursor()
-        try:
-            print 'Running arbitary script %s sql %s against %s ...' % (script, sql, self.path),
-            cur.executescript(sql)
-        except sqlite3.Error:
-            cnx.rollback()
-            raise
-        else:
-            cnx.commit()   # Error from this commit are not caught 
-        finally:
-            cur.close()
-
-
-    def builds_(self, where="cast(rev as int) < 10000"):
-        sql = "select id from bitten_build where %(where)s " % locals()
-
+def builds_(where="cast(rev as int) < 10000"):
+    sql = "select id from bitten_build where %(where)s " % locals()
+    return sql 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
-    n = len(sys.argv)
-    path = sys.argv[1] if n > 1 else None 
-    script = sys.argv[2] if n > 2 else None
-    db = DB(path)
-    print db.tables
-    if script:
-        db.arbitary_(script) 
-    else:
-        cnt = db.count_()
-        print "\n".join(["%-30s : %s " % (t, cnt[t]) for t in sorted(cnt, key=lambda _:cnt[_])])
-
+    shk = Shrink(action="select count(*)", bid=10000)
+    sql = repr(shk)
+    path = "/tmp/env/shrink/count.sql" 
+    dir = os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    with open(path,"w") as fp:
+        fp.write(sql)
+    print sql  
 
 
