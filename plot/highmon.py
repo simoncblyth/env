7259679e9ae2fcdbe39b86d636e2a45894e526d6
Simon C Blyth <simoncblyth@gmail.com>
2012-11-13 13:55:10 +0800

    handle notification of violations in the env.plot.highmon.HighMon base class

--- plot/highmon.py
+++ plot/highmon.py
@@ -5,41 +5,82 @@ Base class for monitoring JSON data in HighStock format, usage::
     from env.plot.highmon import HighMon
 
 """
-import inspect, urllib2, logging
+import os, inspect, urllib2, logging
+from env.tools.sendmail import sendmail
 log = logging.getLogger(__name__)
+
 try:
     import json
 except ImportError:
     import simplejson as json
         
 def read_json( url ):
+    """
+    TODO: handlefailure to connect by returning None
+    """
     req = urllib2.Request( url )
     opener = urllib2.build_opener()
     f = opener.open(req)
     return json.load(f)
 
+class Violation(dict):
+    def __repr__(self):
+        return "method:%(method)s series:%(name)s msg:%(msg)s" % self
 
 class HighMon(list):
     @classmethod
     def introspect_monitor_methods(cls):
         return [(k,v) for k,v in inspect.getmembers(cls) if k[0:8] == 'monitor_']
 
-    def __init__(self, url):
+    def __init__(self, url, email=None):
+        """
+        Defer loading js to the `__call__`, as that might fail and wish to 
+        handle failures by sending notification emails
+        """
         self.url = url
-        self.js = read_json(url)
+        self.email = email
         self.mm = self.introspect_monitor_methods()
+        self.js = None
         list.__init__(self)
 
-    def __call__(self):
+    def __repr__(self):
+        return "\n".join(map(repr, self))
+
+    def add_violation(self, method, series, msg ):
+        v = Violation(method=method, name=name, msg=msg )
+        self.append(v)
+
+    def _load(self):
+        self.js = read_json(self.url)
+        if not self.js:
+            self.add_violation( method="_load", name="", msg="failed to load JSON from url %s " % self.url )                
+
+    def _constrain(self):
+        if not self.js:
+            log.warn("skip method calls as json not loaded")
+            return
         for series in self.js['series']:
             for method_name, method in self.mm:
                 method(self, method=method_name, series=series )
-  
-    def monitor_example_(self, method, series):
-        name = series['name']
-        data = series['data']
-        last = data[-1]
-        log.info( "method %s name %s len data %s last %r  " % (method,name,len(data),last ))
+
+    def _notify(self):
+        if len(self) == 0:
+            log.info("no violations, not sending email")
+            return
+        msg = repr(self)
+        log.warn("%s violations, sending email\n%s\n" % ( len(self), msg ))
+        if self.email:
+            for _ in self.email.split():
+                log.warn("sendmail to %s " % _ )
+                sendmail( msg, _ )
+        else:
+            log.warn("email address(s) for notification not configured")
+
+    def __call__(self):
+        self._load()
+        self._constrain()
+        self._notify()
+
 
 
 class ExampleHighMon(HighMon):
@@ -49,13 +90,18 @@ class ExampleHighMon(HighMon):
         print method
     def monitor_othername(self, method, series ):
         print method
+    def monitor_example_(self, method, series):
+        name = series['name']
+        data = series['data']
+        last = data[-1]
+        log.info( "method %s name %s len data %s last %r  " % (method,name,len(data),last ))
 
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     url = "http://dayabay.ihep.ac.cn/data/scm_backup_monitor_SDU.json"
-    ehm = ExampleHighMon(url)
+    ehm = ExampleHighMon(url, email=os.environ['SCM_HIGHMON_EMAIL'] )
     ehm()     
 
 
--- scm/tgzmon.py
+++ scm/tgzmon.py
@@ -1,22 +1,67 @@
 #!/usr/bin/env python
 """
 
+TODO:
+
+TZ debugging
+
+simon:scm blyth$ ./tgzmon.py 
+DEBUG:__main__:utcnow 2012-11-13 05:49:06.341120 
+DEBUG:__main__:stamp  2012-11-13 08:00:00 
+DEBUG:__main__:method monitor_age name OK stamp 2012-11-13 08:00:00 age -1 day, 21:49:06.341120 
+DEBUG:__main__:utcnow 2012-11-13 05:49:06.341120 
+DEBUG:__main__:stamp  2012-11-13 20:00:01 
+DEBUG:__main__:method monitor_age name svn/dybaux stamp 2012-11-13 20:00:01 age -1 day, 9:49:05.341120 
+DEBUG:__main__:utcnow 2012-11-13 05:49:06.341120 
+DEBUG:__main__:stamp  2012-11-13 20:00:01 
+DEBUG:__main__:method monitor_age name svn/dybsvn stamp 2012-11-13 20:00:01 age -1 day, 9:49:05.341120 
+DEBUG:__main__:utcnow 2012-11-13 05:49:06.341120 
+DEBUG:__main__:stamp  2012-11-13 20:00:01 
+DEBUG:__main__:method monitor_age name tracs/dybaux stamp 2012-11-13 20:00:01 age -1 day, 9:49:05.341120 
+DEBUG:__main__:utcnow 2012-11-13 05:49:06.341120 
+DEBUG:__main__:stamp  2012-11-13 20:00:01 
+DEBUG:__main__:method monitor_age name tracs/dybsvn stamp 2012-11-13 20:00:01 age -1 day, 9:49:05.341120 
+INFO:env.plot.highmon:no violations, not sending email
+
 """
 
 import logging
+from datetime import datetime, timedelta
 log = logging.getLogger(__name__)
 from env.plot.highmon import HighMon
 
 class TGZMon(HighMon):
+    """
+    """
     def __init__(self, *args, **kwa ):
         HighMon.__init__(self, *args, **kwa )
+        self.utcnow = datetime.utcnow()
+        self.maxage = timedelta(hours=24)   
+
+    def monitor_age(self, method, series ):
+        """
+        :param method: name 
+        :param series: 
+
+        Timestamps are UTC ms since epoch, the age is obtained from the 
+        delta of two naive UTC datetimes
+        """
+        stamp = datetime.fromtimestamp( series['data'][-1][0]/1e3 )  
+        age = self.utcnow - stamp                         
+        log.debug("utcnow %s " % self.utcnow )
+        log.debug("stamp  %s " % stamp  )
+        log.debug("method %s name %s stamp %s age %s " % ( method, series['name'], stamp, age ))
+        if age > self.maxage:
+            msg="age %r exceeds maximum allowable %r  " % ( age, self.maxage ) 
+            self.add_violation( method=method, name=series['name'], msg=msg )
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     url = "http://dayabay.ihep.ac.cn/data/scm_backup_monitor_SDU.json"
-    mon = TGZMon(url)
+    mon = TGZMon(url, email="blyth@hep1.phys.ntu.edu.tw" )
     mon()
+    assert len(mon) == 0 , repr(mon) 
 
 
 
