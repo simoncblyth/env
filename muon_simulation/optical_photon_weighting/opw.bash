a9ae0e19256a562d35786f59e2f62c3889ae744f
Simon C Blyth <simoncblyth@gmail.com>
2013-07-24 20:20:47 +0800

    opw- bash funcs for trying out optical photon weighting

--- muon_simulation/optical_photon_weighting/opw.bash
+++ muon_simulation/optical_photon_weighting/opw.bash
@@ -14,7 +14,11 @@ FUNCTIONS
 *opw-get*
     checkout my people copy of Davids OPW
 
+*opw-gen-muons*
+    10k muon vectors took ~6 seconds to generate, the sample is reproducible::
 
+        [blyth@belle7 OPW]$ diff  tenthousandmuons tenthousandmuons.1 
+        [blyth@belle7 OPW]$ 
 
 EOU
 }
@@ -22,11 +26,49 @@ opw-dir(){ echo $(local-base)/env/muon_simulation/optical_photon_weighting/OPW ;
 opw-cd(){  cd $(opw-dir); }
 opw-mate(){ mate $(opw-dir) ; }
 opw-get(){
-   local dir=$(dirname $(opw-dir)) &&  mkdir -p $dir && cd $dir
-   local nam=$(basename $(opw-dir))
+    local dir=$(dirname $(opw-dir)) &&  mkdir -p $dir && cd $dir
+    local nam=$(basename $(opw-dir))
 
-   [ -z "$DYBSVN" ] && echo $msg missing DYBSVN && return 1 
-   [ ! -d "$nam" ] && svn co $DYBSVN/people/blyth/$nam
-   [   -d "$nam" ] && svn up $nam
-
+    [ -z "$DYBSVN" ] && echo $msg missing DYBSVN && return 1 
+    [ ! -d "$nam" ] && svn co $DYBSVN/people/blyth/$nam
+    [   -d "$nam" ] && svn up $nam
 }
+
+opw-prep(){
+    opw-gen-muons
+}
+
+opw-gen-muons-smpl(){ echo $(opw-dir)/tenthousandmuons ; }
+opw-gen-muons(){
+    type $FUNCNAME
+    opw-cd
+    local smpl=$(opw-gen-muons-smpl)
+    [ -f "$smpl" ] &&  echo $msg sample already created $smpl && return 0
+   
+    time Muon.exe -n 10000 -s DYB -seed 1377339 -r Yes -v RPC -music_dir $SITEROOT/../external/data/0.0/Muon > $smpl
+    wc $smpl
+    du -hs $smpl
+}
+
+opw-sim(){
+   type $FUNCNAME
+    [ "$(which nuwa.py 2> /dev/null)" == "" ] && echo $msg setup nuwa environment first laddie && return 1
+   opw-cd
+
+   mkdir -p out log 
+   local tag=226
+   time nuwa.py -R 3 -n 1000 -m "$(opw-sim-args)" -o out/$tag.root >& log/$tag.log &
+   #time nuwa.py -R 3 -n 1000 -m "$(opw-sim-args)" -o out/$tag.root 
+}
+
+opw-sim-args(){ cat << EOA
+     fmcpmuon --use-pregenerated-muons --use-basic-physics 
+        --wsLimit=1 
+        --wsWeight=1 
+        --adVolumes=['oil','lso','gds'] 
+        --adLimits=[1,3000,1000] 
+        --adWeights=[1,100,100]
+
+EOA
+}
+
