cea59416063cfeeb7aca5a59a7525657b93add09
Simon C Blyth <simoncblyth@gmail.com>
2013-11-11 17:59:40 +0800

    add dae/tree/name.png uploading capability, as convenient way to transfer renders from shared laptop into samples without fiddling with email/logins/ssh/usb-sticks etc.. on shared machine

--- geant4/geometry/daeserver/daeserver.py
+++ geant4/geometry/daeserver/daeserver.py
@@ -19,15 +19,13 @@ With webpy SCGI deployment with apache
 * http://localhost/dae/tree/0___0.dae
 
 
-
-
-
 """
 import os, sys, logging
 log = logging.getLogger(__name__)
 import web
 from env.graphics.collada.pycollada.daenode import DAENode, getSubCollada, DAESubTree, getTextTree
 
+opts = None
 
 class Defaults(object):
     logpath = None
@@ -36,6 +34,8 @@ class Defaults(object):
     daepath = "$LOCAL_BASE/env/geant4/geometry/xdae/g4_01.dae"
     port = "8080"
     webpy = "scgi"
+    uploads = False
+    uploaddir = "$LOCAL_BASE/env/geant4/geometry/daeserver/samples"
 
 def parse_args(doc):
     from optparse import OptionParser
@@ -45,6 +45,8 @@ def parse_args(doc):
     op.add_option("-l", "--loglevel",   default=defopts.loglevel, help="logging level : INFO, WARN, DEBUG. Default %default"  )
     op.add_option("-f", "--logformat", default=defopts.logformat , help="logging format" )
     op.add_option("-p", "--daepath", default=defopts.daepath , help="Path to the original geometry file. Default %default ")
+    op.add_option(      "--uploads", default=defopts.uploads , help="Allow receiving uploaded PNGs. Default %default ")
+    op.add_option(      "--uploaddir", default=defopts.uploaddir , help="Directory to store uploaded PNGs. Default %default ")
     op.add_option(      "--port",  default=defopts.port , help="Webserving port passed to webpy. Default %default ")
     op.add_option("-w", "--webpy", default=defopts.webpy , help="Webserving protocal config argv passed to webpy eg scgi fcgi. Default %default ")
 
@@ -102,15 +104,77 @@ class _tree_r62_html:
         subtree = DAESubTree( node, maxdepth=maxdepth_(arg), text=False )
         return r62_render.daeload(arg, node, subtree )
 
+
+def find_unique_path( dir, name):
+    names = os.listdir(dir)
+    if not name in names:
+        return os.path.join(dir, name)  
+    base, ext = os.path.splitext(name) 
+    counter = 1 
+    uname = name
+    while uname in names:
+        uname = base + "_%0.2d" % counter + ext
+        counter += 1
+    return os.path.join(dir, uname)    
+
+class _tree_png:
+    def PUT(self, name):
+        """
+        ::
+
+            curl -T snapshot05.png http://belle7.nuu.edu.tw/dae/tree/
+            curl -T "snapshot0[0-5].png" http://belle7.nuu.edu.tw/dae/tree/    
+                # curl needs to do the globbing not the shell, so need the quotes
+
+        For large files (>1M) getting "413: Request Entity too large" 
+        from nginx until increase the limit with the below inside the 
+        http block of nginx config::
+
+              client_max_body_size 2M;
+       
+        Currently samples just listed by nginx at
+ 
+        * http://belle7.nuu.edu.tw/samples/
+
+        To do something more could use X-Sendfile approach, together with 
+        a dynamic listing.
+
+        * http://wiki.nginx.org/X-accel
+        * http://wiki.nginx.org/XSendfile
+ 
+        """
+        if not opts.uploads:
+            return "uploading is disabled, restart server with `--uploads` option to allow " 
+        log.info("PUT received %s  " % (name) )
+        updir = os.path.expandvars(opts.uploaddir) 
+        if name.startswith("..") or name.find("/") > -1:
+            log.info("PUT request with disallowed resource name %s " % name )
+            return None
+        if not os.path.exists(updir):
+            log.info("upload dir does not exist  %s " % updir )
+            return None
+        pass
+        data = web.data()
+        log.info("PUT data received %s : %s bytes " % (name,len(data)) )
+        path = find_unique_path( updir, name )     
+        fp = open(path, "wb")
+        fp.write(data)
+        fp.close()
+        log.info("PUT %s wrote %s bytes to %s  " % (name,len(data), path) )
+        return "OK"
+
+
 URLS = (
           '/',                              '_index', 
           '/tree/(.+)?.webglbook.html',     '_tree_webglbook_html',
           '/tree/(.+)?.html',               '_tree_r62_html',
           '/tree/(.+)?.dae',                '_tree_dae',
           '/tree/(.+)?.txt',                '_tree_txt',
+          '/tree/(.+\.png)',                '_tree_png',
        )
 
 def main():
+    global opts
     opts, args = parse_args(__doc__) 
     log.info("daeserver startup with webpy %s " % web.__version__ ) 
     DAENode.parse( opts.daepath )
