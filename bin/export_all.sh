f953e82d3890ada248d4d083ff44111daa89d683
Simon C Blyth <simoncblyth@gmail.com>
2013-11-19 13:51:09 +0800

    recompile libG4VRML.so with formatting precision fix

---
+++ bin/export_all.sh
@@ -0,0 +1,1 @@
+../geant4/geometry/gdml/export_all.sh
--- geant4/g4.bash
+++ geant4/g4.bash
@@ -19,6 +19,46 @@ g4-get(){
 
 }
 
-g4-vrml(){ g4-cd source/visualization/VRML/src/$1 ; }
 g4-gdml(){ g4-cd source/persistency/gdml/src/$1 ; }
+
+
+g4-vrml(){ g4-cd source/visualization/VRML/src/$1 ; }
+g4-vrml-dir(){ echo $(g4-dir)/source/visualization/VRML ; }
+g4-vrml-libname(){ echo libG4VRML.so ; }
+
+g4-vrml-deploy(){
+  cd $(env-home)/geant4/source
+  DEPLOY_MODE="diff" ./deploy.sh 
+  DEPLOY_MODE="cp" ./deploy.sh 
+}
+
+g4-vrml-ls(){
+  cd $(g4-vrml-dir)
+  local name=$(g4-vrml-libname)
+  local lib=../../../lib/Linux-g++/$name
+  ls -l $lib $(nuwa-g4-libdir)/$name
+}
+
+g4-vrml-make(){
+  local msg="=== $FUNCNAME :" 
+  type $FUNCNAME
+
+  cd $(g4-vrml-dir)
+
+  local name=$(g4-vrml-libname)
+  local lib=../../../lib/Linux-g++/$name
+  echo $msg before
+  ls -l $lib $(nuwa-g4-libdir)/$name
+
+  export VERBOSE=1
+  rm -f $lib && make CLHEP_BASE_DIR=$(nuwa-clhep-idir) G4SYSTEM=Linux-g++ G4LIB_BUILD_SHARED=1 && cp $lib $(nuwa-g4-libdir)/
+
+  echo $msg after
+  ls -l $lib $(nuwa-g4-libdir)/$name
+}
+
+
+
+
+
 
