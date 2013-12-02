176c7f55e1e46103b9571e7165e0f81f703eab27
Simon C Blyth <simoncblyth@gmail.com>
2013-12-02 18:30:12 +0800

    add zipfile generation to diff.py, allowing to archive files that differ between a pristine and modified tree

--- base/diff.py
+++ base/diff.py
@@ -7,7 +7,28 @@ Finds files that differ and files that only
 appear in left or right trees. Generated files
 can be ignored via ``ignore_`` arguments.
 
-Usage example::
+Commandline example::
+
+   diff.py MeshLabSrc_AllInc_v132 meshlab -z mlab.zip
+
+This recursively look for differences between
+directory trees and adds new files on the **right** 
+(ie from ``meshlab``) as well as common files that 
+differ into the zipfile.
+
+This allows quick archiving of ad-hoc developments 
+in an unmanaged(not in SCM) tree. 
+
+NB the directory argument order determines which new files get 
+added to the zip. The default files to write to the zip:
+
+#. files in common between left and right that differ
+#. new files on the right
+
+Due to this remember to distclean on the right to avoid zipping
+generated files.
+
+Usage from python example::
 
     from env.base.diff import Diff
     dqt = Diff.qt( "meshlab_pristine", "meshlab" )
@@ -18,11 +39,50 @@ Usage example::
 
 Implementation based on filecmp.dircmp
 
+
 """
 import os, sys, logging
 from filecmp import dircmp
+from zipfile import ZipFile
 log = logging.getLogger(__name__)
-       
+
+
+class Defaults(object):
+    logpath = None
+    loglevel = "INFO"
+    logformat = "%(asctime)s %(name)s %(levelname)-8s %(message)s"
+    zippath = None  
+    report = False
+
+def parse_args(doc):
+    from optparse import OptionParser
+    defopts = Defaults()
+    op = OptionParser(usage=doc)
+    op.add_option("-o", "--logpath", default=defopts.logpath , help="logging path" )
+    op.add_option("-l", "--loglevel",   default=defopts.loglevel, help="logging level : INFO, WARN, DEBUG. Default %default"  )
+    op.add_option("-f", "--logformat", default=defopts.logformat , help="logging format" )
+    op.add_option("-z", "--zippath", default=defopts.zippath , help="Path to zipfile to be created. Default %default ")
+    op.add_option("-r", "--report", action="store_true", default=defopts.report , help="Full closure report. Default %default ")
+
+    opts, args = op.parse_args()
+
+    level = getattr( logging, opts.loglevel.upper() )
+    if opts.logpath:  # logs to file as well as console, needs py2.4 + (?)
+        logging.basicConfig(format=opts.logformat,level=level,filename=opts.logpath)
+        console = logging.StreamHandler()
+        console.setLevel(level)
+        formatter = logging.Formatter(opts.logformat)
+        console.setFormatter(formatter)
+        logging.getLogger('').addHandler(console)  # add the handler to the root logger
+    else:
+        logging.basicConfig(format=opts.logformat,level=level)
+    pass
+    log.info(" ".join(sys.argv))
+    if not opts.zippath is None:
+        opts.zippath = os.path.expandvars(os.path.expanduser(opts.zippath))
+    return opts, args
+
+
 class Diff(dict):
     """
     For comparing similar directory trees, looking for files
@@ -47,6 +107,11 @@ class Diff(dict):
         self.top = top
         self.recurse(top)
 
+    def collect(self, label, rec):
+        if label not in self:
+            self[label] = []
+        self[label].append(rec)
+
     def visit_only( self, label, cf, name):
         igtype = self.ignore_type(name)
         ighead = self.ignore_head(name)
@@ -58,12 +123,7 @@ class Diff(dict):
             base = getattr(cf, label)
             path = "%s/%s" % ( base, name )
             log.debug("only %s : %s " % (label, path) )
-            self.collect_only(label, path)
-
-    def collect_only(self, label, path):
-        if label not in self:
-            self[label] = []
-        self[label].append(path)
+            self.collect(label, (path,))
 
     def visit_diff(self, cf):
         for name in cf.diff_files:
@@ -76,14 +136,7 @@ class Diff(dict):
             lpath = "%s/%s" % ( cf.left, name )
             rpath = "%s/%s" % ( cf.right, name )
             pass
-            self.collect_diff( path, lpath, rpath )
-
-    def collect_diff(self, path, lpath, rpath, label='diff'):
-        if label not in self:
-            self[label] = []
-        rec =  ( path , lpath, rpath, )
-        self[label].append( rec )
-        log.debug(label + " %s : %s %s  " % rec)
+            self.collect( 'diff', (path, lpath, rpath,) )
 
     def ignore_type(self, name):
         base, ext = os.path.splitext(name)
@@ -107,14 +160,48 @@ class Diff(dict):
             self.recurse(sf)
 
 
+
+def zipdiff( diff, zippath, keys ):
+    """
+    # CAUTION : SINGLE LEVEL ASSUMPTION FOR NEW DIR, 
+    # TODO: RECURSIVE ADDING 
+    """
+    zf = ZipFile(zippath,"w")
+    for k in keys:
+        for _ in diff[k]:
+            path = _[-1]
+            if os.path.isdir(path):
+                for name in os.listdir(path):
+                    print name
+                    subpath = os.path.join(path, name)
+                    print subpath
+                    zf.write(subpath) 
+            else:
+                zf.write(path) 
+        pass
+    pass    
+    zf.close()
+
+
+
 def main():
-    logging.basicConfig(level=logging.INFO)
-    dqt = Diff.qt( *sys.argv[1:] )
-    #print dqt.top.report_full_closure()
+    opts, args = parse_args(__doc__) 
+
+    dqt = Diff.qt( *args )
+    if opts.report:
+        print dqt.top.report_full_closure()
+
     for label, ls in dqt.items():
         print label
         for _ in ls:
             print " " * 5, _
+
+    if not opts.zippath is None:
+        zippath = opts.zippath
+        keys = "diff right".split()
+        log.info("writing %s to zippath %s " % ( keys, zippath ))
+        zipdiff( dqt, zippath, keys )
+
 
 if __name__ == '__main__':
     main()
