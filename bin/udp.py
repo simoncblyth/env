4caca0b4c62b18e15bf2afd4113986164fc99ed5
Simon C Blyth <simoncblyth@gmail.com>
2014-03-31 21:08:05 +0800

    make udp.py work remotely

--- bin/udp.py
+++ bin/udp.py
@@ -4,11 +4,11 @@ import os, sys, socket
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     port = os.environ.get("UDP_PORT","15006")
-    ip = os.environ.get("UDP_IP","127.0.0.1")
+    host = os.environ.get("UDP_HOST","127.0.0.1")
     
-    arg = sys.argv[1]
-    print "sending [%s] to ip:port %s:%s " % (arg, ip, port) 
-    sock.sendto(arg, (ip,int(port)))
+    arg = " ".join(sys.argv[1:])
+    print "sending [%s] to host:port %s:%s " % (arg, host, port) 
+    sock.sendto(arg, (host,int(port)))
 
 if __name__ == '__main__':
     main()
