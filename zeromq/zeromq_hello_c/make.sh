af009c0122864a0352ce847c7517bfcc0b376c9e
Simon C Blyth <simoncblyth@gmail.com>
2014-05-01 21:01:59 +0800

    generalize zeromq hello building

--- zeromq/zeromq_hello_c/make.sh
+++ zeromq/zeromq_hello_c/make.sh
@@ -1,21 +1,22 @@
-#!/bin/bash
+#!/bin/bash -l
 
-PREFIX=$VIRTUAL_ENV
+zeromq-
+PREFIX=$(zeromq-prefix)
 
-make-cmd(){ cat << EOC
+hello-make-cmd(){ cat << EOC
 cc -I$PREFIX/include -c $name.c && cc -L$PREFIX/lib -lzmq $name.o -o $out && rm $name.o 
 EOC
 }
 
-make(){
+hello-make(){
   local name
   for name in $* ; do
      local out=/tmp/$name
      echo $msg making $out
-     make-cmd
-     eval $(make-cmd)
+     hello-make-cmd
+     eval $(hello-make-cmd)
   done
 }
 
-make hwserver hwclient
+hello-make hwserver hwclient
 
