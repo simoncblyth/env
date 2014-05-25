5bf9f86b271663c6b97b1868055f52166a55f95b
Simon C Blyth <simoncblyth@gmail.com>
2014-05-25 16:00:37 +0800

    avoid a failed ROOT import from scrubbing sys.argv

--- root/import_ROOT.py
+++ root/import_ROOT.py
@@ -6,9 +6,15 @@
 import sys 
 _argv = sys.argv
 sys.argv = []
-import ROOT
-# Have to do something with the module to get TApplication initialized
-ROOT.TObject # This just fetches the class and does nothing else
+
+try:
+    import ROOT
+    # Have to do something with the module to get TApplication initialized
+    ROOT.TObject # This just fetches the class and does nothing else
+except ImportError:
+    ROOT = None
+
+
 sys.argv = _argv
 del _argv
 
