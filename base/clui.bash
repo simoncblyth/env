97fb60d27ac573f0edcddd7faf3650bc13285976
Simon Blyth <simoncblyth@gmail.com>
2014-09-18 13:50:28 +0800

    bash tools for finding repo roots and providing repo relative paths from a PWD relative one

--- base/clui.bash
+++ base/clui.bash
@@ -19,6 +19,21 @@ clui-env(){
 
 clui-usage(){ cat << EOU
 
+*clui-root*
+    Emit repository root folder
+    (Mercurial only)
+
+    For SVN would need to shimmy up the directory tree looking 
+    for the root .svn folders in 1.6+ OR the last .svn for
+    earlier SVN
+
+*clui-path relpath*
+    provide repo relative path for a PWD relative path
+    (Mercurial only)
+
+
+
+
 EOU
 }
 
@@ -36,7 +51,7 @@ clui-alias(){
    alias lz="ls -la -Z"
    alias ll="ls -ltra "
    alias lt="ls -lt $se "
-   alias p="ps aux $se"
+   alias pa="ps aux $se"
    alias n="nosetests"
    alias ns="nosetests -s"
    alias nsv="nosetests -s -v"
@@ -56,6 +71,8 @@ clui-alias(){
    alias eu="env-u"
    #alias pyc="find $ENV_HOME -name '*.pyc' -exec rm -f {} \;"
    alias pyc="clui-pyc"
+   alias p="clui-path"
+   alias pp="clui-ppath"
 
 }
 
@@ -64,9 +81,61 @@ clui-alias(){
 #   svn --username $USER ci $*
 #}
 
+
+clui-svnroot-old(){    # shimmy up tree and emit path of last folder containing .svn
+   local dir=$1
+   local last=$2
+   [ "$dir" == "/" ] && echo "" && return
+   [ -d "$dir/.svn" ]   && clui-svnroot-old $(dirname $dir) $dir
+   [ ! -d "$dir/.svn" ] && echo $last
+}
+
+clui-svnroot-new(){    # shimmy up tree and emit path of first folder containing .svn 
+   local msg="=== $FUNCNAME :"
+   local dir=$1
+   #echo $msg $dir 
+   [ "$dir" == "/" ] && echo "" && return
+   [ ! -d "$dir/.svn" ] && clui-svnroot-new $(dirname $dir)
+   [ -d "$dir/.svn" ] && echo $dir
+}
+
+clui-svnroot(){  # absolute path of svn repo root folder
+   [ "$(which realpath)" == "" ] && echo no realpath : make it with env/tools/realpath/Makefile && return
+   local dir=$(realpath ${1:-$PWD})
+   local vers=$(svn --version --quiet)
+   case $vers in 
+      1.6*) clui-svnroot-old $dir ;;
+      1.7*) clui-svnroot-new $dir ;;
+      1.8*) clui-svnroot-new $dir ;;
+   esac
+}
+
+clui-gitroot(){ # untested
+   git rev-parse --show-toplevel 2>/dev/null 
+}
+
+clui-root(){ # Mercurial or SVN root of the 
+   local root=$(hg root 2>/dev/null) 
+   [ "$root" == "" ] && root=$(clui-svnroot)
+   [ "$root" == "" ] && root=$(clui-gitroot)
+   echo $root
+}
+
+clui-path(){ # normally invoked with alias p, provides repobase/repopath for the relative path argument 
+   local path=${1:-$PWD}
+   local real=$(realpath $path)
+   local root=$(clui-root)
+   local base=$(basename $root)
+   echo $base/${real/$root\/}
+}
+clui-ppath(){
+   local path=$(clui-path $*)
+   echo \`$path\`::
+}
+
 clui-pyc(){
    local msg="=== $FUNCNAME : "
-   local root=$(hg root 2>/dev/null)
+   local root=$(clui-root)
    if [ "$root" == "" ]; then
       echo $msg not in hg repo : remove pyc beneath pwd $PWD
       root="."
