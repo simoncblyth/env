a57d977940853937fed1b7a63cf2ee651b0c7ecb
Simon Blyth <simoncblyth@gmail.com>
2014-10-31 12:04:06 +0800

    adopt the row major serialization and row vector convention used by G4AffineTransform internally for consistency of external usage from numpy

--- chroma/G4DAEChroma/src/G4DAETransformCache.cc
+++ chroma/G4DAEChroma/src/G4DAETransformCache.cc
@@ -201,7 +201,7 @@ std::size_t G4DAETransformCache::GetSize(){
 Key_t G4DAETransformCache::GetKey( std::size_t index )
 {
     if( index > m_itemcapacity ) return 0 ;
-        Key_t* id = m_key + index*m_keysize  ;
+    Key_t* id = m_key + index*m_keysize  ;
     return id[0];
 }
 
@@ -221,24 +221,6 @@ G4AffineTransform* G4DAETransformCache::FindTransform( Key_t key )
 } 
 
 
-G4AffineTransform* G4DAETransformCache::GetTransform( std::size_t index )
-{
-    if( index > m_itemcapacity ) return NULL ;
-    double* data = m_data + index*m_itemsize ;   
-
-    CLHEP::HepRep3x3 r33( 
-            data[0], data[1], data[2] ,         
-            data[4], data[5], data[6] ,         
-            data[8], data[9], data[10] 
-    );         
-    G4RotationMatrix rot(r33) ; 
-    G4ThreeVector tlate(data[3],data[7],data[11]) ; 
-
-    G4AffineTransform* transform = new G4AffineTransform();
-    transform->SetNetRotation(rot);
-    transform->SetNetTranslation(tlate);
-    return transform; 
-}
 
 void G4DAETransformCache::Add( Key_t key, const G4AffineTransform&  transform )
 {
@@ -263,23 +245,28 @@ void G4DAETransformCache::AddSerial( Key_t key, const G4AffineTransform&  transf
     id[0] = key ; 
 
 /*
+  External use of G4AffineTransform data 
+  ======================================= 
 
-  Indexed access to G4AffineTransform follows
-  row-vector convention, with translation
-  along the bottom 
-  
-  my attempt to convert this into the more familiar column-vector
-  convention layout, was a bad idea 
-  TODO: adjust to just following the row-vector convention
-  as used in G4AffineTransform 
+    R(row)(col)
 
-  can always transpose in numpy when want to post- rather than pre- multiply  
- 
+     col=x col=y  col=z  col=w 
 
-    0:Rxx  1:Rxy  2:Rxz   3:0.
-    4:Ryx  5:Ryy  6:Ryz   7:0.  
-    8:Rzx  9:Rzy 10:Rzz  11:0.
-   12:Tx  13:Ty  14:Tz   15:1.
+    0:Rxx  1:Rxy  2:Rxz   3:0.    row=x
+    4:Ryx  5:Ryy  6:Ryz   7:0.    row=y
+    8:Rzx  9:Rzy 10:Rzz  11:0.    row=z
+   12:Tx  13:Ty  14:Tz   15:1.    row=w
+
+   
+  * serialization indices follows row-major layout 
+
+  * transforms adopt row-vector convention, meaning the 
+    translation portion appears in last row (rather than 
+    the more familiar last column of column-vector convention)
+
+  * this means than use of the serialized arrays from numpy
+    needs to pre-multiply `np.dot(vec, m)` or post-multiply
+    with transpose applied `np.dot( m.T, vec)` 
 
 */
 
@@ -289,28 +276,47 @@ void G4DAETransformCache::AddSerial( Key_t key, const G4AffineTransform&  transf
     data[0] = rot.xx();
     data[1] = rot.xy();
     data[2] = rot.xz();
+    data[3] = 0. ;
 
     data[4] = rot.yx();
     data[5] = rot.yy();
     data[6] = rot.yz();
+    data[7] = 0. ;
 
     data[8] = rot.zx();
     data[9] = rot.zy();
     data[10] = rot.zz();
+    data[11] = 0. ;
 
-
-    data[3] = tlate.x();
-    data[7] = tlate.y();
-    data[11] = tlate.z();
-
-    data[12] = 0. ;
-    data[13] = 0. ;
-    data[14] = 0. ;
+    data[12] = tlate.x();
+    data[13] = tlate.y();
+    data[14] = tlate.z();
     data[15] = 1. ;
 
-  //
 }
 
 
+G4AffineTransform* G4DAETransformCache::GetTransform( std::size_t index )
+{
+    if( index > m_itemcapacity ) return NULL ;
+    double* data = m_data + index*m_itemsize ;   
+
+    CLHEP::HepRep3x3 r33( 
+            data[0], data[1], data[2] ,         
+            data[4], data[5], data[6] ,         
+            data[8], data[9], data[10] 
+    );         
+    G4RotationMatrix rot(r33) ; 
+    G4ThreeVector tlate(data[12],data[13],data[14]) ; 
+
+    G4AffineTransform* tr0 = new G4AffineTransform();
+    tr0->SetNetRotation(rot);
+    tr0->SetNetTranslation(tlate);
+
+    G4AffineTransform* tr1 = new G4AffineTransform(rot,tlate);
+    assert( *tr0 == *tr1 );
+
+    return tr1; 
+}
 
 
