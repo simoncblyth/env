ee6498927b6da6ea08c403b7dafb1edc345783ce
Simon Blyth <simoncblyth@gmail.com>
2014-12-22 20:28:40 +0800

    add omitted G4DAEPhoton minimal type class

---
+++ chroma/G4DAEChroma/G4DAEChroma/G4DAEPhoton.hh
@@ -0,0 +1,39 @@
+#ifndef G4DAEPHOTON_H
+#define G4DAEPHOTON_H 
+
+class G4DAEPhoton  {
+    public:
+
+    static const char* TMPL ;   // name of envvar containing path template 
+    static const char* SHAPE ;  // numpy array itemshape eg "8,3" or "4,4" 
+    static const char* KEY ;  
+
+    enum {
+       _post_x,
+       _post_y,
+       _post_z,
+       _post_w,
+
+       _dirw_x,
+       _dirw_y,
+       _dirw_z,
+       _dirw_w,
+
+       _polw_x,
+       _polw_y,
+       _polw_z,
+       _polw_w,
+
+       _flag_x,
+       _flag_y,
+       _flag_z,
+       _flag_w
+    };
+
+
+};
+
+
+#endif 
+
+
---
+++ chroma/G4DAEChroma/src/G4DAEPhoton.cc
@@ -0,0 +1,7 @@
+#include "G4DAEChroma/G4DAEPhoton.hh"
+
+const char* G4DAEPhoton::TMPL = "DAE_PHOTON_PATH_TEMPLATE" ;
+const char* G4DAEPhoton::SHAPE = "4,4" ;
+const char* G4DAEPhoton::KEY   = "PHO" ;
+
+
