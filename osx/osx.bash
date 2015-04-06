eb8b6e4574dc158b1f0560696415f58a7888cb4f
Simon Blyth <simoncblyth@gmail.com>
2015-04-06 14:11:20 +0800

    remove mutex locked queue approach classes usurped by multi io_service boost::asio approach, add npy response via posts from another thread functionality

--- osx/osx.bash
+++ osx/osx.bash
@@ -32,4 +32,33 @@ osx-library-visible(){
    chflags nohidden ~/Library/
 }
 
+osx-prevent-ds-store-droppings-on-shares(){
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+  cat << EON
+
+Mac OS X v10.4 and later: How to prevent .DS_Store file creation over network connections
+
+https://support.apple.com/en-gb/ht1629
+
+After changing defaults, 
+   Either restart the computer or log out and back in to the user account.
+
+If you want to prevent .DS_Store file creation for other users on the same
+computer, log in to each user account and perform the steps above—or distribute
+a copy of the newly modified com.apple.desktopservices.plist file to the
+~/Library/Preferences folder of other user accounts.  These steps do not
+prevent the Finder from creating .DS_Store files on the local volume, and these
+steps do not prevent previously existing .DS_Store files from being copied to
+the remote file server.
+
+Disabling the creation of .DS_Store files on remote file servers can cause
+unexpected behavior in the Finder (click here for an example).
+
+https://support.apple.com/kb/TA21373?locale=en_GB
+
+EON
+
+}
+
 
