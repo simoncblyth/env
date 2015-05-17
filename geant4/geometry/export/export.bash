b30416df60a43d8b3889ef5189286eae600d148b
Simon Blyth <simoncblyth@gmail.com>
2015-05-17 15:08:20 +0800

    Scintillation step photon generation within OptiX context, distributions for Scintillation and Cerenkov are matching Geant4/NuWa/DetSim

--- geant4/geometry/export/export.bash
+++ geant4/geometry/export/export.bash
@@ -292,6 +292,15 @@ opcerenkov
    Chroma generated cerenkov photon
 
 
+oxscintillation
+   OptiX generated scintilation photon
+
+oxcerenkov
+   OptiX generated cerenkov photon
+
+
+
+
 scintillation
    Scintillation Generation Step
 
@@ -393,6 +402,11 @@ export-export-templates(){
 
    export DAE_OPCERENKOV_PATH_TEMPLATE=$(export-path-template opcerenkov)
    export DAE_OPSCINTILLATION_PATH_TEMPLATE=$(export-path-template opscintillation)
+
+   export DAE_OXCERENKOV_DESCRIPTION="Cerenkov photons generated from OptiX based machinery"
+   export DAE_OXSCINTILLATION_DESCRIPTION="Scintillation photons generated from OptiX based machinery"
+   export DAE_OXCERENKOV_PATH_TEMPLATE=$(export-path-template oxcerenkov)
+   export DAE_OXSCINTILLATION_PATH_TEMPLATE=$(export-path-template oxscintillation)
 
    export DAE_OPCERENKOVGEN_PATH_TEMPLATE=$(export-path-template opcerenkovgen)
    export DAE_OPSCINTILLATIONGEN_PATH_TEMPLATE=$(export-path-template opscintillationgen)
