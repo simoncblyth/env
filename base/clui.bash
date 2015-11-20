4d5dd12b7acf736504cec077dd0244bdd364e248
Simon Blyth <simoncblyth@gmail.com>
2015-11-20 14:09:12 +0800

    bring some of the pmt- python simple geometry partitioning functionality over to npy- npart nplane ndisc nquad nsphere

--- base/clui.bash
+++ base/clui.bash
@@ -66,6 +66,7 @@ clui-alias(){
    alias t='type'
    alias f='typeset -F'   ## list functions 
    alias e='cd $ENV_HOME ; hg st '
+   alias s='hg st '
    alias vip='vi ~/.bash_profile'
    alias vips='grep BUILD_PATH ~/.bash_profile | grep -v grep '
    alias eu="env-u"
