513a4c58fb443b8dca5db2a1d9ee82dfd7f6a5f3
Simon Blyth <simoncblyth@gmail.com>
2015-12-07 14:02:53 +0800

    normalization of XYZ attempt using whitepoint

--- graphics/photonmap/photonmap.bash
+++ graphics/photonmap/photonmap.bash
@@ -8,6 +8,35 @@ photonmap-usage(){ cat << EOU
 https://code.google.com/p/275a-photonmap/source/checkout
 
 
+https://www.inf.ed.ac.uk/publications/thesis/online/IM090675.pdf
+
+Photon Mapping on the GPU
+Martin Fleisz
+
+http://www.ks.uiuc.edu/Research/vmd/doxygen/CUDASpatialSearch_8cu-source.html
+
+
+
+http://stackoverflow.com/questions/29445429/how-to-bring-equal-elements-together-using-thrust-without-sort
+
+https://sites.google.com/a/compgeom.com/stann/
+
+
+http://http.developer.nvidia.com/GPUGems3/gpugems3_ch32.html
+
+
+Spatial Data Structures, Sorting and GPU Parallelism for Situated-agent Simulation and Visualisation
+
+http://worldcomp-proceedings.com/proc/p2012/MSV2429.pdf
+
+
+
+Fast Uniform Grid Construction on GPGPUs Using Atomic Operations
+
+http://www.ce.uniroma2.it/publications/parco2013_uniformgrids.pdf
+
+
+
 
 EOU
 }
