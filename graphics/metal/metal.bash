d58c00fcfaacc792d633a5616f85c75c807cca15
Simon Blyth <simoncblyth@gmail.com>
2016-01-04 15:40:17 +0800

    metal refs

--- graphics/metal/metal.bash
+++ graphics/metal/metal.bash
@@ -5,10 +5,34 @@ metal-vi(){       vi $(metal-source) ; }
 metal-env(){      elocal- ; }
 metal-usage(){ cat << EOU
 
-Metal : close to the metal OpenGL ES alternative on iOS
-=========================================================
+Metal : close to the metal OpenGL ES alternative on iOS and OSX
+=================================================================
+
+
+Warren Moore
+-------------
 
 * http://metalbyexample.com/
+* https://github.com/metal-by-example
+* https://realm.io/news/3d-graphics-metal-swift/
+* https://github.com/warrenm/slug-swift-metal/
+
+Amund Tveit
+-------------
+
+* http://memkite.com/blog/2015/06/10/swift-and-metal-gpu-programming-on-osx-10-11-el-capitan/
+* http://memkite.com/blog/2014/12/30/example-of-sharing-memory-between-gpu-and-cpu-with-swift-and-metal-for-ios8/
+* https://github.com/atveit
+
+Simon Gladman
+--------------
+
+* https://realm.io/news/swift-summit-simon-gladman-metal/
+* https://github.com/FlexMonkey
+* https://github.com/FlexMonkey/MetalKit-Particles
+* https://github.com/FlexMonkey/ParticleLab
+
+
 
 
 
