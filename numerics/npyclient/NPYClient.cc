732abc81482fd6a89d0b2304806b9b68665b1363
Simon Blyth <simoncblyth@gmail.com>
2016-06-18 18:40:41 +0800

    boost logging config doesnt pass across dll divide when using static lib boost, portable-izing more of npy-

--- numerics/npyclient/NPYClient.cc
+++ numerics/npyclient/NPYClient.cc
@@ -2,9 +2,24 @@
 
 int main()
 {
-    NPY<float>* npy = NPY<float>::make(10,1,4) ; 
-    npy->zero();
-    npy->save("/tmp/NPYClient.npy");
+
+    NPYBase::setGlobalVerbose();
+
+    const char* path = "/tmp/NPYClient.npy" ; 
+
+
+    unsigned int N = 10 ; 
+
+    NPY<float>* a = NPY<float>::make(N,1,4) ; 
+    a->zero();
+
+    for(unsigned int i=0; i < N ; i++) a->setQuad(i,0,0, i*10 + 0,i*10+1, i*10+2, i*10+3) ;
+    a->save(path);
+
+
+    NPY<float>* b = NPY<float>::load(path);
+    b->dump();
+
 
     return 0 ; 
 }
