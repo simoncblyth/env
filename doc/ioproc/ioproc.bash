33257d3a64e6c27c79b62abeda5c8a2feb198277
Simon Blyth <simoncblyth@gmail.com>
2017-01-14 18:36:55 +0800

    proceedings almost there, needs a dyb fig and cleaning

--- doc/ioproc/ioproc.bash
+++ doc/ioproc/ioproc.bash
@@ -86,6 +86,39 @@ ioproc-guide(){ open $(ioproc-dir)/JPCSLaTeXGuidelines.pdf ; }
 
 ioproc-etex(){ echo $(ioproc-edir)/$(ioproc-conf).tex ; }
 ioproc-edit(){ vi $(ioproc-etex) ; }
+
+ioproc-make-verbose(){ export VERBOSE=1 ; ioproc-make ; }
+
+ioproc-make0-(){
+  local tex=$1
+  local dvi=${tex/.tex}.dvi
+  if [ -n "$VERBOSE" ]; then
+      latex $tex 
+      latex $tex 
+      latex $tex 
+      dvipdf $dvi
+  else
+      latex $tex > /dev/null
+      latex $tex > /dev/null
+      latex $tex > /dev/null
+      dvipdf $dvi > /dev/null  
+  fi 
+}
+
+ioproc-make1-(){
+ # http://tex.stackexchange.com/questions/17734/cannot-determine-size-of-graphic
+  local tex=$1
+  if [ -n "$VERBOSE" ]; then
+      pdflatex $tex 
+      pdflatex $tex 
+      pdflatex $tex 
+  else
+      pdflatex $tex > /dev/null
+      pdflatex $tex > /dev/null
+      pdflatex $tex > /dev/null
+  fi 
+}
+
 ioproc-make(){
 
    ioproc-cd
@@ -93,22 +126,14 @@ ioproc-make(){
    local etex=$(ioproc-etex) 
 
    local tex=$conf.tex
-   local dvi=$conf.dvi
-   local pdf=$conf.pdf
+   local pdf=${tex/.tex}.pdf
 
    [ $etex -nt $tex ] && echo $msg UPDATING FROM ETEX $etex && cp $etex $tex || return 
 
-   if [ -n "$VERBOSE" ]; then
-       latex $tex 
-       latex $tex 
-       latex $tex 
-       dvipdf $dvi
-   else
-       latex $tex > /dev/null
-       latex $tex > /dev/null
-       latex $tex > /dev/null
-       dvipdf $dvi > /dev/null  
-   fi
+   echo tex $tex
+
+   #ioproc-make0- $tex
+   ioproc-make1- $tex
 
    ls -l $pdf
 }
@@ -126,7 +151,67 @@ ioproc-info(){ cat << EOI
    ioproc-pdf  : $(ioproc-pdf)
 
 
+simon:chep2016 blyth$ l $HOME/simoncblyth.bitbucket.org/env/graphics/ggeoview/jpmt-inside-wide*
+-rw-r--r--@ 1 blyth  staff   622872 Dec 19 14:00 /Users/blyth/simoncblyth.bitbucket.org/env/graphics/ggeoview/jpmt-inside-wide_crop_half_half.png
+-rw-r--r--  1 blyth  staff  2285892 May  8  2016 /Users/blyth/simoncblyth.bitbucket.org/env/graphics/ggeoview/jpmt-inside-wide_crop_half.png
+-rw-r--r--@ 1 blyth  staff  6601014 Mar 26  2016 /Users/blyth/simoncblyth.bitbucket.org/env/graphics/ggeoview/jpmt-inside-wide_crop.png
+-rw-r--r--  1 blyth  staff  2926418 Jan 13  2016 /Users/blyth/simoncblyth.bitbucket.org/env/graphics/ggeoview/jpmt-inside-wide_half.png
+-rw-r--r--@ 1 blyth  staff  9162714 Jan 13  2016 /Users/blyth/simoncblyth.bitbucket.org/env/graphics/ggeoview/jpmt-inside-wide.png
+simon:chep2016 blyth$ 
+
+
+    Make a dyb plot with ray trace sliced composited with raster ?
+
+        op -c --analyticmesh 1   # analytic PMTs
+
+        /Users/blyth/opticks/oglrap/gl/tex/frag.glsl
+
+::
+
+ 15 void main ()
+ 16 {
+ 17    frag_colour = texture(ColorTex, texcoord);
+ 18    float depth = frag_colour.w ;  // alpha is hijacked for depth in pinhole_camera.cu material1_radiance.cu
+ 19    frag_colour.w = 1.0 ;
+ 20 
+ 21    gl_FragDepth = depth  ;
+ 22 
+ 23    if(NrmParam.z == 1)
+ 24    {
+ 25         if(depth < ScanParam.x || depth > ScanParam.y ) discard ;
+ 26    }
+
+
 
 EOI
 }
+
+
+ioproc-figs-(){ cat << EOF
+$HOME/simoncblyth.bitbucket.org/env/graphics/ggeoview/jpmt-inside-wide.png
+$HOME/simoncblyth.bitbucket.org/env/graphics/ggeoview/jpmt-inside-wide_crop_half_half.png
+EOF
+}
+
+
+ioproc-figs-copy(){
+
+  local dstd=$(ioproc-dir)
+
+  local msg="=== $FUNCNAME :"
+  local src
+  ioproc-figs- | while read src ; do
+     [ ! -f "$src" ] && echo $msg src $src does not exist && return 
+     local nam=$(basename $src)
+     local dst=$dstd/$nam
+
+     if [ -f "$dst" ]; then  
+         echo $msg dst $dst already copied 
+     else
+         cp $src $dst 
+     fi
+  done
+
+}
+
 
