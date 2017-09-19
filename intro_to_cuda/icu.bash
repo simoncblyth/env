1e265b590449df399ca089f1a01dad5fd2dacc66
Simon Blyth <simoncblyth@gmail.com>
2017-09-19 14:15:15 +0800

    links update

--- intro_to_cuda/icu.bash
+++ intro_to_cuda/icu.bash
@@ -9,13 +9,38 @@ Introduction To CUDA
 =======================
 
 
+CUDA Documentation/Download
+-----------------------------
+
+* http://docs.nvidia.com/cuda/index.html
+
+Hello World Examples
+----------------------
+
+* https://bitbucket.org/simoncblyth/env/src/tip/intro_to_cuda/
+ 
+
+GPU Intro
+----------
+
+* https://blogs.nvidia.com/blog/2009/12/16/whats-the-difference-between-a-cpu-and-a-gpu/
 
 CUDA Intro
 -----------
 
-* http://www.nvidia.com/content/GTC-2010/pdfs/2131_GTC2010.pdf
-
 * http://on-demand.gputechconf.com/gtc-express/2011/presentations/GTC_Express_Sarah_Tariq_June2011.pdf
+
+
+cudaMalloc : why void** ?
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+::
+
+    int* ptr = 0;
+    void** ptr_to_ptr = &ptr;
+    cudaMalloc(ptr_to_ptr, sizeof(int));
+    assert(ptr != 0);
+    // ptr now points to a segment of device memory
 
 
 Thrust
@@ -23,9 +48,11 @@ Thrust
 
 * http://on-demand.gputechconf.com/gtc/2012/presentations/S0602-Intro-to-Thrust-Parallel-Algorithms-Library.pdf
 
-* http://ppomorsk.sharcnet.ca/CSE746/lecture8_CSE746_2014.pdf
 
+GTC Search for CUDA
+------------------------
 
+* http://on-demand-gtc.gputechconf.com/gtcnew/on-demand-gtc.php?searchByKeyword=CUDA&searchItems=&sessionTopic=&sessionEvent=&sessionYear=&sessionFormat=&submit=&select=
 
 
 
