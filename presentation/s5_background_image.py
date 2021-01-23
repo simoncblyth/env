27bcfa72ce7b6de26f01eb97621f8073a0184b55
Simon C Blyth <simoncblyth@gmail.com>
2021-01-23 11:45:53 +0000

    generalize s5_background_image to allow more than one on a page, adding pages on opticks users

--- presentation/s5_background_image.py
+++ presentation/s5_background_image.py
@@ -9,8 +9,94 @@ urls = []
 class s5backgroundimage(nodes.General, nodes.Inline, nodes.Element):
     pass
 
-def render_s5backgroundimage( n ):
+class div_background(object):
+
+    div_tmpl = r"""div.slide#%(tid)s{
+             background-image: %(image)s;
+             background-size: %(size)s;
+             background-position: %(position)s;
+             %(extra)s
+          }"""
+
+    def parse_spec(self, spec_line):
+        spec_elem = spec_line.split()
+        nelem = len(spec_elem)
+        size, position, extra = "auto_auto", "0px_0px", ""
+        if nelem > 0:url = spec_elem[0] 
+        if nelem > 1:size = spec_elem[1]
+        if nelem > 2:position = spec_elem[2]
+        if nelem > 3:extra = spec_elem[3]
+        if len(extra) > 0: 
+            extra = "%s ; " % extra  
+        pass
+        _ = lambda _:_.replace("_"," ")
+        return dict(url=url, size=_(size),position=_(position), extra=_(extra)) 
+
+    def __init__(self, lines):
+        title, specs = lines[0], lines[1:]
+        self.lines = lines 
+        self.title = title
+        self.specs = specs
+        dd = [] 
+        for spec in specs:
+            d = self.parse_spec(spec)
+            urls.append(d["url"]) 
+            dd.append(d)
+        pass
+        dc = {}
+        dc["image"] = ",".join(map(lambda d:"url(%s)" % d["url"], dd))   
+        dc["size"] = ",".join(map(lambda d:"%s" % d["size"], dd))
+        dc["position"] = ",".join(map(lambda d:"%s" % d["position"], dd))
+        dc["extra"] = " ".join(map(lambda d:"%s" % d["extra"], dd))
+        dc["tid"] = nodes.make_id(title)
+
+        self.html = self.div_tmpl % dc
+
+    def __repr__(self):
+        return "div_background %d" % (len(self.lines))
+    def __str__(self):
+        return self.html
+
+
+def render_s5backgroundimage_1( n ):
     """
+    Generalize to handle multiple urls, size and position 
+    """
+    style_tmpl = r"""
+       <style type="text/css">
+
+          div.slide { 
+             background-clip: border-box;
+             background-repeat: no-repeat;
+             height: 100%%;
+          }
+          %(divs)s 
+
+       </style>
+    """
+    global urls  # kludge
+
+    content = "\n".join(filter(lambda _:len(_) == 0 or _[0] != '#', n.content)) + "\n" # remove comments and make into big string
+    divs = []
+    for i, item in enumerate(content.split("\n\n")):  # split on empty lines
+        lines = item.split("\n")
+        if len(lines) < 2: 
+            print("skip single line item [%s] " % lines[0])
+        else:
+            div = div_background(lines)
+            print(div)
+            divs.append(div.html)
+        pass
+    pass
+    html = style_tmpl % dict(divs="\n          ".join(divs))
+    return html
+
+
+
+def render_s5backgroundimage_0( n ):
+    """
+    Old way handles only one url per item 
+
     http://www.w3schools.com/cssref/tryit.asp?filename=trycss3_background-size
 
 
@@ -28,6 +114,7 @@ def render_s5backgroundimage( n ):
              %(extra)s
           }"""
 
+
     style_tmpl = r"""
        <style type="text/css">
 
@@ -41,6 +128,7 @@ def render_s5backgroundimage( n ):
        </style>
     """
     global urls  # kludge
+
     content = filter(lambda _:_[0] != '#',filter(lambda _:len(_) > 0, n.content))
     assert len(content) % 2 == 0  
     divs = []
@@ -115,7 +203,11 @@ class S5BackgroundImage(Directive):
         #    slides become sections and only 2 pages show 
         #
 
-        raw = nodes.raw('', render_s5backgroundimage(n), format = 'html')
+        #render = render_s5backgroundimage_0
+        render = render_s5backgroundimage_1
+
+        raw = nodes.raw('', render(n), format = 'html')
+
         raw.document = self.state.document
         return [raw]
  
