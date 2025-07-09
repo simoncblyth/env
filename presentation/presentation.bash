3c3d1b5afbdc017921be7279555f9254bae34b55
Simon C Blyth <simoncblyth@gmail.com>
2025-07-09 19:29:43 +0800

    wp_pmt_side_ipc

--- presentation/presentation.bash
+++ presentation/presentation.bash
@@ -1576,6 +1576,7 @@ presentation-rst2talk-(){
 #presentation-iname(){ echo ${INAME:-opticks_20241122_ihep_assessment_5min} ; }
 #presentation-iname(){ echo ${INAME:-opticks_20241122_ihep_assessment_5min_TALK} ; }
 #presentation-iname(){ echo ${INAME:-opticks_20250303_ihep_gpu_symposium} ; }
-presentation-iname(){ echo ${INAME:-opticks_20250708} ; }
+#presentation-iname(){ echo ${INAME:-opticks_20250708} ; }
+presentation-iname(){ echo ${INAME:-opticks_202507XX_kaiping} ; }
 
 
